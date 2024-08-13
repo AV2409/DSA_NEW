c3ae0c342d57684c89dd67607bfcc4e7d4af4381
AV2409 <142912539+AV2409@users.noreply.github.com>
2024-08-13 14:26:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (55.66%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int rev = 0;
+
+        // if(n>INT_MAX||n<INT_MIN) return 0;
+        while (n != 0) {
+            if ((rev > INT_MAX / 10) || (rev < INT_MIN / 10))
+                return 0;
+            int rem = n % 10;
+            rev = rev * 10 + rem;
+            n = n / 10;
+        }
+
+        return rev;
+    }
+};
