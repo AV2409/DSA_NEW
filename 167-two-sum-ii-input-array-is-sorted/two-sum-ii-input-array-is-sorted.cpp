eed7167e7e363c58411efbea0257299b99b7b932
AV2409 <142912539+AV2409@users.noreply.github.com>
2024-08-16 13:07:54 +0530

    Time: 8 ms (62.59%) | Memory: 18.3 MB (37.69%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,6 +1,22 @@
 class Solution {
+
+private:
+    int find(vector<int>& arr, int req, int s,int e){
+        int mid=s+(e-s)/2;
+        while(s<=e){
+            if(arr[mid]==req) return mid;
+            else if(arr[mid]>req) e=mid-1;
+            else s=mid+1;
+
+            mid=s+(e-s)/2;
+        }
+        return -1;
+    }
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+
+        //2Pointer
+        /*
         int i=0;
         int j=numbers.size()-1;
         int sum;
@@ -19,6 +35,24 @@ public:
 
         }
         return ans;
+        */
+
+        //Binary search
+
+        int i=0;
+        int n=numbers.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int req=target-numbers[i];
+            int x=find(numbers,req,i+1,n-1);
+            if(x!=-1){
+                ans.push_back(i+1);
+                ans.push_back(x+1);
+                return ans;
+            }
+        }
+        return ans;
+
         
     }
 };
