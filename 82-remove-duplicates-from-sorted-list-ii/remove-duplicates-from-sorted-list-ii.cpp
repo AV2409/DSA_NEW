7a4daae38eb9966b35819f91137958365526239f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2024-09-03 21:36:21 +0530

    Time: 4 ms (68.65%) | Memory: 16.3 MB (14.37%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    void insert_at_tail(ListNode*& head, int x) {
+        if (head == NULL) {
+            head = new ListNode(x);
+            return;
+        }
+
+        ListNode* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        ListNode* newnode = new ListNode(x);
+        temp->next = newnode;
+    }
+
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        // ListNode* head1 = NULL;
+
+        // ListNode* temp = head;
+        // if(head==NULL) return head;
+        // if (head1 == NULL) {
+        //     insert_at_tail(head1, temp->val);
+        //     temp = temp->next;
+        // }
+
+        // ListNode* temp1 = head1;
+
+        // while (temp) {
+        //     if (temp1->val != temp->val) {
+        //         insert_at_tail(head1, temp->val);
+        //         temp1 = temp1->next;
+        //     }
+        //     temp = temp->next;
+        // }
+
+        ListNode* temp = head;
+        map<int, int> mp;
+        if (head == NULL)
+            return head;
+        while (temp) {
+            if (mp.find(temp->val) == mp.end()) {
+                mp.insert({temp->val, 1});
+            }
+
+            else {
+                mp[temp->val]++;
+            }
+            temp = temp->next;
+        }
+        ListNode* head1 = NULL;
+        for (auto it : mp) {
+            if (it.second == 1) {
+                insert_at_tail(head1, it.first);
+            }
+
+        }
+        return head1;
+    }
+};
