1c20e0f6d63587c497a63d20517d358f68c29dd4
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-06-27 17:31:17 +0530

    Time: 92 ms (5.06%) | Memory: 138.2 MB (5.01%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    void dfs_traversal(vector<int> adj[], int node, vector<int>& visited) {
+    void dfs_traversal(vector<vector<int>> isConnected, int node,
+                       vector<int>& visited,int n) {
         visited[node] = 1;
-        for (int i : adj[node]) {
-            if (!visited[i])
-                dfs_traversal(adj, i, visited);
+        for (int i =0;i<n;i++) {
+            if ((isConnected[node][i] == 1) && (!visited[i]))
+                dfs_traversal(isConnected, i, visited,n);
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        int m = isConnected[0].size();
-        vector<int> adj[n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if ((isConnected[i][j] == 1) && i != j)
-                    adj[i].push_back(j);
-            }
-        }
-
-        cout << n << " " << m << endl;
-
-        for (int i = 0; i < n; i++) {
-            cout << "Node " << i << ": ";
-            for (int j : adj[i]) {
-                cout << j << " ";
-            }
-            cout << endl;
-        }
-
         vector<int> visited(n, 0);
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs_traversal(adj, i, visited);
+                dfs_traversal(isConnected, i, visited,n);
                 cnt++;
             }
         }
