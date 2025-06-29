50f9d46680853b019dee0ceeabcca7d844a2f6dc
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-06-29 20:09:12 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (39.78%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int f(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        if (m == 0 && n == 0) {
-            return dp[m][n] = grid[0][0];
-        }
-        if (m < 0 || n < 0) {
-            return 1e9;
-        }
-        if (dp[m][n] != -1) {
-            return dp[m][n];
-        }
-        int u = grid[m][n] + f(m - 1, n, grid, dp);
-        int r = grid[m][n] + f(m, n - 1, grid, dp);
-        return dp[m][n] = min(u, r);
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(m - 1, n - 1, grid, dp);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = grid[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int x1 = 1e9;
+                int x2 = 1e9;
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (i > 0)
+                    x1 = grid[i][j] + dp[i - 1][j];
+                if (j > 0)
+                    x2 = grid[i][j] + dp[i][j - 1];
+                dp[i][j] = min(x1, x2);
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
