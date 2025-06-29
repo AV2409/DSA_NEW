2c5ca76477bf32ac01ee240244d5ace12696e79e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-06-29 20:26:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (30.99%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int f(int i, int j, vector<vector<int>>& triangle, int n,
           vector<vector<int>>& dp) {
-        if (i == n) {
+        if (i == n-1) {
             return dp[i][j] = triangle[i][j];
         }
-        if (i > n) {
+        if (i > n-1) {
             return 1e9;
         }
         if (dp[i][j] != -1) {
@@ -21,6 +21,6 @@ public:
         int n = triangle.size();
         // return f_rec(0, 0, triangle, n - 1);
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(0, 0, triangle, n - 1, dp);
+        return f(0, 0, triangle, n, dp);
     }
 };
