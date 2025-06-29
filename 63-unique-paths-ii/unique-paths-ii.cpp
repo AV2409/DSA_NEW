2bf4f00c28482fc9892d455c61113a3010bef639
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-06-29 19:54:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (6.01%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int f(int m, int n, vector<vector<int>>& obstacleGrid,
-          vector<vector<int>>& dp) {
-        if (m < 0 || n < 0) {
-            return 0;
-        }
-        if (obstacleGrid[m][n] == 1) {
-            return dp[m][n] = 0;
-        }
-        if (m == 0 && n == 0) {
-            return dp[m][n] = 1;
-        }
-
-        if (dp[m][n] != -1) {
-            return dp[m][n];
-        }
-        int u = f(m - 1, n, obstacleGrid, dp);
-        int r = f(m, n - 1, obstacleGrid, dp);
-        return dp[m][n] = u + r;
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(m - 1, n - 1, obstacleGrid, dp);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    int x1 = 0;
+                    int x2 = 0;
+                    if (i > 0) {
+                        x1 = dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        x2 = dp[i][j - 1];
+                    }
+                    dp[i][j] = x1 + x2;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
