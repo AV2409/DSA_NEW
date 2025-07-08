8fa656b0e905430d227163b0d4e166e3aeabce94
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-07-08 19:39:07 +0530

    Time: 14 ms (62.96%) | Memory: 16.6 MB (75.43%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
-    vector<int> dijkstra(int V, vector<vector<pair<int, int>>>& adj, int src) {
-        vector<int> ans(V, 1e9);
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            q;
-        q.push({0, src});
-        ans[src] = 0;
-        while (!q.empty()) {
-            pair<int, int> p = q.top();
-            q.pop();
-            int node = p.second;
-            int dis = p.first;
-
-            for (auto it : adj[node]) {
-                int adjNode = it.first;
-                int pathCost = it.second;
-
-                if (dis + pathCost < ans[adjNode]) {
-                    q.push({dis + pathCost, adjNode});
-                    ans[adjNode] = dis + pathCost;
-                }
-            }
-        }
-        return ans;
-    }
-
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<pair<int, int>>> adj(n);
+        vector<vector<int>> dist(n, vector<int>(n, 1e8));
         int m = edges.size();
         for (int i = 0; i < m; i++) {
             int u = edges[i][0];
             int v = edges[i][1];
             int wt = edges[i][2];
-
-            adj[u].push_back({v, wt});
-            adj[v].push_back({u, wt});
+            dist[u][v] = wt;
+            dist[v][u] = wt;
         }
 
-        vector<vector<int>> dist(n);
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    // can also be done while initialization
+                    if (i == j) {
+                        dist[i][j] = 0;
+                        continue;
+                    }
 
-        for (int i = 0; i < n; i++) {
-            dist[i] = dijkstra(n, adj, i);
+                    if (dist[i][via] == 1e8 || dist[via][j] == 1e8)
+                        continue;
+                    dist[i][j] = min(dist[i][j], (dist[i][via] + dist[via][j]));
+                }
+            }
         }
+
         int city = 0;
         int minCity = INT_MAX;
         int ans = 0;
@@ -56,7 +40,7 @@ public:
                 minCity = city;
                 ans = i;
             }
-            city=0;
+            city = 0;
         }
         return ans;
     }
