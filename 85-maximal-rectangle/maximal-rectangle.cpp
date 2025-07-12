d699128b8f03f55810cd6d7d5b70fd28d85d61a9
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-07-13 02:01:48 +0530

    Time: 3 ms (86.11%) | Memory: 23 MB (9.09%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,99 +1,70 @@
 class Solution {
-vector<int> next_smaller(vector<int> arr)
-{
-    int n = arr.size();
-    stack<int> st;
-    vector<int> arr1(n);
-    for (int i = n - 1; i >= 0; i--)
-    {
-        while (!st.empty() && arr[st.top()] >= arr[i])
-        {
-            st.pop();
-        }
-
-        if (st.empty())
-        {
-            arr1[i] = n;
-        }
-
-        else
-        {
-            arr1[i] = st.top();
-        }
-
-        st.push(i);
-    }
-
-    return arr1;
-}
-
-vector<int> prev_smaller(vector<int> arr)
-{
-    int n = arr.size();
-    stack<int> st;
-    vector<int> arr1(n);
-    for (int i = 0; i < n; i++)
-    {
-        while (!st.empty() && arr[st.top()] >= arr[i])
-        {
-            st.pop();
-        }
-
-        if (st.empty())
-        {
-            arr1[i] = -1;
-        }
-
-        else
-        {
-            arr1[i] = st.top();
-        }
-
-        st.push(i);
-    }
-
-    return arr1;
-}
-
-int find_area(vector<int> arr)
-{
-    int n = arr.size();
-    vector<int> next = next_smaller(arr);
-    vector<int> prev = prev_smaller(arr);
-
-    int area = 0;
-    int temp;
-    for (int i = 0; i < n; i++)
-    {
-        temp = arr[i] * (next[i] - prev[i] - 1);
-        area = max(area, temp);
-    }
-
-    return area;
-}
-
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
+    vector<int> pse(vector<int> arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!(st.empty()) && (arr[st.top()] >= arr[i])) {
+                st.pop();
+            }
+            if (st.empty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = st.top();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
 
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> temp(m,vector<int>(n));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                    temp[i][j]=matrix[i][j]-'0';
+    vector<int> nse(vector<int> arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!(st.empty()) && (arr[st.top()] >= arr[i])) {
+                st.pop();
             }
-        }
-        int area = find_area(temp[0]);
-        for (int i = 1; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (temp[i][j] ==0) {
-                    temp[i][j] =0;
-                } else {
-                    temp[i][j] += temp[i - 1][j];
-                }
+            if (st.empty()) {
+                ans[i] = n;
+            } else {
+                ans[i] = st.top();
             }
-            area = max(area, find_area(temp[i]));
+            st.push(i);
         }
-        return area;
+        return ans;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> left = pse(heights);
+        vector<int> right = nse(heights);
+
+        int n = heights.size();
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int area = heights[i] * (right[i] - left[i] - 1);
+            maxArea = max(area, maxArea);
+        }
+        return maxArea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> arr(m + 1, 0);
+
+        int maxRec = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1')
+                    arr[j]++;
+                else
+                    arr[j] = 0;
+            }
+            int rec = largestRectangleArea(arr);
+            maxRec = max(maxRec, rec);
+        }
+        return maxRec;
     }
 };
