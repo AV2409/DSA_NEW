559407643ca4d7bb8047e3898584b0d9fb2e8e16
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-07-13 01:56:19 +0530

    Time: 19 ms (74.97%) | Memory: 91.9 MB (10.33%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,71 +1,51 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> next_smaller(vector<int> arr, int n)
-    {
+    vector<int> pse(vector<int> arr) {
+        int n = arr.size();
+        vector<int> ans(n);
         stack<int> st;
-        vector<int> arr1(n);
-        for (int i = n - 1; i >= 0; i--)
-        {
-            while (!st.empty() && arr[st.top()] >= arr[i])
-            {
+        for (int i = 0; i < n; i++) {
+            while (!(st.empty()) && (arr[st.top()] >= arr[i])) {
                 st.pop();
             }
-
-            if (st.empty())
-            {
-                arr1[i] = n;
+            if (st.empty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = st.top();
             }
-
-            else
-            {
-                arr1[i] = st.top();
-            }
-
             st.push(i);
         }
-
-        return arr1;
+        return ans;
     }
 
-    vector<int> prev_smaller(vector<int> arr, int n)
-    {
+    vector<int> nse(vector<int> arr) {
+        int n = arr.size();
+        vector<int> ans(n);
         stack<int> st;
-        vector<int> arr1(n);
-        for (int i = 0; i < n; i++)
-        {
-            while (!st.empty() && arr[st.top()] >= arr[i])
-            {
+        for (int i = n - 1; i >= 0; i--) {
+            while (!(st.empty()) && (arr[st.top()] >= arr[i])) {
                 st.pop();
             }
-
-            if (st.empty())
-            {
-                arr1[i] = -1;
+            if (st.empty()) {
+                ans[i] = n;
+            } else {
+                ans[i] = st.top();
             }
-
-            else
-            {
-                arr1[i] = st.top();
-            }
-
             st.push(i);
         }
-
-        return arr1;
+        return ans;
     }
 
-    int largestRectangleArea(vector<int> &heights)
-    {
-        int n=heights.size();
-        vector<int> next = next_smaller(heights, n);
-        vector<int> prev = prev_smaller(heights, n);
-        int area=0;
-        int temp;
-        for(int i=0;i<n;i++){
-            temp=heights[i]*(next[i]-prev[i]-1);
-            area=max(area,temp);
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> left = pse(heights);
+        vector<int> right = nse(heights);
+
+        int n = heights.size();
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int area = heights[i] * (right[i] - left[i] - 1);
+            maxArea = max(area, maxArea);
         }
-        return area;
+        return maxArea;
     }
 };
