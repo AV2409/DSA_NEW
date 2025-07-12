e94e3fee2c1a3df3888ca5467fb095b4ca95ad94
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-07-12 20:37:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (66.34%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,20 +1,82 @@
-class Solution {
+class DisjointSet {
 public:
-    void dfs_traversal(vector<vector<int>> &isConnected, int node,
-                       vector<int>& visited, int n) {
-        visited[node] = 1;
-        for (int i = 0; i < n; i++) {
-            if ((isConnected[node][i] == 1) && (!visited[i]))
-                dfs_traversal(isConnected, i, visited, n);
+    vector<int> parent;
+    vector<int> size;
+    vector<int> rank;
+
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
         }
     }
+
+    int findUParent(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+
+        return parent[node] = findUParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+
+        if (ulp_u == ulp_v) {
+            return;
+        }
+
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_u] > rank[ulp_v]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+
+        if (ulp_u == ulp_v) {
+            return;
+        }
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> visited(n, 0);
+
+        DisjointSet ds(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j]) {
+                    if (ds.findUParent(i) != ds.findUParent(j)) {
+                        ds.unionByRank(i, j);
+                    }
+                }
+            }
+        }
+
         int cnt = 0;
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs_traversal(isConnected, i, visited, n);
+            if (ds.parent[i] == i) {
                 cnt++;
             }
         }
