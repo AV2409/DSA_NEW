fc363245caa8078f17c15905bab4f25e5a6a7179
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-08-04 23:35:04 +0530

    Time: 26 ms (30.34%) | Memory: 17.5 MB (57.55%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,33 +2,30 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        set<vector<int>> st;
+        vector<vector<int>> ans;
         int n = nums.size();
-        for (int k = 0; k < n; k++) {
-            for (int i = k+1; i < n; i++) {
-                int l = i + 1;
-                int r = n - 1;
+        
+        for (int k = 0; k < n - 3; k++) {
+            if (k > 0 && nums[k] == nums[k - 1]) continue;
+            for (int i = k + 1; i < n - 2; i++) {
+                if (i > k + 1 && nums[i] == nums[i - 1]) continue;
+                int l = i + 1, r = n - 1;
                 while (l < r) {
                     long long sum = (long long)nums[k] + (long long)nums[i] + (long long)nums[l] + (long long)nums[r];
-
                     if (sum == target) {
-                        vector<int> temp = {nums[k],nums[i], nums[l], nums[r]};
-                        st.insert(temp);
-                        while (l < r && nums[l] == nums[l + 1])
-                            l++;
-                        while (l < r && nums[r] == nums[r - 1])
-                            r--;
+                        ans.push_back({nums[k], nums[i], nums[l], nums[r]});
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
                         l++;
                         r--;
                     } else if (sum < target) {
                         l++;
-                    } else if (sum > target) {
+                    } else {
                         r--;
                     }
                 }
             }
         }
-        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
