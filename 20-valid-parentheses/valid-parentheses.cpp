905161370b2f321fa6dae6f7005aea3b3f3c439b
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-08-04 23:51:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -6,8 +6,7 @@ public:
         int valid=true;
         stack<int>st;
         for(int i=0;i<n;i++){
-            char c=s[i];
-            if(c=='('||c=='{'||c=='['){
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
                 st.push(s[i]);
             }
             else{
