195a3f6f317c3717c0e9099cf5c78162d531e3ff
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-08-04 23:50:57 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (5.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<int> s1;
-    int n = s.length();
-    int i = 0;
-    if ((s[0] == ')') || (s[0] == '}') || (s[0] == ']')){
-          return 0;
-      }
-    while (i < n) {
-      
-        if ((s[i] == '(') || (s[i] == '{') || (s[i] == '['))
-        {
-            s1.push(s[i]);
-        }
-        else {
-            if (s1.empty()) {
-                return false; // If there's no opening bracket for a closing one
-            }
-            char top = s1.top();
-            if ((s[i] == ')' && top == '(') ||
-                (s[i] == '}' && top == '{') ||
-                (s[i] == ']' && top == '[')) {
-                s1.pop(); // Matching pair found, pop the stack
-            } else {
-                return false; // Mismatched bracket
-            }
-        }
 
-        i++;
-    }
-    if (s1.empty())
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    bool isValid(string s) {
+        int n=s.size();
+        int valid=true;
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            char c=s[i];
+            if(c=='('||c=='{'||c=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                char c=st.top();
+                if(s[i]==')'&&c=='(') st.pop();
+                else if(s[i]==']'&&c=='[') st.pop();
+                else if(s[i]=='}'&&c=='{') st.pop();
+                else break;
+            }
+        }
+        return st.empty();
     }
 };
