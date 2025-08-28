3e7014eac6063e0fb46234cb5ec8b73174f2c09c
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-08-28 09:53:17 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (94.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,63 +1,38 @@
 class Solution {
-private:
-    int get_pivot(vector<int> v) {
-        int size = v.size();
-        int st = 0;
-        int end = size - 1;
-        int mid = st + (end - st) / 2;
-
-        while (st < end) {
-
-            if (v[st] <= v[end])
-                return st;
-
-            if (v[mid] >= v[0])
-                st = mid + 1;
-
-            else
-                end = mid;
-
-            mid = st + (end - st) / 2;
-        }
-        return st;
-    }
-
-    int solve(vector<int>& v, int target, int st, int end) {
-        int mid = st + (end - st) / 2;
-
-        while (st <= end) {
-
-            if (v[mid] == target)
-                return mid;
-
-            else if (v[mid] > target)
-                end = mid - 1;
-
-            else
-                st = mid + 1;
-
-            mid = st + (end - st) / 2;
-        }
-        return -1;
-    }
-
 public:
     int search(vector<int>& nums, int target) {
-
-        int st = 0;
-        int end = nums.size() - 1;
-        int pivot = get_pivot(nums);
-        if (nums[pivot] == target)
-            return pivot;
-
-        if (pivot==0)
-            return solve(nums, target, st, end);
-        if (target >= nums[st] && target <= nums[pivot - 1])
-            return solve(nums, target, st, pivot - 1);
-
-        else
-            return solve(nums, target, pivot, end);
-
+    int n = nums.size();
+    if (nums[0] > target && nums[n - 1] < target) {
         return -1;
     }
+
+    int r = n - 1;
+    int l=0;
+    
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(nums[mid]==target) return mid;
+        
+        //left half sorted
+        if(nums[l]<=nums[mid]){
+            if(nums[l]<=target&&target<=nums[mid]){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+    
+        //right half sorted 
+        else{
+            if(nums[mid]<=target&&target<=nums[r]){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+    }
+    return -1;
+}
 };
