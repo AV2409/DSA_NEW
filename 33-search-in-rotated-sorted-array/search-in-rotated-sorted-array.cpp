04c5f4cc77b0a11a3d84fbc8aa244cb36ba92f52
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-08-28 10:02:38 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (94.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,38 +1,54 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+int Bsearch(vector<int>& nums, int target,int l,int r) {
+    while (l <= r) {
+        int mid = (l + r) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        else if (nums[mid] > target) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int pivotEle(vector<int>& nums) {
+    int n = nums.size();
+    int r = n - 1;
+    int l = 0;
+
+    while (l < r) {
+        int mid = (l + r) / 2;
+        if (nums[mid] > nums[r]) {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
+    }
+    return r;
+}
+
+int search(vector<int>& nums, int target) {
     int n = nums.size();
     if (nums[0] > target && nums[n - 1] < target) {
         return -1;
     }
 
     int r = n - 1;
-    int l=0;
-    
-    while(l<=r){
-        int mid=(l+r)/2;
-        if(nums[mid]==target) return mid;
-        
-        //left half sorted
-        if(nums[l]<=nums[mid]){
-            if(nums[l]<=target&&target<=nums[mid]){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-    
-        //right half sorted 
-        else{
-            if(nums[mid]<=target&&target<=nums[r]){
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
-            }
-        }
+    int l = 0;
+    int pvt=pivotEle(nums);
+    int ans=-1;
+    if(target>nums[r]){
+        ans=Bsearch(nums,target,0,pvt-1);
     }
-    return -1;
+    else{
+        ans=Bsearch(nums,target,pvt,r);
+    }
+    return ans;
 }
 };
