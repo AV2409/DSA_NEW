8676f31eb2cbe49d5252e531000bfa15995818a4
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2025-09-09 22:24:05 +0530

    Time: 16 ms (10.48%) | Memory: 140.1 MB (16.66%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -4,38 +4,25 @@ public:
         int n = nums.size();
         int i = 0;
         int j = 0;
-
-        if(nums[0] > 0) {
-            for (j = 0; j < n; j++) {
-                if (nums[j] < 0)
-                    break;
-            }
-        }
-        else {
-            for (i = 0; i < n; i++) {
-                if (nums[i] > 0)
-                    break;
-            }
+        queue<int> pos;
+        queue<int> neg; 
+        vector<int>ans;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0)
+                pos.push(nums[i]);
+            else
+                neg.push(nums[i]);
         }
 
-        bool flag = 1;
-        vector<int> ans;
-        while (i < n && j < n) {
-            if (flag) {
-                ans.push_back(nums[i++]);
-                while (i < n && nums[i] < 0)
-                    i++;
+        for (int i = 0; i < n; i++) {
+            if (i%2==0) {
+                ans.push_back(pos.front());
+                pos.pop();
             } else {
-                ans.push_back(nums[j++]);
-                while (j < n && nums[j] > 0)
-                    j++;
+                ans.push_back(neg.front());
+                neg.pop();
             }
-            flag = !flag;
         }
-        if (i < n)
-            ans.push_back(nums[i]);
-        else
-            ans.push_back(nums[j]);
         return ans;
     }
 };
