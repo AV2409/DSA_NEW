9f416a6ce6b67d4d8167a03bc95198241b277770
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-01-30 19:08:52 +0530

    Time: 365 ms (5.00%) | Memory: 552.5 MB (5.04%) - LeetSync

---
+++ 4171-rotate-non-negative-elements/rotate-non-negative-elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> rotateElements(vector<int>& nums, int k) {
+        queue<int>q;
+        int n=nums.size();
+        vector<bool>z(n);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0){
+                q.push(nums[i]);
+                z[i]=true;
+                cnt++;
+            }
+        }
+
+        if(cnt==0) return nums;
+
+        while(k--){
+            int temp=q.front();
+            q.pop();
+            q.push(temp);
+        }
+
+        for(int i=0;i<n;i++){
+            if(z[i]){
+                nums[i]=q.front();
+                q.pop();
+            }
+        }
+        return nums;
+    }
+};
