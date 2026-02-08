563cd5e7f76b6fbbf7dbc5e59ebd09333ae27736
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-02-08 19:06:32 +0530

    Time: 3 ms (53.96%) | Memory: 36.5 MB (42.85%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,25 +1,56 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int, int>> pq;
+        int n = arr.size();
+        int lo = 0;
+        int hi = n - 1;
+        int mid;
+        int idx = -1;
+        pair<int, int> bestDiff = {INT_MAX, INT_MAX};
+        while (lo <= hi) {
+            mid = (lo + hi) / 2;
+            int diff = abs(arr[mid] - x);
 
-        for (int num : arr) {
-            int temp = abs(num - x);
-            if (pq.size() == k) {
-                if (pq.top().first > temp) {
-                    pq.pop();
-                    pq.push({abs(num - x), num});
-                }
-            } else
-                pq.push({abs(num - x), num});
+            if (diff < bestDiff.first ||
+                (diff == bestDiff.first && arr[mid] < bestDiff.second)) {
+                bestDiff = {diff, arr[mid]};
+                idx = mid;
+            }
+
+            if (arr[mid] < x)
+                lo = mid + 1;
+
+            else
+                hi = mid - 1;
         }
 
+        int i = idx;
+        int j = idx + 1;
         vector<int> ans;
-        while (!pq.empty()) {
-            ans.push_back(pq.top().second);
-            pq.pop();
+        while (k && i >= 0 && j < n) {
+            if (abs(arr[i] - x) <= abs(arr[j] - x)) {
+                ans.push_back(arr[i]);
+                i--;
+            }
+
+            else {
+                ans.push_back(arr[j]);
+                j++;
+            }
+            k--;
         }
-        sort(ans.begin(),ans.end());
+
+        while (k && i >= 0) {
+            ans.push_back(arr[i--]);
+            k--;
+        }
+
+        while (k && j < n) {
+            ans.push_back(arr[j++]);
+            k--;
+        }
+        sort(ans.begin(), ans.end());
+
         return ans;
     }
 };
