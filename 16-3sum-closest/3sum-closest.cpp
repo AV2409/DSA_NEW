9cf5f1377b0c47af3c94e260da3c7433bb497f37
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-22 13:01:29 +0530

    Time: 14 ms (35.38%) | Memory: 13.9 MB (94.75%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -6,28 +6,22 @@ public:
         int ans = 1e9;
         for (int i = 0; i < n - 2; i++) {
 
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             int l = i + 1, r = n - 1;
-            
+
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
+                if (abs(target - sum) < abs(target - ans)) {
+                    ans = sum;
+                }
+
                 if (sum == target)
                     return sum;
 
-                else if (sum < target) {
+                else if (sum < target) l++;
 
-                    if (abs(target - sum) < abs(target - ans)) {
-                        ans = sum;
-                    }
-                    l++;
-                }
-
-                else {
-                    if (abs(target - sum) < abs(target - ans)) {
-                        ans = sum;
-                    }
-                    r--;
-                }
+                else r--;
             }
         }
         return ans;
