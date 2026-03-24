edd4167e1aae7846240d313b8224cfcb5f964ce4
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-24 19:32:48 +0530

    Time: 3 ms (30.18%) | Memory: 23.1 MB (5.89%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,34 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        if (n == 1)
-            return intervals;
-        sort(intervals.begin(), intervals.end());
-
-        int i = 0;
-        int j = 1;
-
-        vector<vector<int>> ans;
-        int st1 = intervals[i][0];
-        int end1 = intervals[i][1];
-
-        while (j < n) {
-            int st2 = intervals[j][0];
-            int end2 = intervals[j][1];
-            if (end1 < st2) {
-                ans.push_back({st1, end1});
-                st1 = st2;
-                end1 = end2;
-            } else {
-                end1 = max(end1, end2);
-            }
-            j++;
-        }
-
-        ans.push_back({st1, end1});
-        return ans;
-    }
+    
     vector<vector<int>> insert(vector<vector<int>>& intervals,
                                vector<int>& newInterval) {
         int n = intervals.size();
@@ -60,6 +32,32 @@ public:
             i++;
         }
 
-        return merge(i1);
+        n = i1.size();
+        if (n == 1)
+            return intervals;
+        sort(intervals.begin(), intervals.end());
+
+        i = 0;
+        int j = 1;
+
+        vector<vector<int>> ans;
+        int st1 = i1[i][0];
+        int end1 = i1[i][1];
+
+        while (j < n) {
+            int st2 = i1[j][0];
+            int end2 = i1[j][1];
+            if (end1 < st2) {
+                ans.push_back({st1, end1});
+                st1 = st2;
+                end1 = end2;
+            } else {
+                end1 = max(end1, end2);
+            }
+            j++;
+        }
+
+        ans.push_back({st1, end1});
+        return ans;
     }
 };
