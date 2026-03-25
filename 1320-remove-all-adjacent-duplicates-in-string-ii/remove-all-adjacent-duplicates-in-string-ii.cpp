57482767bf952923ee87e94e517a30d3d34649a3
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-25 10:54:25 +0530

    Time: 15 ms (35.93%) | Memory: 14 MB (61.61%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        stack<pair<char,int>>st;
+        stack<pair<char, int>> st;
 
-        for(char c:s){
-            if(st.empty()||st.top().first!=c) st.push({c,1});
-            else if(st.top().first==c){
-                if(st.top().second>=k-1){
-                    for(int i=0;i<k-1;i++){
-                        st.pop();
-                    }
-                }
-                else{
-                    st.push({c,st.top().second+1});
+        for (char c : s) {
+            if (st.empty() || st.top().first != c)
+                st.push({c, 1});
+            else if (st.top().first == c) {
+                if (st.top().second == k - 1) {
+                    st.pop();
+                } else {
+                    auto temp = st.top();
+                    st.pop();
+                    st.push({c, temp.second + 1});
                 }
             }
         }
 
-        string ans="";
-        while(!st.empty()){
-            ans+=st.top().first;
+        string ans = "";
+        while (!st.empty()) {
+            for (int i = 0; i < st.top().second; i++)
+                ans += st.top().first;
             st.pop();
         }
 
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
