5ae6c7b33bc57256a7f344b2f7ac814e4f70ff61
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-25 10:39:42 +0530

    Time: 8 ms (25.91%) | Memory: 28.5 MB (35.73%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
-
-        vector<int>ans1(2*n);
-        vector<int>arr(2*n);
-        int n1=2*n;
-        for(int i=0;i<n;i++){
-            arr[i]=nums[i];
-            arr[i+n]=nums[i];
-        }
         stack<int>st1;
-        st1.push(arr[n1-1]);
-        ans1[n1-1]=-1;
-        for(int i=n1-2;i>=0;i--){
-            while(!st1.empty()&&st1.top()<=arr[i]) st1.pop();
+        st1.push(nums[n-1]);
+        
 
-            if(st1.empty()) ans1[i]=-1;
-            else ans1[i]=st1.top();
-
-            st1.push(arr[i]);
+        for(int i=n-2;i>=0;i--){
+            while(!st1.empty()&&st1.top()<=nums[i]) st1.pop();
+            st1.push(nums[i]);
         }
-        vector<int> ans(ans1.begin(), ans1.begin()+n);
+        vector<int> ans(n);
+        st1.push(nums[n-1]);
+        for(int i=n-1;i>=0;i--){
+            while(!st1.empty()&&st1.top()<=nums[i]) st1.pop();
 
+            if(st1.empty()) ans[i]=-1;
+            else ans[i]=st1.top();
+
+            st1.push(nums[i]);
+        }
         return ans;
     }
 };
