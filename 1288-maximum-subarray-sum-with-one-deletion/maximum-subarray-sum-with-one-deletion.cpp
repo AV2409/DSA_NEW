b4aafd016d2564104aa393f266adf10d89ebd7b7
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-26 17:56:21 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (51.93%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -3,23 +3,20 @@ public:
     int maximumSum(vector<int>& arr) {
         int n = arr.size();
         int nodelete = arr[0];
-        int onedelete = -1e9;
+        int onedelete = 0;
 
         int ans = arr[0];
 
         for (int i = 1; i < n; i++) {
-            int pnd = nodelete;
-            int pod = onedelete;
 
-            nodelete = max(nodelete + arr[i], arr[i]);
-            int temp;
-            if(pod==-1e9) {
-                pod=arr[i];
-            }
-            else{
-                pod=pod+arr[i];
-            }
-            onedelete = max(pnd, pod);
+            int op1=arr[i];
+            int op2=nodelete+arr[i];
+            int op3=nodelete;//power used
+            int op4=onedelete+arr[i];//power used
+
+
+            nodelete = max(op1,op2);
+            onedelete = max(op3, op4);
 
             ans = max({ans, nodelete, onedelete});
         }
