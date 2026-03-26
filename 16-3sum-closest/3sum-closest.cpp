cb1b1b7015059ea59eb4844b54c6d918e9300873
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-26 12:00:49 +0530

    Time: 19 ms (14.43%) | Memory: 14.1 MB (33.09%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -6,8 +6,8 @@ public:
         int ans = 1e9;
         for (int i = 0; i < n - 2; i++) {
 
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+            // if (i > 0 && nums[i] == nums[i - 1])
+            //     continue;
             int l = i + 1, r = n - 1;
 
             while (l < r) {
