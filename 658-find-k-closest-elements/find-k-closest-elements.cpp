8a9e1f3e1626fb69230da20f31961a8fc152baed
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-27 18:57:00 +0530

    Time: 28 ms (20.56%) | Memory: 38.8 MB (21.00%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,56 +1,24 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n = arr.size();
-        int lo = 0;
-        int hi = n - 1;
-        int mid;
-        int idx = -1;
-        pair<int, int> bestDiff = {INT_MAX, INT_MAX};
-        while (lo <= hi) {
-            mid = (lo + hi) / 2;
-            int diff = abs(arr[mid] - x);
-
-            if (diff < bestDiff.first ||
-                (diff == bestDiff.first && arr[mid] < bestDiff.second)) {
-                bestDiff = {diff, arr[mid]};
-                idx = mid;
-            }
-
-            if (arr[mid] < x)
-                lo = mid + 1;
-
-            else
-                hi = mid - 1;
-        }
-
-        int i = idx;
-        int j = idx + 1;
         vector<int> ans;
-        while (k && i >= 0 && j < n) {
-            if (abs(arr[i] - x) <= abs(arr[j] - x)) {
-                ans.push_back(arr[i]);
-                i--;
+
+        priority_queue<pair<int, int>> pq;
+        for (auto& y : arr) {
+            int diff = abs(x-y);
+
+            pq.push({diff,y});
+
+            if (pq.size() > k) {
+                pq.pop();
             }
-
-            else {
-                ans.push_back(arr[j]);
-                j++;
-            }
-            k--;
         }
 
-        while (k && i >= 0) {
-            ans.push_back(arr[i--]);
-            k--;
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
-
-        while (k && j < n) {
-            ans.push_back(arr[j++]);
-            k--;
-        }
-        sort(ans.begin(), ans.end());
-
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
