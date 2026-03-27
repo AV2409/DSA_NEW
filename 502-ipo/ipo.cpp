b0edebbb08a34371e030a55aef31a89773565c31
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-28 00:30:57 +0530

    Time: 103 ms (27.86%) | Memory: 135.9 MB (10.79%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -3,36 +3,30 @@ public:
     int findMaximizedCapital(int k, int w, vector<int>& profits,
                              vector<int>& capital) {
         priority_queue<pair<int, int>> max_pq;
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            min_pq;
         int n = profits.size();
         int ans = w;
         int tasks = 0;
+        vector<pair<int, int>> gg;
         for (int i = 0; i < n; i++) {
-            min_pq.push({capital[i], profits[i]});
+            gg.push_back({capital[i], profits[i]});
         }
 
-        while (!min_pq.empty() && min_pq.top().first <= ans) {
-            int cap = min_pq.top().first;
-            int pro = min_pq.top().second;
-            max_pq.push({pro, cap});
-            min_pq.pop();
-        }
+        sort(gg.begin(), gg.end());
+        int idx=0;
+        for (int i = 0; i < k; i++) {
 
-        while (!max_pq.empty()) {
+            while (idx<n && gg[idx].first<=ans) {
+                int cap = gg[idx].first;
+                int pro = gg[idx].second;
+                max_pq.push({pro, cap});
+                idx++;
+            }
+            if(max_pq.empty()) return ans;
+
             int c = max_pq.top().second;
             int p = max_pq.top().first;
             ans += p;
-            tasks++;
             max_pq.pop();
-            if(tasks==k) return ans;
-            while (!min_pq.empty() && min_pq.top().first <= ans) {
-                int cap = min_pq.top().first;
-                int pro = min_pq.top().second;
-                max_pq.push({pro, cap});
-                min_pq.pop();
-            }
         }
         return ans;
     }
