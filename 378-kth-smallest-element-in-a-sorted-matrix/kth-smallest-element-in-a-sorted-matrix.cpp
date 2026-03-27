4df983db496661151c65f599ddcdfe16bd402682
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-27 13:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (80.10%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-
-    bool isPossible(vector<vector<int>>& m,int ans,int k){
-        int n=m.size();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(m[i][j]<=ans) cnt++;
-                if(cnt==k) return true;
+    bool isPossible(vector<vector<int>>& m, int ans, int k) {
+        int n = m.size();
+        int j=n-1;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            while (j >= 0 && m[i][j] > ans){
+                j--;
             }
+            cnt+=j+1;
+            if(cnt>=k) return true;
         }
         return false;
     }
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int l=matrix[0][0];
-        int n=matrix.size();
-        int h=matrix[n-1][n-1];
+        int l = matrix[0][0];
+        int n = matrix.size();
+        int h = matrix[n - 1][n - 1];
         int mid;
         int ans;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(isPossible(matrix,mid,k)){
-                ans=mid;
-                h=mid-1;
-            }
-            else l=mid+1;
+        while (l <= h) {
+            int mid = (l + h) / 2;
+            if (isPossible(matrix, mid, k)) {
+                ans = mid;
+                h = mid - 1;
+            } else
+                l = mid + 1;
         }
         return ans;
     }
