46064b5bce3ad36564f601421254aad8ebad0211
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-27 18:01:55 +0530

    Time: 39 ms (31.73%) | Memory: 61.5 MB (49.42%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -5,8 +5,9 @@ public:
 
         for (int num : nums) {
             pq.push(num);
-            if (pq.size() > k)
+            if (pq.size() > k) {
                 pq.pop();
+            }
         }
 
         return pq.top();
