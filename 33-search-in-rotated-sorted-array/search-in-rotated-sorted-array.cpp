7b473f9f32c2b5b3425841a2fd688c7571b29c60
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-27 12:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (34.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,54 +1,52 @@
 class Solution {
 public:
-int Bsearch(vector<int>& nums, int target,int l,int r) {
-    while (l <= r) {
-        int mid = (l + r) / 2;
-
-        if (nums[mid] == target) {
-            return mid;
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int n = nums.size();
+        int ans = -1;
+        int mid;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (nums[mid] >= nums[0])
+                l = mid + 1;
+            else {
+                ans = mid;
+                r = mid - 1;
+            }
         }
-
-        else if (nums[mid] > target) {
-            r = mid - 1;
-        } else {
-            l = mid + 1;
-        }
+        if (ans == -1)
+            return 0;
+        return ans;
     }
-    return -1;
-}
+    int bs(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
 
-int pivotEle(vector<int>& nums) {
-    int n = nums.size();
-    int r = n - 1;
-    int l = 0;
+        while (l <= r) {
+            int mid = l + (r - l) / 2; // avoids overflow
 
-    while (l < r) {
-        int mid = (l + r) / 2;
-        if (nums[mid] > nums[r]) {
-            l = mid + 1;
-        } else {
-            r = mid;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
         }
-    }
-    return r;
-}
 
-int search(vector<int>& nums, int target) {
-    int n = nums.size();
-    if (nums[0] > target && nums[n - 1] < target) {
+        return -1; // not found
+    }
+    int search(vector<int>& nums, int target) {
+        int x = findMin(nums);
+        vector<int> arr1(nums.begin(), nums.begin() + x);
+        vector<int> arr2(nums.begin() + x, nums.end());
+
+        int a1 = bs(arr1, target);
+        int a2 = bs(arr2, target);
+
+        if (a1 != -1)
+            return a1;
+        if (a2 != -1)
+            return a2+x;
         return -1;
     }
-
-    int r = n - 1;
-    int l = 0;
-    int pvt=pivotEle(nums);
-    int ans=-1;
-    if(target>nums[r]){
-        ans=Bsearch(nums,target,0,pvt-1);
-    }
-    else{
-        ans=Bsearch(nums,target,pvt,r);
-    }
-    return ans;
-}
 };
