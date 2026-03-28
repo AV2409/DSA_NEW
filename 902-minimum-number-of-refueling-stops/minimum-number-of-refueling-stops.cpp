2106d125ba00e566f0a0ae0d78ac23ee342ffcf9
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-28 12:21:04 +0530

    Time: 3 ms (37.41%) | Memory: 20.2 MB (46.65%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -6,29 +6,24 @@ public:
         if (target <= startFuel)
             return 0;
         int n = stations.size();
-        priority_queue<pair<int, int>> pq;
+        priority_queue<int> pq;
         int dist = startFuel;
         int idx = 0;
         int ans = 0;
         while (idx < n && stations[idx][0] <= dist) {
-            pq.push({stations[idx][1], stations[idx][0]});
+            pq.push(stations[idx][1]);
             idx++;
         }
 
-        if (pq.empty())
-            return -1;
-
         while (!pq.empty()) {
-            auto temp = pq.top();
+            int fuel = pq.top();
             pq.pop();
-
-            int fuel = temp.first;
             dist += fuel;
             ans++;
             if (dist >= target)
                 return ans;
             while (idx < n && stations[idx][0] <= dist) {
-                pq.push({stations[idx][1], stations[idx][0]});
+                pq.push(stations[idx][1]);
                 idx++;
             }
         }
