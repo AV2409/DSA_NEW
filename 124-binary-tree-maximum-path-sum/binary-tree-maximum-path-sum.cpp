09cd4289045919304543f5f1748382384dff24a5
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-30 12:45:19 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (46.00%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -6,25 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int find(TreeNode* root,int &maxi){
-        if(root==NULL){
+    int helper(TreeNode* root, int& maxi) {
+        if (!root)
             return 0;
-        }
 
-        int l=max(0,find(root->left,maxi));
-        int r=max(0,find(root->right,maxi));
-
-        maxi=max(maxi,root->val+l+r);
-        return root->val+max(l,r);
+        int l = max(0,helper(root->left, maxi));
+        int r = max(0,helper(root->right, maxi));
+        maxi = max(maxi,root->val+ l + r);
+        return root->val + max(l, r);
     }
     int maxPathSum(TreeNode* root) {
-        int maxi=INT_MIN;
-        int x=find(root,maxi);
+        int maxi = -1001;
+        int x = helper(root, maxi);
         return maxi;
     }
 };
