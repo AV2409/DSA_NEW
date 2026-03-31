f6e6585aa4d2fabc1f62feadd06aa8eb22d2dcdf
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-03-31 12:19:32 +0530

    Time: 2 ms (81.07%) | Memory: 11.9 MB (10.21%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,55 +1,42 @@
 class Solution {
 public:
-
-    bool valid(vector<int>&have,vector<int>&needed){
-        for(int i=0;i<256;i++){
-            if(needed[i]>have[i]) return false;
-        }
-        return true;
-    }
     string minWindow(string s, string t) {
-        int n1=s.size();
-        int n2=t.size();
+        int n1 = s.size();
+        int n2 = t.size();
 
-        string ans="";
-        if(n2>n1) return ans;
-        int start=0;
-        int minLen=1e9;
+        if (n2 > n1) return "";
 
-        int l=0;
-        int r=0;
-        vector<int>needed(256);
-        vector<int>have(256);
+        vector<int> needed(256, 0), have(256, 0);
 
-        for(char c:t){
-            needed[c]++;
-        }
-        int cnt=t.size();
+        for (char c : t) needed[c]++;
 
-        for(int r=0;r<n1;r++){
-            if (needed[s[r]] > 0) {
-                have[s[r]]++;
-                if (have[s[r]] <= needed[s[r]]) {
-                    cnt--;
-                }
+        int cnt = t.size();
+        int l = 0, start = 0, minLen = INT_MAX;
+
+        for (int r = 0; r < n1; r++) {
+
+            have[s[r]]++;
+
+            if (needed[s[r]] > 0 && have[s[r]] <= needed[s[r]]) {
+                cnt--;
             }
-            while(cnt==0){
+
+            while (cnt == 0) {
                 if (r - l + 1 < minLen) {
                     minLen = r - l + 1;
                     start = l;
                 }
-                if (needed[s[l]] > 0) {
-                    have[s[l]]--;
-                    if (have[s[l]] < needed[s[l]]) {
-                        cnt++;
-                    }
+
+                have[s[l]]--;
+
+                if (needed[s[l]] > 0 && have[s[l]] < needed[s[l]]) {
+                    cnt++;
                 }
+
                 l++;
             }
         }
 
-        if(minLen==1e9) return ans;
-        string res = s.substr(start, minLen); 
-        return res;
+        return minLen == INT_MAX ? "" : s.substr(start, minLen);
     }
 };
