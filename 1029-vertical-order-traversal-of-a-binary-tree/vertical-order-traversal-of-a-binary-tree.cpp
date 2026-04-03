035582ebdb9ceb01cb366e182d98b24411c77fce
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-03 11:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (83.70%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -6,39 +6,44 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(!root) return ans;
-        
-        queue<pair<TreeNode*,pair<int,int>>>q;
-        q.push({root,{0,0}});
+        vector<vector<int>> ans;
+        if (!root)
+            return ans;
+
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        q.push({root, {0, 0}});
         //{hd,vd,set}
-        map<int, map<int, multiset<int>>> mp;
-        
-        while(!q.empty()){
-            auto [node,p]=q.front();
+        map<int, map<int, vector<int>>> mp;
+
+        while (!q.empty()) {
+            auto [node, p] = q.front();
             q.pop();
-            int hd=p.first;
-            int v=p.second;
-            mp[hd][v].insert(node->val);
-            if(node->left) q.push({node->left,{hd-1,v+1}});
-            if(node->right) q.push({node->right,{hd+1,v+1}});
+            int hd = p.first;
+            int v = p.second;
+            mp[hd][v].push_back(node->val);
+            if (node->left)
+                q.push({node->left, {hd - 1, v + 1}});
+            if (node->right)
+                q.push({node->right, {hd + 1, v + 1}});
         }
-        for(auto it:mp){
-            vector<int>temp;
-            for(auto rows: it.second){
-                for(int x:rows.second){
+        for (auto it : mp) {
+            vector<int> temp;
+            for (auto rows : it.second) {
+                auto vec = rows.second;
+                sort(vec.begin(), vec.end());
+                for (int x : vec) {
                     temp.push_back(x);
                 }
             }
             ans.push_back(temp);
         }
         return ans;
-
     }
 };
