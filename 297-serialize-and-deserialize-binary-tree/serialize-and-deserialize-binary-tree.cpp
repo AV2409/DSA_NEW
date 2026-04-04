90d9a8ec690b947e634c5c83ce1d9c2c37e3578e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-04 09:55:59 +0530

    Time: 26 ms (64.70%) | Memory: 38.5 MB (23.76%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -10,64 +10,57 @@
 class Codec {
 public:
     // Encodes a tree to a single string.
+    void trav(TreeNode* root, string& ans) {
+        if (!root) {
+            ans += "n,";
+            return;
+        }
+        ans += to_string(root->val);
+        ans += ",";
+        trav(root->left, ans);
+        trav(root->right, ans);
+    }
     string serialize(TreeNode* root) {
+
         if (!root)
             return "n";
-
         string ans = "";
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            if (node == NULL) {
-                ans += "n,";
-                continue;
-            }
-
-            ans += to_string(node->val) + ",";
-            q.push(node->left);
-            q.push(node->right);
-        }
-
+        trav(root, ans);
         return ans;
     }
     // Decodes your encoded data to tree.
+    TreeNode* build(vector<string>& vals, int& idx) {
+        if (idx >= vals.size())
+            return NULL;
+        if (vals[idx] == "n") {
+            idx++;
+            return NULL;
+        }
+
+        TreeNode* root = new TreeNode(stoi(vals[idx]));
+        idx++;
+
+        root->left = build(vals, idx);
+        root->right = build(vals, idx);
+
+        return root;
+    }
     TreeNode* deserialize(string data) {
-        if(data=="n") return NULL;
-        vector<string>vals;
-        string temp="";
-        for(char c:data){
-            if(c==','){
+        if (data == "n")
+            return NULL;
+        vector<string> vals;
+        string temp = "";
+        for (char c : data) {
+            if (c == ',') {
                 vals.push_back(temp);
-                temp="";
-            }
-            else{
-                temp+=c;
+                temp = "";
+            } else {
+                temp += c;
             }
         }
 
-        TreeNode* root = new TreeNode(stoi(vals[0]));
-        queue<TreeNode*> q;
-        q.push(root);
-        int i=1;
-        while(!q.empty()){
-            TreeNode* node=q.front();
-            q.pop();
-
-            if(vals[i]!="n"){
-                node->left=new TreeNode(stoi(vals[i]));
-                q.push(node->left);
-            }
-            i++;
-            if(vals[i]!="n"){
-                node->right=new TreeNode(stoi(vals[i]));
-                q.push(node->right);
-            }
-            i++;
-        }
+        int idx = 0;
+        TreeNode* root = build(vals, idx);
         return root;
     }
 };
