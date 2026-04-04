2fa8c9f923316e4d3b8783dfa33c4087fbf466d2
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-04 16:22:26 +0530

    Time: 2 ms (58.49%) | Memory: 37.1 MB (81.86%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -9,35 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class BSTIterator {
+private: 
+    void pushAll1(TreeNode* root){
+        while(root){
+            st1.push(root);
+            root=root->left;
+        }
+    }
+
+    void pushAll2(TreeNode* root){
+        while(root){
+            st2.push(root);
+            root=root->right;
+        }
+    }
+public:
+    stack<TreeNode*>st1;
+    stack<TreeNode*>st2;
+    BSTIterator(TreeNode* root) {
+        pushAll1(root);
+        pushAll2(root);
+    }
+    
+    int nextL() {
+        TreeNode* node=st1.top();
+        st1.pop();
+        pushAll1(node->right);
+        return node->val;
+    }
+    int nextG() {
+        TreeNode* node=st2.top();
+        st2.pop();
+        pushAll2(node->left);
+        return node->val;
+    }
+};
+
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>&ans){
-        if(root==NULL){
-            return;
-        }
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-
-    }
     bool findTarget(TreeNode* root, int k) {
-        vector<int>ans;
-        inorder(root,ans);
-        int l=0;
-        int r=ans.size()-1;
-        int sum=0;
-        while(l<r){
-            sum=ans[l]+ans[r];
-            if(sum==k){
-                return 1;
-            }
-            if(sum<k){
-                l++;
-            }
-            else{
-                r--;
-            }
+        if(!root) return false;
+        BSTIterator it(root);
+
+        int i=it.nextL();
+        int j=it.nextG();
+
+        while(i<j){
+            if(i+j==k) return true;
+
+            if(i+j>k) j=it.nextG();
+            else i=it.nextL();
         }
-        return 0;
+        return false;
     }
 };
