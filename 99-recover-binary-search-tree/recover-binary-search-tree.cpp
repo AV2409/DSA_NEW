db7fadad66889f10d734cdb9d4b2efc871970b05
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-04 17:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 60.7 MB (95.29%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -6,45 +6,50 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* prev=NULL;
-    TreeNode* first;
-    TreeNode* second;
-    int f=0;
-    bool found=false;
+    TreeNode* w11 = NULL;
+    TreeNode* w12 = NULL;
+    TreeNode* w21 = NULL;
+    TreeNode* w22 = NULL;
+    TreeNode* prev = NULL;
+    int f = 0;
 
-    void helper(TreeNode* root){
-        if(found) return;
-        if(!root) return;
+    void find(TreeNode* root) {
+        if (!root)
+            return;
+        if (f == 2)
+            return;
+        
+        find(root->left);
 
-        helper(root->left);
-
-        if(prev==NULL){
-            prev=root;
-        }
-        else{
-            if(root->val<=prev->val){
-                if(f==0){
-                    first=prev;
-                    second=root;
-                    f++;
+        if (prev == NULL) {
+            prev = root;
+        } else {
+            if (root->val <= prev->val) {
+                if (f == 0) {
+                    w11 = prev;
+                    w12 = root;
                 }
-                else{
-                    second=root;
-                    f++;
-                    found=true;
+
+                if (f == 1) {
+                    w21 = prev;
+                    w22 = root;
                 }
+                f++;
             }
             prev=root;
         }
-        helper(root->right);
+        find(root->right);
     }
     void recoverTree(TreeNode* root) {
-        helper(root);
-        swap(first->val,second->val);
+        find(root);
+        if(!w21) swap(w11->val,w12->val);
+        else swap(w11->val,w22->val);
+
     }
 };
