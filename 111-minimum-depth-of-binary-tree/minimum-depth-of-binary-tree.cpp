24d2f2a61174473b01bb43925993ea900cd79399
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-05 10:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 146.9 MB (47.74%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -12,27 +12,28 @@
  */
 class Solution {
 public:
-    int helper(TreeNode* root) {
-        if (root == NULL)
-            return 0;
-
-        if (root->left == NULL && root->right == NULL) {
-            return 1;
-        } 
-
-        if (root->left == NULL){
-            return 1+helper(root->right);
-        }
-
-        if (root->right == NULL){
-            return 1+helper(root->left);
-        }
-
-        return 1+min(helper(root->left),helper(root->right));
-
-    }
     int minDepth(TreeNode* root) {
-        if(!root) return 0;
-        return helper(root);
+        int ans=0;
+        if (!root)
+            return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool flag=false;
+        while (!q.empty()) {
+            int x = q.size();
+            for (int i = 0; i < x; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if(node->left==NULL && node->right==NULL) flag=true;
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+            ans++;
+            if(flag) break;
+        }
+        return ans;
     }
 };
