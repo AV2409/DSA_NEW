706ae062d6b9c819a687c94ed704b43eed333181
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-05 11:33:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (74.28%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -13,26 +13,29 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*,long long>> q;
-        q.push({root,0});
-        long long maxi = -1;
+        queue<pair<TreeNode*, long long>> q;
+        q.push({root, 0});
+        long long ans=0;
+
         while (!q.empty()) {
             int s = q.size();
             long long l = 0;
             long long r = 0;
-            long long mini=q.front().second;
+            int mini=q.front().second;
             for (int i = 0; i < s; i++) {
-                auto [node,idx] = q.front();
+                auto [node, idx] = q.front();
                 q.pop();
                 idx-=mini;
-                if(i==0) l=idx;
-                r=idx;
-                if(node->left) q.push({node->left,(2*idx+1)});
-                if(node->right) q.push({node->right,(2*idx+2)});
-                
+                if (node->left)
+                    q.push({node->left, 2 * idx + 1});
+                if (node->right)
+                    q.push({node->right, 2 * idx + 2});
+                if (i == 0)
+                    l = idx;
+                r =idx;
             }
-            maxi = max(maxi, r - l + 1);
+            ans=max(ans,r-l+1);
         }
-        return maxi;
+        return ans;
     }
 };
