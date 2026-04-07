7a88a539a499adce44e71e188464fa6c904d7ddc
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-07 13:43:51 +0530

    Time: 16 ms (10.34%) | Memory: 20.1 MB (10.26%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,57 +1,41 @@
 class Solution {
 public:
-    int n; // make n global inside class
-
-    int getId(int node, int layer) {
-        return node + layer * n;
-    }
-
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        this->n = n;
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto it:flights){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
 
-        int totalNodes = n * (k + 2);
-        vector<vector<pair<int,int>>> adj(totalNodes);
-
-        // 🔗 Build layered graph
-        for (auto &it : flights) {
-            int u = it[0], v = it[1], w = it[2];
-
-            for (int layer = 0; layer <= k; layer++) {
-                int from = getId(u, layer);
-                int to   = getId(v, layer + 1);
-
-                adj[from].push_back({to, w});
-            }
+            adj[u].push_back({v,wt});
         }
 
-        // 🚀 Dijkstra
-        vector<int> dist(totalNodes, 1e9);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        vector<vector<int>>dist(k+2,vector<int>(n,1e9));;
+        dist[0][src]=0;
 
-        int srcId = getId(src, 0);
-        dist[srcId] = 0;
-        pq.push({0, srcId});
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+        // {dist,node,stops}
+        pq.push({0,src,0});
 
-        while (!pq.empty()) {
-            auto [cost, node] = pq.top();
+        while(!pq.empty()){
+            auto [d,node,st]=pq.top();
             pq.pop();
 
-            if (cost > dist[node]) continue;
+            if (st > k) continue;
+            // if(node==dst) return d;
 
-            for (auto [nei, wt] : adj[node]) {
-                if (cost + wt < dist[nei]) {
-                    dist[nei] = cost + wt;
-                    pq.push({dist[nei], nei});
+            for(auto [nei,wt]:adj[node]){
+                if(wt+d<dist[st+1][nei]){
+                    dist[st+1][nei]=wt+d;
+                    pq.push({dist[st+1][nei], nei, st+1});
                 }
             }
         }
-
-        // 🎯 Extract answer
-        int ans = 1e9;
-        for (int layer = 0; layer <= k+1; layer++) {
-            ans = min(ans, dist[getId(dst, layer)]);
+        int ans=1e9;
+        for(int i=0;i<k+2;i++){
+            ans=min(ans,dist[i][dst]);
         }
-
-        return ans == 1e9 ? -1 : ans;
+        if(ans==1e9) return -1;
+        return ans;
     }
 };
