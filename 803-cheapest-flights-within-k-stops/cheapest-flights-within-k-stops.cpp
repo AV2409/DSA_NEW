621c65219669c560eabbafd0718c1e2b65eafd72
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-07 11:24:27 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (23.42%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-                          int k) {
-        int m = flights.size();
-        vector<vector<pair<int, int>>> adj(n);
-        for (int i = 0; i < m; i++) {
-            int u = flights[i][0];
-            int v = flights[i][1];
-            int cost = flights[i][2];
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto it:flights){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
 
-            adj[u].push_back({v, cost});
+            adj[u].push_back({v,wt});
         }
 
-        //{stops,{node,stops}}
+        vector<int>dist(n+1,1e9);
 
-        vector<int> dist(n, 1e9);
-        dist[src] = 0;
-        queue<pair<int, pair<int, int>>> q;
-        q.push({0, {src, 0}});
-
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            int stops = it.first;
-            int node = it.second.first;
-            int d = it.second.second;
-
-            if (stops <= k) {
-                for (auto it : adj[node]) {
-                    int adjNode = it.first;
-                    int pathCost = it.second;
-
-                    if (pathCost + d < dist[adjNode]) {
-                        dist[adjNode] = pathCost + d;
-                        q.push({stops + 1, {adjNode, dist[adjNode]}});
-                    }
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+        //{stops,dist,src}
+        pq.push({0,0,src});
+        dist[src]=0;
+        while(!pq.empty()){
+            auto [st,d,node]=pq.top();
+            pq.pop();
+            if(st>k) continue;
+            // if(node==dst) return d;
+            for(auto [nei,wt]:adj[node]){
+                if(d+wt<dist[nei]){
+                    dist[nei]=d+wt;
+                    pq.push({st+1,dist[nei],nei});
                 }
             }
         }
-        if (dist[dst] == 1e9) {
-            return -1;
-        }
+        if(dist[dst]==1e9) return -1;
         return dist[dst];
+
     }
 };
