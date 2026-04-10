0d51dfec71f028b8244a5ec52a376db6993a3a90
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-10 16:00:11 +0530

    Time: 71 ms (19.59%) | Memory: 15.9 MB (51.13%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -7,28 +7,32 @@ public:
         vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1));
         dp[0][0] = true;
 
-        for(int i=1;i<=n2;i++){
-            if(p[i-1]=='*') dp[0][i]=dp[0][i-1];
+        for (int i = 1; i <= n2; i++) {
+            bool flag = true;
+            for (int ii = 0; ii < i; ii++) {
+                if (p[ii] != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][i] = flag;
         }
 
         for (int i = 1; i <= n1; i++) {
             for (int j = 1; j <= n2; j++) {
-                if(p[j-1]=='*'||p[j-1]=='?'){
-                    if(p[j-1]=='?'){
-                        dp[i][j]=dp[i-1][j-1];
+                if (p[j - 1] == '*' || p[j - 1] == '?') {
+                    if (p[j - 1] == '?') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        bool op1 = dp[i - 1][j];
+                        bool op2 = dp[i][j - 1];
+                        dp[i][j] = op1 || op2;
                     }
-                    else{
-                        bool op1=dp[i-1][j];
-                        bool op2=dp[i][j-1];
-                        dp[i][j]=op1||op2;
-                    }
-                }
-                else{
-                    if(p[j-1]==s[i-1]){
-                        dp[i][j]=dp[i-1][j-1];
-                    }
-                    else{
-                        dp[i][j]=false;
+                } else {
+                    if (p[j - 1] == s[i - 1]) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = false;
                     }
                 }
             }
