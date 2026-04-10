ad242f4c49ac932bb26d7e742bea318f123f715f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-10 12:49:44 +0530

    Time: 57 ms (15.72%) | Memory: 53.1 MB (11.86%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,37 +2,23 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 1e9));
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
 
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = 0;
+        for(int i=1;i<=amount;i++){
+            dp[0][i]=1e9;
         }
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                int np = dp[i - 1][j];
-                int p = 1e9;
-                if (coins[i - 1] <= j) {
-                    p = 1 + dp[i][j - coins[i - 1]];
-                }
-                dp[i][j] = min(p, np);
+            for (int x = 1; x <= amount; x++) {
+                int op1 = dp[i - 1][x]; // not pick
+                int op2 = 1e9;
+
+                if (x - coins[i - 1] >= 0)
+                    op2 = 1+dp[i][x - coins[i - 1]];
+
+                dp[i][x] = min(op1 , op2);
             }
         }
 
-        // for (auto i : dp)
-        // {
-        //     for (auto j : i)
-        //     {
-        //         if (j == 1e9)
-        //             cout << "i" << "\t";
-        //         else
-        //             cout << j << "\t";
-        //     }
-        //     cout << endl;
-        // }
-
-        if (dp[n][amount] == 1e9) {
-            return -1;
-        }
-        return dp[n][amount];
+        return (dp[n][amount] >= 1e9 ? -1 : dp[n][amount]);
     }
 };
