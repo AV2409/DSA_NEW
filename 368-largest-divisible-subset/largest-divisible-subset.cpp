2a2f4e7052db6219e0744e110eae38d6824ebdbe
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-12 13:15:05 +0530

    Time: 11 ms (63.19%) | Memory: 12.6 MB (47.07%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -9,7 +9,7 @@ public:
         int st=0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if ((nums[i] % nums[j]==0)||(nums[j] % nums[i]==0))
+                if (nums[i] % nums[j]==0)
                     if (dp[j] + 1 > dp[i]) {
                         dp[i] = 1 + dp[j];
                         parent[i] = j;
