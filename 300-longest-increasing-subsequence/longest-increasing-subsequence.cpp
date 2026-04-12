8e4ecf03bdb1f57501ff195716092db9c7e3e229
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-12 12:39:19 +0530

    Time: 277 ms (28.49%) | Memory: 296.4 MB (22.06%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    
-    int f(int idx,int prev,vector<int>& nums,vector<vector<int>>&dp){
-        if(idx==nums.size()) return 0;
-        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        int op1=f(idx+1,prev,nums,dp);
-        int op2=0;
-        if(prev==-1 || nums[idx]>nums[prev]) op2=1+f(idx+1,idx,nums,dp);
-
-        return dp[idx][prev+1]=max(op1,op2);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        f(0,-1,nums,dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= -1; j--) {
+                int op1 = dp[i + 1][j+1];
+                int op2 = 0;
+                if (j == -1 || nums[i] > nums[j])
+                    op2 = 1 + dp[i + 1][i+1];
+                
+                dp[i][j + 1] = max(op1, op2);
+            }
+        }
         return dp[0][0];
     }
 };
