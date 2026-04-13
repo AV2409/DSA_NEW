259825db27de6c0310a88010dc7908a96e5a99fc
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-13 17:48:30 +0530

    Time: 1528 ms (28.62%) | Memory: 9.2 MB (88.07%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    bool isPal(int i,int j,string &s){
-        while(i<j){
-            if(s[i]!=s[j]) return false;
+    bool isPal(int i, int j, string& s) {
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
             i++;
             j--;
         }
         return true;
     }
-    int f(int i,string &str,int n,vector<int>&dp){
-        if(i==n) return 0;
-        int mini=2000;
-        if(dp[i]!=-1) return dp[i];
-        for(int j=i;j<n;j++){
-            if(isPal(i,j,str)){
-                int cc=1+f(j+1,str,n,dp);
-                mini=min(cc,mini);
-            }
-        }
-        return dp[i]= mini;
-
-    }
     int minCut(string s) {
-        int n=s.size();
-        vector<int>dp(n,-1);
-        return f(0,s,n,dp)-1;
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            int mini = 2000;
+            for (int j = i; j < n; j++) {
+                if (isPal(i, j, s)) {
+                    int cc = 1 + dp[j + 1];
+                    mini = min(cc, mini);
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[0] - 1;
     }
 };
