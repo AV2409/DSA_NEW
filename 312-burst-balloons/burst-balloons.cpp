c68e34b6381d289ea2115b5a9871bde0b97c279e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-13 16:29:48 +0530

    Time: 89 ms (84.36%) | Memory: 14 MB (7.25%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int f(int i,int j,vector<int>&arr,vector<vector<int>> &dp){
-        if(abs(i-j)<=1) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int temp=-1;
-        for(int k=i+1;k<j;k++){
-            int cc=arr[i]*arr[k]*arr[j]+(f(i,k,arr,dp)+f(k,j,arr,dp));
-            temp=max(temp,cc);
-        }
-        return dp[i][j]= temp;
-    }
     int maxCoins(vector<int>& nums) {
-        vector<int>arr;
+        vector<int> arr;
         arr.push_back(1);
-        int maxi=0;
-        for(int x:nums){
+        int maxi = 0;
+        for (int x : nums) {
             arr.push_back(x);
-            maxi=max(x,maxi);
+            maxi = max(x, maxi);
         }
 
         arr.push_back(1);
-        int m=arr.size();
-        vector<vector<int>> dp(m, vector<int>(m, -1));
-        return f(0,m-1,arr,dp);
+        int m = arr.size();
+        vector<vector<int>> dp(m, vector<int>(m, 0));
 
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = i + 2; j <= m - 1; j++) {
+                int temp = -1;
+                for (int k = i + 1; k < j; k++) {
+                    int cc = arr[i] * arr[k] * arr[j] +dp[i][k] + dp[k][j];
+                    temp = max(temp, cc);
+                }
+                dp[i][j] = temp;
+            }
+        }
+
+        return dp[0][m-1];
     }
 };
