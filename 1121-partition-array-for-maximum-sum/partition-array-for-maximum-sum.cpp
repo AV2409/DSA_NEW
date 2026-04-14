1d12dcd3e101a84e18c684eb98bb80a877383f70
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-14 12:40:15 +0530

    Time: 2 ms (87.36%) | Memory: 12.5 MB (31.40%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int f(int i, int n, int k, vector<int>& arr,vector<int>& dp) {
-        if(i==n) return 0;
 
-        if(dp[i]!=-1) return dp[i];
-        if (n - i <= k) {
-            int maxe = arr[i];
-            for (int t = i; t < n; t++) {
-                maxe = max(maxe, arr[t]);
-            }
-            return dp[i]= (n - i) * maxe;
-        }
-
-        int ans=0;
-        int maxe = arr[i];
-        for(int t=i;t<i+k;t++){
-            maxe=max(maxe,arr[t]);
-            int temp=maxe*(t-i+1)+f(t+1,n,k,arr,dp);
-            ans=max(ans,temp);
-        }
-        return dp[i]= ans;
-    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int>dp(n+1,-1);
-        return f(0, n, k, arr,dp);
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n-1; i >= 0; i--) {
+            if (n - i <= k) {
+                int maxe = arr[i];
+                for (int t = i; t < n; t++) {
+                    maxe = max(maxe, arr[t]);
+                }
+                dp[i] = (n - i) * maxe;
+                continue;
+            }
+
+            int ans = 0;
+            int maxe = arr[i];
+            for (int t = i; t < i + k; t++) {
+                maxe = max(maxe, arr[t]);
+                int temp = maxe * (t - i + 1) + dp[t + 1];
+                ans = max(ans, temp);
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
     }
 };
