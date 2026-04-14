933ed7c1eee581de9a1011f6d39a8011a9e47dd1
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-14 16:59:36 +0530

    Time: 294 ms (5.07%) | Memory: 32.2 MB (51.65%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.cpp
+++ 923-super-egg-drop/super-egg-drop.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-    int f(int eggs,int floor,vector<vector<int>>&dp){
-        if(eggs==1) return floor;
-        if(floor==0||floor==1) return floor;
-        if(dp[eggs][floor]!=-1) return dp[eggs][floor];
-        int mini=1e9;
-        int low = 1, high = floor;
-        while(low <= high){
-            int mid = (low + high) / 2;
-
-            int op1 = f(eggs - 1, mid - 1, dp);
-            int op2 = f(eggs, floor - mid, dp);
-
-            int temp = 1 + max(op1, op2);
-            mini = min(mini, temp);
-
-            if(op1 > op2)
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return dp[eggs][floor]= mini;
-    }
+    
     int superEggDrop(int k, int n) {
-        vector<vector<int>>dp(k+1,vector<int>(n+1,-1));
-        return f(k,n,dp);
+        int eggs = k;
+        int floors = n;
+        vector<vector<int>> dp(k + 1, vector<int>(n + 1, -1));
+        for (int i = 1; i <= k; i++) {
+            dp[i][0] = 0;
+            dp[i][1] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            dp[1][i] = i;
+            dp[0][i] = 1e9;
+        }
+
+        for (int i = 1; i <= eggs; i++) {
+            for (int j = 1; j <= floors; j++) {
+                int mini = 1e9;
+                int low = 1, high = j;
+                while (low <= high) {
+                    int mid = (low + high) / 2;
+
+                    int op1 = dp[i-1][mid-1];
+                    int op2 = dp[i][j-mid];
+
+                    int temp = 1 + max(op1, op2);
+                    mini = min(mini, temp);
+
+                    if (op1 > op2)
+                        high = mid - 1;
+                    else
+                        low = mid + 1;
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[k][n];
     }
 };
