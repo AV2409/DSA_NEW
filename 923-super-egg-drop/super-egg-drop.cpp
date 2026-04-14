35032ad802344a260289aeca6c4c604f0afec11e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-14 16:59:57 +0530

    Time: 302 ms (5.07%) | Memory: 32.2 MB (45.92%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.cpp
+++ 923-super-egg-drop/super-egg-drop.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     
     int superEggDrop(int k, int n) {
-        int eggs = k;
-        int floors = n;
         vector<vector<int>> dp(k + 1, vector<int>(n + 1, -1));
         for (int i = 1; i <= k; i++) {
             dp[i][0] = 0;
@@ -15,8 +13,8 @@ public:
             dp[0][i] = 1e9;
         }
 
-        for (int i = 1; i <= eggs; i++) {
-            for (int j = 1; j <= floors; j++) {
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
                 int mini = 1e9;
                 int low = 1, high = j;
                 while (low <= high) {
