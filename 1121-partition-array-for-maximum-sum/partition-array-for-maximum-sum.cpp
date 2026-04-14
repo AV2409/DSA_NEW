876c8b8e022a33f4f7efde97d0ced7913df2434f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-14 12:49:26 +0530

    Time: 3 ms (84.01%) | Memory: 12.5 MB (31.40%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -6,18 +6,18 @@ public:
         vector<int> dp(n + 1, 0);
 
         for (int i = n-1; i >= 0; i--) {
-            if (n - i <= k) {
-                int maxe = arr[i];
-                for (int t = i; t < n; t++) {
-                    maxe = max(maxe, arr[t]);
-                }
-                dp[i] = (n - i) * maxe;
-                continue;
-            }
+            // if (n - i <= k) {
+            //     int maxe = arr[i];
+            //     for (int t = i; t < n; t++) {
+            //         maxe = max(maxe, arr[t]);
+            //     }
+            //     dp[i] = (n - i) * maxe;
+            //     continue;
+            // }
 
             int ans = 0;
             int maxe = arr[i];
-            for (int t = i; t < i + k; t++) {
+            for (int t = i; t < min(n,i + k); t++) {
                 maxe = max(maxe, arr[t]);
                 int temp = maxe * (t - i + 1) + dp[t + 1];
                 ans = max(ans, temp);
