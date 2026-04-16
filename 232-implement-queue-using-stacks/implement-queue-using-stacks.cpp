654d8ad9692c52b05d3dd16ae9cfef29169625dc
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-16 19:06:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (69.32%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -7,31 +7,44 @@ stack<int>st2;
     }
     
     void push(int x) {
-        while(!st1.empty()) {
-            st2.push(st1.top());
-            st1.pop();
-        }
         st1.push(x);
-        while(!st2.empty()) {
-            st1.push(st2.top());
-            st2.pop();
-        }
     }
     
     int pop() {
-        if(st1.empty()) return -1;
-        int ele=st1.top();
-        st1.pop();
-        return ele;
+        if(!st2.empty()){
+            int ele=st2.top();
+            st2.pop();
+            return ele;
+        }
+
+        while(!st1.empty()){
+            st2.push(st1.top());
+            st1.pop();
+        }
+        if(!st2.empty()){
+            int ele=st2.top();
+            st2.pop();
+            return ele;
+        }
+        return -1;
     }
     
     int peek() {
-        if(st1.empty()) return -1;
-        return st1.top();
+        if(!st2.empty()){
+            return st2.top();
+        }
+        while(!st1.empty()){
+            st2.push(st1.top());
+            st1.pop();
+        }
+        if(!st2.empty()){
+            return st2.top();
+        }
+        return -1;
     }
     
     bool empty() {
-        return st1.empty();
+        return st1.empty() && st2.empty();
     }
 };
 
