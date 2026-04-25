94740bad9d8eb1e09eb4f9a05e629660a9b9f7bb
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-25 15:37:48 +0530

    Time: 158 ms (18.89%) | Memory: 14.7 MB (20.18%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int f(int n,vector<int>& sq,vector<int>& dp){
+        if(n==0) return 0;
+        if(n<0) return 1e9;
+        if(dp[n]!=-1) return dp[n];
+        int ans=1e9;
+        for(int i=0;i<sq.size();i++){
+            int op=f(n-sq[i],sq,dp);
+            if(op!=1e9) ans=min(ans,1+op);
+        }
+        return dp[n]=ans;
+    }
+    int numSquares(int n) {
+        vector<int>sq;
+        vector<int>dp(n+1,-1);
+        for(int i=1;i*i<=n;i++){
+            sq.push_back(i*i);
+        }
+        return f(n,sq,dp);
+    }
+};
