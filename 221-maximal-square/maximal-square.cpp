68470ef56541f6d01b5190c7b4cf1ce2bc1a4fc0
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-25 23:07:42 +0530

    Time: 9 ms (55.96%) | Memory: 30.9 MB (78.07%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -15,14 +15,6 @@ public:
                 }
             }
         }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
-        }
         return ans*ans;
     }
 };
