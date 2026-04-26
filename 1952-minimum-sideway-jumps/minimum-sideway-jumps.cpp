004a3dc96c9b8776763a56c5d7f6584ed47b838b
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-26 13:34:00 +0530

    Time: 409 ms (19.12%) | Memory: 392.9 MB (8.64%) - LeetSync

---
+++ 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int f(int pt, int lane, vector<int>& obstacles,vector<vector<int>>&dp) {
+        if (pt >= obstacles.size() - 1)
+            return 0;
+        if (obstacles[pt] == lane)
+            return 1e9;
+        
+        if(dp[pt][lane]!=-1) return dp[pt][lane];
+        if (obstacles[pt + 1] != lane)
+            return dp[pt][lane]= f(pt + 1, lane, obstacles,dp);
+        int op2 = 1e9;
+        for (int i = 1; i <= 3; i++) {
+            if (i == lane)
+                continue;
+            op2 = min(op2, 1 + f(pt, i, obstacles,dp));
+        }
+
+        return dp[pt][lane]= op2;
+    }
+    int minSideJumps(vector<int>& obstacles) { 
+        int n=obstacles.size();
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        return f(0, 2, obstacles,dp); 
+        }
+};
