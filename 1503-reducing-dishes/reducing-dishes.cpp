bc6beca9d66489f24ede03f70ca60fc1a20db9ed
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-26 13:58:47 +0530

    Time: 18 ms (29.42%) | Memory: 25.2 MB (5.74%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-
-    int f(int idx,int time,vector<int>& sat,vector<vector<int>>&dp){
-        if(idx==sat.size()) return 0;
-        if(dp[idx][time]!=-1) return dp[idx][time];
-        int take = sat[idx]*time + f(idx+1,time+1,sat,dp);
-
-        int skip = f(idx+1,time,sat,dp);
-
-        return dp[idx][time] = max(take,skip);
-    }
     int maxSatisfaction(vector<int>& satisfaction) {
-        sort(satisfaction.begin(),satisfaction.end());
+
+        sort(satisfaction.begin(), satisfaction.end());
+
         int n=satisfaction.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return f(0,1,satisfaction,dp);
+
+        vector<vector<int>> dp(n+1,vector<int>(n+2,0));
+
+        for(int i=n-1;i>=0;i--){
+            for(int time=n;time>=1;time--){
+                int take=satisfaction[i]*time + dp[i+1][time+1];
+                int skip=dp[i+1][time];
+                dp[i][time]=max(take,skip);
+            }
+        }
+
+        return dp[0][1];
     }
 };
