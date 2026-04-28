ec3757df4cd1ce2d1cf5c71fd7d20938df7abeb8
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-28 18:51:05 +0530

    Time: 35 ms (5.41%) | Memory: 11.4 MB (36.74%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    const int MOD=1e9+7;
-    int f(int n, int k, int target) {
-        if (n == 0 && target == 0)
-            return 1;
-        if (n == 0 || target < 0)
-            return 0;
-        
-        if(dp[n][target]!=-1) return dp[n][target];
-        int ans = 0;
-        for (int i = 1; i <= k; i++) {
-            ans = ((ans%MOD) + (f(n - 1, k, target - i)%MOD))%MOD;
-        }
-        return dp[n][target]= ans;
-    }
+    
+    const int MOD = 1e9 + 7;
+
     int numRollsToTarget(int n, int k, int target) {
-        dp.assign(n + 1, vector<int>(target + 1, -1));
-        return f(n, k, target); 
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+
+        dp[0][0] = 1;
+        for (int ii = 1; ii <= n; ii++) {
+            for (int j = 1; j <= target; j++) {
+                int ans = 0;
+                for (int i = 1; i <= k; i++) {
+                    if (j - i >= 0)
+                        ans =((ans % MOD) + (dp[ii - 1][j - i] % MOD)) % MOD;
+                }
+                dp[ii][j] = ans;
+            }
         }
+        return dp[n][target];
+    }
 };
