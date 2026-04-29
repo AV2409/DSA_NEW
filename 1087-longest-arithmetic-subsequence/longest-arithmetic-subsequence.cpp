70ca7fc4272a9c5659e5275edcf5597b3fa47780
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-04-29 17:12:39 +0530

    Time: 2755 ms (5.94%) | Memory: 242.5 MB (57.34%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int solve(int idx,int diff,vector<int>& nums){
+    
+    int solve(int idx,int diff,vector<int>& nums,vector<unordered_map<int,int>>&dp){
         if(idx<0) return 0;
-        int d=diff+500;
-        if(dp[idx][d]!=-1) return dp[idx][d];
+
+        if(dp[idx].count(diff)) return dp[idx][diff];
         int ans=0;
 
         for(int i=idx-1;i>=0;i--){
             if(nums[idx]-nums[i]==diff){
-                ans=max(ans,1+solve(i,diff,nums));
+                ans=max(ans,1+solve(i,diff,nums,dp));
             }
         }
 
-        return dp[idx][d]= ans;
+        return dp[idx][diff]= ans;
     }
     int longestArithSeqLength(vector<int>& nums) {
         int n=nums.size();
         int ans=0;
-        dp.assign(n,vector<int>(1005,-1));
+        vector<unordered_map<int,int>> dp(n);
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int diff=nums[j]-nums[i];
-                ans=max(ans,2+solve(i,diff,nums));
+                ans=max(ans,2+solve(i,diff,nums,dp));
             }
         }
         return ans;
