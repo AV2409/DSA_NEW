a00f5c3814b9e56feb01ff8a6c90340e23dafd57
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-05-02 19:02:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (48.08%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int n=s.size();
-        if(s[0]==')') return false;
+        stack<int> open, star;
 
-        stack<int>open;
-        stack<int>star;
-
-        for(int i=0;i<n;i++){
-            if(s[i]=='(') open.push(i);
-
-            else if(s[i]==')'){
-                if(!open.empty()) open.pop();
-                else if(!star.empty()) star.pop();
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                open.push(i);
+            } else if (s[i] == '*') {
+                star.push(i);
+            } else {
+                if (!open.empty()) open.pop();
+                else if (!star.empty()) star.pop();
                 else return false;
             }
-
-            else star.push(i);
         }
 
-        while(!open.empty()&&!star.empty()){
-            if(open.top()<star.top()){
+        while (!open.empty() && !star.empty()) {
+            if (open.top() < star.top()) {
                 open.pop();
                 star.pop();
+            } else {
+                return false;
             }
-
-            else return false;
         }
 
-        if(open.empty())  return true;
-        return false;
+        return open.empty();
+
     }
 };
