d08cafd61cef14a8f4402c06ed78ed0bbfba9ffd
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-05-30 12:58:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (36.45%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long helper(long long x, long long n) {
+        if (n == 0)
+            return 1;
+
+        long long tt = helper(x, n / 2);
+        long long res = (tt * tt) % MOD;
+        if (n % 2)
+            res = (res * (x % MOD)) % MOD;
+        return res;
+    }
+    const int MOD = 1e9 + 7;
+    int countGoodNumbers(long long n) {
+        long long pow = n / 2;
+        long long x = helper(20, pow) % MOD;
+        if (n % 2)
+            return (5 * x) % MOD;
+        return x % MOD;
+    }
+};
