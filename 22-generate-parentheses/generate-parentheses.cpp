bbd9247e81a59c3c7b406d357081cbc710ced659
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-05-30 13:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (89.16%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    void helper(int op,int cl,string &str,set<string>&ans){
-        if(op<0||cl<0) return;
-        if(op==0 && cl==0){
-            ans.insert(str);
+
+    void helper(int op,int cl,int n,string &str,vector<string>&ans){
+        if(op==n && cl==n){
+            ans.push_back(str);
             return;
         }
-        if(op==0){
-            str+=')';
-            helper(op,cl-1,str,ans);
+        if (op < n) {
+            str+='(';
+            helper(op + 1, cl, n, str, ans);
             str.pop_back();
         }
-        if(op>cl) return;
-        if(op==cl){
-            str+='(';
-            helper(op-1,cl,str,ans);
-            str.pop_back();
-        }
-        else{
-            str+='(';
-            helper(op-1,cl,str,ans);
-            str.pop_back();
 
+        if (cl < op) {
             str+=')';
-            helper(op,cl-1,str,ans);
+            helper(op, cl + 1, n, str, ans);
             str.pop_back();
         }
     }
     vector<string> generateParenthesis(int n) {
-        int op=n;
-        int cl=n;
+        int op=0;
+        int cl=0;
 
         string str="";
-        set<string>ans;
-        helper(op,cl,str,ans);
-        vector<string>res;
-        for(auto s:ans) res.push_back(s);
-        return res;
+        vector<string>ans;
+        helper(op,cl,n,str,ans);
+        return ans;
     }
 };
