f9b145f377e84f7ac3a81a71b1608695d5129cca
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-05-31 11:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (39.26%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    void f(int k, int n,vector<int>&nums,int i,vector<int>&list,vector<vector<int>>&ans){
+    void f(int k, int n,int i,vector<int>&list,vector<vector<int>>&ans){
         if(k==0 && n==0){
             ans.push_back(list);
             return;
         }
-        if(k==0||n==0||i==9) return;
+        if(k==0||n==0||i==10) return;
         
-        f(k,n,nums,i+1,list,ans);
+        f(k,n,i+1,list,ans);
 
-        list.push_back(nums[i]);
-        f(k-1,n-nums[i],nums,i+1,list,ans);
+        list.push_back(i);
+        f(k-1,n-i,i+1,list,ans);
         list.pop_back();
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>nums(9);
-        for(int i=1;i<=9;i++) nums[i-1]=i;
         vector<int>list;
         vector<vector<int>>ans;
-        f(k,n,nums,0,list,ans);
+        f(k,n,1,list,ans);
         return ans;
     }
 };
