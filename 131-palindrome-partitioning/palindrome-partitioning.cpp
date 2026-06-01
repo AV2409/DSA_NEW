2e9ed1cc2aefb23ef00fe73c1c697a976b421d72
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-01 14:13:45 +0530

    Time: 62 ms (28.08%) | Memory: 79.9 MB (23.35%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -4,7 +4,7 @@ public:
     vector<vector<string>>ans;
 
 
-    bool isPossible(string s,int l,int r){
+    bool isPossible(string &s,int l,int r){
         while(l<r){
             if(s[l]!=s[r]) return false;
             l++;
@@ -12,7 +12,7 @@ public:
         }
         return true;
     }
-    void solve(string s,int i){
+    void solve(string &s,int i){
         if(i==s.size()){
             ans.push_back(lst);
             return;
