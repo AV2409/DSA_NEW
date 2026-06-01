f7ff08c0758eddf390b56aaa9f8818f7db08f18b
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-01 13:13:27 +0530

    Time: 2 ms (72.49%) | Memory: 10.6 MB (32.71%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     vector<int> cols;
-    vector<vector<string>> ans;
-
-    bool isPossible(int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (col == cols[i])
+    vector<vector<int>> ans;
+    bool canPlace(int idx) {
+        int ss = cols.size();
+        for (int i = 0; i < ss; i++) {
+            if (cols[i] == idx)
                 return false;
-            if (abs(row - i) == abs(col - cols[i]))
+            if (abs(idx - cols[i]) == abs(ss - i))
                 return false;
         }
         return true;
     }
-    void helper(int row, int n) {
-        if (row == n) {
-            vector<string> board(n, string(n, '.'));
-            for (int i = 0; i < n; i++) {
-                board[i][cols[i]] = 'Q';
-            }
-            ans.push_back(board);
+    void f(int i, int n) {
+        if (i == n) {
+            ans.push_back(cols);
             return;
         }
 
-        for (int col = 0; col < n; col++) {
-            if (isPossible(row, col)) {
-                cols.push_back(col);
-                helper(row + 1, n);
+        for (int idx = 0; idx < n; idx++) {
+            if (canPlace(idx)) {
+                cols.push_back(idx);
+                f(i + 1, n);
                 cols.pop_back();
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        helper(0,n);
-        return ans;
+        f(0, n);
+        vector<vector<string>> res;
+
+        for (auto& col : ans) {
+            vector<string> board(n, string(n, '.'));
+            for (int row = 0; row < n; row++) {
+                board[row][col[row]] = 'Q';
+            }
+            res.push_back(board);
+        }
+
+        return res;
     }
 };
