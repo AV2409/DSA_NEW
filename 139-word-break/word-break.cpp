ce7028a9683e0290e97a86b9e6168d13c608098a
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-02 18:22:33 +0530

    Time: 19 ms (13.68%) | Memory: 18.6 MB (14.44%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    set<string>dict;
+    vector<int> dp;
+    bool solve(string s,int idx){
+        if(idx==s.size()) return true;
+        if(dp[idx]!=-1) return dp[idx];
+        for(int i=idx;i<s.size();i++){
+            string first=s.substr(idx,i-idx+1);
+            if(dict.count(first)){
+                if(solve(s,i+1)) return dp[idx]=true;
+            }
+        }
+        return dp[idx]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        dp.assign(s.size(),-1);
+        for(string x:wordDict) dict.insert(x);
+        return solve(s,0);
+    }
+};
