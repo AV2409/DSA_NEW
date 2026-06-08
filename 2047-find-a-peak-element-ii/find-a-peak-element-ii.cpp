d8f1c4727d020219ca5fe034dba6c37adcb16c02
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-08 18:27:18 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (99.46%) - LeetSync

---
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findPeak(vector<int>&arr){
+        int n=arr.size();
+        int l=0;
+        int h=n-1;
+
+        while(l<h){
+            int mid=(l+h)/2;
+            int prev=-1;
+            int next=-1;
+            if(mid-1>=0) prev=arr[mid-1];
+            if(mid+1<n) next=arr[mid+1];
+            if(arr[mid]>prev && arr[mid]>next) return mid;
+            else if(arr[mid]>next) h=mid-1;
+            else l=mid+1;
+        }
+        return l;
+    }
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<int>ans(2);
+        for(int i=0;i<m;i++){
+            int col=findPeak(mat[i]);
+            int top=-1;
+            int bottom=-1;
+
+            if(i-1>=0) top=mat[i-1][col];
+            if(i+1<m) top=mat[i+1][col];
+
+            if(mat[i][col]>top && mat[i][col]>bottom){
+                ans={i,col};
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
