b1c6a06f545a201f98b6b9da727eb65d05e3877c
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-08 18:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (99.46%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    int findPeak(vector<int>&arr){
-        int n=arr.size();
-        int l=0;
-        int h=n-1;
-
-        while(l<h){
-            int mid=(l+h)/2;
-            int prev=-1;
-            int next=-1;
-            if(mid-1>=0) prev=arr[mid-1];
-            if(mid+1<n) next=arr[mid+1];
-            if(arr[mid]>prev && arr[mid]>next) return mid;
-            else if(arr[mid]>next) h=mid-1;
-            else l=mid+1;
+    int findMax(vector<int>&arr){
+        int ans=0;
+        int maxi=arr[0];
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]>maxi){
+                maxi=arr[i];
+                ans=i;
+            }
         }
-        return l;
+        return ans;
     }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
         vector<int>ans(2);
-        for(int i=0;i<m;i++){
-            int col=findPeak(mat[i]);
+        int l=0;
+        int h=m-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            int col=findMax(mat[mid]);
             int top=-1;
             int bottom=-1;
 
-            if(i-1>=0) top=mat[i-1][col];
-            if(i+1<m) bottom=mat[i+1][col];
+            if(mid-1>=0) top=mat[mid-1][col];
+            if(mid+1<m) bottom=mat[mid+1][col];
 
-            if(mat[i][col]>top && mat[i][col]>bottom){
-                ans={i,col};
+            if(mat[mid][col]>top && mat[mid][col]>bottom){
+                ans={mid,col};
                 return ans;
             }
+            else if(mat[mid][col]<=top) h=mid-1;
+            else l=mid+1;
         }
         return ans;
     }
