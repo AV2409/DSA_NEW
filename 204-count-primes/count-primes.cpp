fb32240ddba71dc8def59462fb5acd15fcd40e19
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-09 22:38:01 +0530

    Time: 142 ms (92.64%) | Memory: 132.5 MB (14.10%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int countPrimes(int n) {
-        if (n <= 2)
-            return 0; // There are no primes less than 2
+    vector<int> sieve;
 
-        int count = 0;
-        int i = 2;
-        vector<int> prime(n,1);
-        prime[0] = 0;
-        prime[1] = 0;
-        for (int i = 2; i * i < n; i++) {
-            if (prime[i]) {
-                for (int j = i; j * i < n; j++) {
-                    prime[j * i] = 0;
+    void solve(int n) {
+        sieve.assign(n + 1, 1);
+        sieve[0] = sieve[1] = 0;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (sieve[i]) {
+                for (int j = i+i; j <= n; j += i) {
+                    sieve[j] = 0;
                 }
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (prime[i] == 1)
-                count++;
+    }
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+        solve(n - 1);
+        int cnt = 0;
+        for (int i = 2; i < n; i++) {
+            cnt += sieve[i];
         }
-        return count;
+        return cnt;
     }
 };
