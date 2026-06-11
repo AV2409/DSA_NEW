05b7c5a9db4336ded6ce0adb77606e4ae823ccfa
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-11 17:55:14 +0530

    Time: 29 ms (42.85%) | Memory: 66.8 MB (5.05%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        if (n == 0)
-            return tasks.size();
-        unordered_map<char, int> mp;
-        for (char ch : tasks) {
-            mp[ch]++;
-        }
+        unordered_map<int, int> mp;
+        for (char c : tasks)
+            mp[c]++;
         priority_queue<int> pq;
-        stack<int> st;
-
         for (auto it : mp) {
             pq.push(it.second);
         }
 
-        int cnt = 0;
-        while (!pq.empty()) {
-            int temp = pq.top();
-            pq.pop();
-            cnt++;
-            if (temp - 1 > 0)
-                st.push(temp - 1);
-            int x = n;
-            while (!pq.empty() && x--) {
-                int tt = pq.top();
-                pq.pop();
-                cnt++;
-                if (tt - 1 > 0) {
-                    st.push(tt - 1);
-                }
-            }
-            while (!st.empty()) {
-                pq.push(st.top());
-                st.pop();
-            }
+        int ans = 0;
 
-            if (!pq.empty()&&x>0)
-                cnt += x;
+        while (!pq.empty()) {
+            queue<int> q;
+            for (int i = 0; i <= n; i++) {
+                if (pq.empty()) {
+                    if (!q.empty())
+                        ans++;
+                    continue;
+                }
+                int x = pq.top();
+                pq.pop();
+                x--;
+                ans++;
+                if (x > 0)
+                    q.push(x);
+            }
+            while (!q.empty()) {
+                pq.push(q.front());
+                q.pop();
+            }
         }
-        return cnt;
+        return ans;
     }
 };
