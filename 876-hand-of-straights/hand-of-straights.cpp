772ef6e94d7380815f7206e520d7b10cbb8e4716
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-11 18:30:09 +0530

    Time: 43 ms (36.67%) | Memory: 43.1 MB (5.08%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
-bool isNStraightHand(vector<int>& hand, int groupSize) {
-    int n = hand.size();
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if (n % groupSize)
+            return false;
+        // card,cnt
+        unordered_map<int, int> mp;
+        for (int x : hand)
+            mp[x]++;
 
-    if (n % groupSize != 0) return false;
-    map<int, int> mp;
-    for (int i = 0; i < n; i++) {
-        mp[hand[i]]++;
-    }
-
-    while (!mp.empty()) {
-        int ele = mp.begin()->first;
-        if (--mp[ele] == 0) {
-            mp.erase(ele);
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        
+        for(auto it:mp){
+            pq.push({it.first,it.second});
         }
-
-        for (int i = 1; i < groupSize; i++) {
-            int next = ele + i;
-            if (mp.find(next) == mp.end()) {
-                return false;
+        while (!pq.empty()) {
+            int card = pq.top().first;
+            int cnt = pq.top().second;
+            cnt--;
+            queue<pair<int,int>>q;
+            if(cnt>0) q.push({card,cnt});
+            pq.pop();
+            int prev = card;
+            for (int i = 1; i < groupSize; i++) {
+                if(pq.empty()) return false;
+                int nextCard = pq.top().first;
+                if(nextCard!=prev+1) return false;
+                int nextCnt = pq.top().second;
+                nextCnt--;
+                pq.pop();
+                if(nextCnt>0) q.push({nextCard,nextCnt});
+                prev=nextCard;
             }
-            if (--mp[next] == 0) mp.erase(next);
+
+            while(!q.empty()){
+                pq.push(q.front());
+                q.pop();
+            }
         }
+        return true;
     }
-
-    return true;
-}
-
 };
