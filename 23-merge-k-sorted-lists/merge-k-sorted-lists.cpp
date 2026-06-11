e934ce21e57aa4677fba0dd1f8b067a96c2ce599
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-11 17:35:57 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (44.76%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -11,23 +11,23 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* dummy=new ListNode(-1);
+        ListNode* temp=dummy;
+
+        int n=lists.size();
         
-        priority_queue<pair<int,ListNode*>, vector<pair<int,ListNode*>>, greater<pair<int,ListNode*>>> pq;
-        for(ListNode* head:lists){
-            if(head) pq.push({head->val,head});
+        priority_queue<pair<int,ListNode*>, vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>pq;
+        for(auto list:lists){
+            if(list) pq.push({list->val,list});
+        }
+        while(!pq.empty()){
+            ListNode* x=pq.top().second;
+            if(x->next) pq.push({x->next->val,x->next});
+            temp->next=x;
+            pq.pop();
+            temp=temp->next;
         }
 
-        ListNode* res = new ListNode(-1);
-        ListNode* tt = res;
-        while (!pq.empty()) {
-            ListNode* x = pq.top().second;
-            pq.pop();
-            tt->next = x;
-            tt = tt->next;
-            x = x->next;
-            if (x)
-                pq.push({x->val,x});
-        }
-        return res->next;
+        return dummy->next;
     }
 };
