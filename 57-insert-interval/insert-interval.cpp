ea2db7aaef9307af84af4dd7a3580025158f43e2
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-13 18:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (45.63%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        if (n == 1)
-            return intervals;
-        vector<vector<int>> ans;
-
-        int st = intervals[0][0];
-        int end = intervals[0][1];
-
-        int res = 0;
-        for (auto it : intervals) {
-            int s2 = it[0];
-            int e2 = it[1];
-
-            if (s2 <= end) {
-                end = max(end, e2);
-                continue;
-            }
-            ans.push_back({st, end});
-            st = s2;
-            end = e2;
-        }
-        ans.push_back({st, end});
-        return ans;
-    }
     vector<vector<int>> insert(vector<vector<int>>& intervals,
                                vector<int>& newInterval) {
 
-        vector<vector<int>> intervals2;
+        vector<vector<int>> ans;
+
+        int newSt = newInterval[0];
+        int newEnd = newInterval[1];
+
         bool inserted = false;
 
-        for (auto& it : intervals) {
-            if (!inserted && newInterval[0] < it[0]) {
-                intervals2.push_back(newInterval);
-                inserted = true;
+        for(auto &it : intervals) {
+
+            if(it[1] < newSt) {
+                ans.push_back(it);
             }
-            intervals2.push_back(it);
+            else if(it[0] > newEnd) {
+
+                if(!inserted) {
+                    ans.push_back({newSt, newEnd});
+                    inserted = true;
+                }
+
+                ans.push_back(it);
+            }
+            else {
+                newSt = min(newSt, it[0]);
+                newEnd = max(newEnd, it[1]);
+            }
         }
 
-        if (!inserted)
-            intervals2.push_back(newInterval);
+        if(!inserted)
+            ans.push_back({newSt, newEnd});
 
-        return merge(intervals2);
+        return ans;
     }
 };
