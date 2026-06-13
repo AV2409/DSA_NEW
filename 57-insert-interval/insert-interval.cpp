75eb691da23b9183e9f7e292546a3e9622dd4d4e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-13 18:13:46 +0530

    Time: 6 ms (8.12%) | Memory: 23.8 MB (5.33%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
-
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
         if (n == 1)
             return intervals;
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>>ans;
+        vector<vector<int>> ans;
 
-        int st=intervals[0][0];
-        int end=intervals[0][1];
-        
-        int res=0;
-        for(auto it:intervals){
-            int s2=it[0];
-            int e2=it[1];
+        int st = intervals[0][0];
+        int end = intervals[0][1];
 
-            if(s2<=end){
-                end=max(end,e2);
+        int res = 0;
+        for (auto it : intervals) {
+            int s2 = it[0];
+            int e2 = it[1];
+
+            if (s2 <= end) {
+                end = max(end, e2);
                 continue;
             }
-            ans.push_back({st,end});
-            st=s2;
-            end=e2;
+            ans.push_back({st, end});
+            st = s2;
+            end = e2;
         }
-        ans.push_back({st,end});
+        ans.push_back({st, end});
         return ans;
     }
     vector<vector<int>> insert(vector<vector<int>>& intervals,
                                vector<int>& newInterval) {
-        
-        intervals.push_back(newInterval);
-        return merge(intervals);
+
+        vector<vector<int>> intervals2;
+        bool inserted = false;
+
+        for (auto& it : intervals) {
+            if (!inserted && newInterval[0] < it[0]) {
+                intervals2.push_back(newInterval);
+                inserted = true;
+            }
+            intervals2.push_back(it);
+        }
+
+        if (!inserted)
+            intervals2.push_back(newInterval);
+
+        return merge(intervals2);
     }
 };
