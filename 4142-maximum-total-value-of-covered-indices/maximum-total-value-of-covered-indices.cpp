11a7b2fd295872bd90f956f03c7b0681fb56f192
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-16 19:35:09 +0530

    Time: 5 ms (87.88%) | Memory: 190.4 MB (83.56%) - LeetSync

--- 4142-maximum-total-value-of-covered-indices/maximum-total-value-of-covered-indices.cpp
+++ 4142-maximum-total-value-of-covered-indices/maximum-total-value-of-covered-indices.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    vector<vector<long long>> dp;
-
-    long long f(int i, int occupied, vector<int>& nums, string& s) {
-        if (i < 0) return 0;
-        if (dp[i][occupied] != -1)
-            return dp[i][occupied];
-
-        long long ans;
-        if (occupied) {
-            ans = nums[i];
-
-            if (s[i] == '1')
-                ans += f(i - 1, 1, nums, s);
-            else
-                ans += f(i - 1, 0, nums, s);
-        }
-        else {
-            if (s[i] == '0') {
-                ans = f(i - 1, 0, nums, s);
-            }
-            else {
-                long long stay = nums[i] + f(i - 1, 0, nums, s);
-                long long move = f(i - 1, 1, nums, s);
-                ans = max(stay, move);
-            }
-        }
-
-        return dp[i][occupied] = ans;
-    }
     long long maxTotal(vector<int>& nums, string s) {
-
         int n = nums.size();
-        dp.assign(n, vector<long long>(2, -1));
-        int idx = n - 1;
-        return f(idx, 0, nums, s);
+        long long ans = 0;
+        if (s[0] == '1')
+            ans += nums[0];
+        int idx = 1;
+        while (idx < n) {
+            int l = idx;
+            int r = idx;
+            if (s[l] == '1') {
+                long long sum = 0;
+                int mini = nums[l];
+                while (r < n && s[r] == '1') {
+                    mini = min(mini, nums[r]);
+                    sum += nums[r];
+                    r++;
+                }
+
+                if (s[l - 1] == '0') {
+                    sum += nums[l - 1];
+                    mini = min(nums[l - 1], mini);
+                    sum -= mini;
+                    ans += sum;
+                } else {
+                    ans += sum;
+                }
+                idx = r + 1;
+            }
+            else idx++;
+            
+        }
+        return ans;
     }
 };
