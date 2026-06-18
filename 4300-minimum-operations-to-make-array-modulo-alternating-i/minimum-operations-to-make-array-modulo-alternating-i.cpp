e6886638852635656bdb18af74962de2385e90b0
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-18 18:17:49 +0530

    Time: 12 ms (71.40%) | Memory: 30.1 MB (5.18%) - LeetSync

---
+++ 4300-minimum-operations-to-make-array-modulo-alternating-i/minimum-operations-to-make-array-modulo-alternating-i.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] % k;
+        }
+
+        vector<vector<int>> v(n, vector<int>(k));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                int d = abs(j - nums[i]);
+                v[i][j] = min(d, k - d);
+            }
+        }
+
+        vector<int> odd(k, 0);
+        vector<int> even(k, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                if (i % 2 == 0)
+                    even[j] += v[i][j];
+                else
+                    odd[j] += v[i][j];
+            }
+        }
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < k; j++) {
+        //         cout << v[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+
+        // cout << endl << endl;
+        // for (int j = 0; j < k; j++) {
+        //     cout << even[j] << " ";
+        // }
+        // cout << endl;
+        // for (int j = 0; j < k; j++) {
+        //     cout << odd[j] << " ";
+        // }
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                if (i == j)
+                    continue;
+                int temp = even[i] + odd[j];
+                ans = min(ans, temp);
+            }
+        }
+        return ans;
+    }
+};
