0d5990a896fb2481be55baf60713dda5d9f8c140
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-22 14:19:25 +0530

    Time: 6 ms (44.17%) | Memory: 16.1 MB (15.50%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,116 +1,80 @@
 class Solution {
-    private:
-    vector<int> prevse(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> ans(n);
-        stack<int> st;
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] > arr[i]) {
-                st.pop();
-            }
-
-            if (st.empty())
-                ans[i] = -1;
-            else
-                ans[i] = st.top();
-
-            st.push(i);
-        }
-
-        return ans;
-    }
-
-    vector<int> nextse(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> ans(n);
-        stack<int> st;
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
-            }
-
-            if (st.empty())
-                ans[i] = n;
-            else
-                ans[i] = st.top();
-
-            st.push(i);
-        }
-
-        return ans;
-    }
-
-    vector<int> prevge(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> ans(n);
-        stack<int> st;
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] < arr[i]) {
-                st.pop();
-            }
-
-            if (st.empty())
-                ans[i] = -1;
-            else
-                ans[i] = st.top();
-
-            st.push(i);
-        }
-
-        return ans;
-    }
-
-    vector<int> nextge(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> ans(n);
-        stack<int> st;
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] <= arr[i]) {
-                st.pop();
-            }
-
-            if (st.empty())
-                ans[i] = n;
-            else
-                ans[i] = st.top();
-
-            st.push(i);
-        }
-
-        return ans;
-    }
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> pse=prevse(nums);
-        vector<int> nse=nextse(nums);
+    long long sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>prevSmaller(n);
+        vector<int>nextSmaller(n);
 
-        vector<int> pge=prevge(nums);
-        vector<int> nge=nextge(nums);
-        long long mini=0;
-        long long maxi=0;
+        stack<int>st;
         for(int i=0;i<n;i++){
-            long long x;
-            long long y;
-            x=i-pse[i];
-            y=nse[i]-i;
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+            if(st.empty()) prevSmaller[i]=-1;
+            else prevSmaller[i]=st.top();
 
-            long long cnt=x*y*1LL;
-            mini=mini+cnt*nums[i];
-
-            long long a;
-            long long b;
-            a=i-pge[i];
-            b=nge[i]-i;
-
-            long long cnt1=a*b*1LL;
-            maxi=maxi+cnt1*nums[i];
-
+            st.push(i);
         }
+
+        stack<int>st1;
+        for(int i=n-1;i>=0;i--){
+            while(!st1.empty() && arr[st1.top()]>arr[i]){
+                st1.pop();
+            }
+            if(st1.empty()) nextSmaller[i]=n;
+            else nextSmaller[i]=st1.top();
+
+            st1.push(i);
+        }
+        long long ans=0;
+
+        for(int i=0;i<n;i++){
+            int l=i-prevSmaller[i];
+            int r=nextSmaller[i]-i;
+
+            ans = (ans + 1LL * arr[i] * l * r);
+        }
+        return ans;
+    }
+    long long sumSubarrayMaxs(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>prevGreater(n);
+        vector<int>nextGreater(n);
+
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]<=arr[i]){
+                st.pop();
+            }
+            if(st.empty()) prevGreater[i]=-1;
+            else prevGreater[i]=st.top();
+
+            st.push(i);
+        }
+
+        stack<int>st1;
+        for(int i=n-1;i>=0;i--){
+            while(!st1.empty() && arr[st1.top()]<arr[i]){
+                st1.pop();
+            }
+            if(st1.empty()) nextGreater[i]=n;
+            else nextGreater[i]=st1.top();
+
+            st1.push(i);
+        }
+        long long ans=0;
+
+        for(int i=0;i<n;i++){
+            int l=i-prevGreater[i];
+            int r=nextGreater[i]-i;
+
+            ans = (ans + 1LL * arr[i] * l * r);
+        }
+        return ans;
+    }
+    long long subArrayRanges(vector<int>& nums) {
+        long long maxi=sumSubarrayMaxs(nums);
+        long long mini=sumSubarrayMins(nums);
         return maxi-mini;
     }
 };
