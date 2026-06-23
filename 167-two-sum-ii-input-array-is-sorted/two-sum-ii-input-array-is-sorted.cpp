e373f6aaeb08e72c989fb838a2e0ac064660663c
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-23 22:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (32.97%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,58 +1,18 @@
 class Solution {
-
-private:
-    int find(vector<int>& arr, int req, int s,int e){
-        int mid=s+(e-s)/2;
-        while(s<=e){
-            if(arr[mid]==req) return mid;
-            else if(arr[mid]>req) e=mid-1;
-            else s=mid+1;
-
-            mid=s+(e-s)/2;
-        }
-        return -1;
-    }
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-
-        //2Pointer
-        /*
         int i=0;
         int j=numbers.size()-1;
-        int sum;
-        vector<int>ans;
+
         while(i<j){
-            sum=numbers[i]+numbers[j];
-            if(sum==target) {
-                ans.push_back(i+1);
-                ans.push_back(j+1);
-                return ans;
+            int sum=numbers[i]+numbers[j];
+            if(sum==target){
+                break;
             }
-
             else if(sum>target) j--;
-
-            else if(sum<target) i++;
-
+            else i++;
         }
+        vector<int>ans={i+1,j+1};
         return ans;
-        */
-
-        //Binary search
-
-        int i=0;
-        int n=numbers.size();
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            int req=target-numbers[i];
-            int x=find(numbers,req,i+1,n-1);
-            if(x!=-1){
-                ans.push_back(i+1);
-                ans.push_back(x+1);
-                return ans;
-            }
-        }
-        return ans;
-
-        
     }
 };
