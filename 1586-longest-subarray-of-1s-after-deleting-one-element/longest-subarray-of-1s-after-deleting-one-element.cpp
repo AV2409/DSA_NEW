191caecef6f8604e1e4a0dacd040c38e2efc06a2
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-24 13:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 60.7 MB (12.81%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        vector<vector<int>>ans;
-        int n=nums.size();
-        int l=0;
-        int res=0;
-        int z=0;
-        while(l<n){
-            int r=l;
-            while(r<n && nums[r]==0) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int l = 0;
+        int res = 0;
+        int z = 0;
+        while (l < n) {
+            int r = l;
+            while (r < n && nums[r] == 0) {
                 r++;
                 z++;
             }
-            int st=r;
-            if(st==n) break;
-            while(r<n && nums[r]==1) r++;
-            ans.push_back({st,r-1});
-            res=max(res,r-st);
-            l=r;
+            int st = r;
+            if (st == n)
+                break;
+            while (r < n && nums[r] == 1)
+                r++;
+
+            if (nums[st] == 1) {
+                ans.push_back({st, r - 1});
+                res = max(res, r - st);
+            }
+            l = r;
         }
 
-        if(z==0) return n-1;
-        if(z==n) return 0;
+        if (z == 0)
+            return n - 1;
+        if (z == n)
+            return 0;
 
-        for(int i=0;i<ans.size()-1;i++){
-            int st1=ans[i][0];
-            int end1=ans[i][1];
-            int st2=ans[i+1][0];
-            int end2=ans[i+1][1];
+        for (int i = 0; i < ans.size() - 1; i++) {
+            int st1 = ans[i][0];
+            int end1 = ans[i][1];
+            int st2 = ans[i + 1][0];
+            int end2 = ans[i + 1][1];
 
-            if(st2-end1==2) res=max(res,end1-st1+1+end2-st2+1);
+            if (st2 - end1 == 2)
+                res = max(res, end1 - st1 + 1 + end2 - st2 + 1);
         }
         return res;
     }
