5b76e5eb7668e585bee3b7e7b3b08a95ddba207f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-25 18:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (34.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
 
-        int l = 0;
-        int r = nums.size() - 1;
-        int n=r+1;
-        int mid;
-        while(l <= r) {
-            mid=(l+r)/2;
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] > nums[n - 1]) {
-                if (target > nums[mid]) {
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0;
+        int h=n-1;
+
+        while(l<=h){
+            int mid=(l+h)/2;
+            cout<<mid<<" ";
+            if(nums[mid]==target) return mid;
+            // Left half is sorted
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid])
+                    h = mid - 1;
+                else
                     l = mid + 1;
-                } else {
-                    if (target >= nums[0])
-                        r = mid - 1;
-                    else
-                        l = mid + 1;
-                }
             }
-            // 2nd part nums[mid]<=nums[n-1]
+            // Right half is sorted
             else {
-                if (target < nums[mid]) {
-                    r = mid - 1;
-                } else {
-                    if (target > nums[n-1])
-                        r = mid - 1;
-                    else
-                        l = mid + 1;
-                }
+                if (nums[mid] < target && target <= nums[h])
+                    l = mid + 1;
+                else
+                    h = mid - 1;
             }
         }
         return -1;
