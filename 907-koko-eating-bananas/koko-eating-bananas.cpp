95f5d26dcbd9d9c80ff5ce7f575c6e53ddf91547
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-25 18:01:34 +0530

    Time: 3 ms (95.24%) | Memory: 23 MB (46.51%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& piles, int h, int mid) {
-        int cnt=0;
-        int n=piles.size();
-        for(int i=0;i<n;i++){
-            cnt+=piles[i]/mid;
-            if(piles[i]%mid) cnt++;
+    bool isPossible(int mid,vector<int>& piles, int h){
+        int hours=0;
+        for(int i=0;i<piles.size();i++){
+            hours += ceil((double)piles[i] / mid);
+            if(hours>h) return false;
         }
-        return cnt<=h;
+        return hours<=h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1;
-        int r = *max_element(piles.begin(), piles.end());
-        int ans = r;
-        int mid;
+        int l=1;
+        int hi=*max_element(piles.begin(),piles.end());
 
-        while (l <= r) {
-            mid = r-(r-l)/2;
-            if (isPossible(piles, h, mid)) {
-                ans = mid;
-                r = mid - 1;
-            } else
-                l = mid + 1;
+        int ans=l;
+        while(l<=hi){
+            int mid=(l+hi)/2;
+            if(isPossible(mid,piles,h)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
         }
         return ans;
     }
