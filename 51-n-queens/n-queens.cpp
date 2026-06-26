cce991200f39f7bda157f51575f9c993a21af257
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-26 17:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (28.34%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    vector<int> cols;
-    vector<vector<int>> ans;
-    bool canPlace(int idx) {
-        int ss = cols.size();
-        for (int i = 0; i < ss; i++) {
-            if (cols[i] == idx)
-                return false;
-            if (abs(idx - cols[i]) == abs(ss - i))
-                return false;
+    vector<int>cols;
+    int N;
+    vector<vector<string>>ans;
+    
+    void storeAns(){
+        vector<string>temp(N,string(N,'.'));
+        int x=cols.size();
+        for(int r=0;r<x;r++){
+            int c=cols[r];
+            temp[r][c]='Q';
+        }
+        ans.push_back(temp);
+    }
+    bool isPossible(int row,int col){
+        int x=cols.size();
+        for(int r=0;r<x;r++){
+            int c=cols[r];
+            if(c==col||abs(row-r)==abs(col-c)) return false;
         }
         return true;
     }
-    void f(int i, int n) {
-        if (i == n) {
-            ans.push_back(cols);
+    void solve(int row){
+        if(row==N){
+            storeAns();
             return;
         }
 
-        for (int idx = 0; idx < n; idx++) {
-            if (canPlace(idx)) {
-                cols.push_back(idx);
-                f(i + 1, n);
+        for(int col=0;col<N;col++){
+            if(isPossible(row,col)) {
+                cols.push_back(col);
+                solve(row+1);
                 cols.pop_back();
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        f(0, n);
-        vector<vector<string>> res;
-
-        for (auto& col : ans) {
-            vector<string> board(n, string(n, '.'));
-            for (int row = 0; row < n; row++) {
-                board[row][col[row]] = 'Q';
-            }
-            res.push_back(board);
-        }
-
-        return res;
+        N=n;
+        solve(0);
+        return ans;
     }
 };
