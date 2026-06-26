7d37e6b58df42a191fb360dfd375dfd069030f03
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-26 18:11:19 +0530

    Time: 418 ms (24.83%) | Memory: 8.8 MB (37.63%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
 
-    bool isPossible(vector<vector<char>>& board,int i,int j){
+    bool isPossible(vector<vector<char>>& board,int i,int j,char ch){
         int sr=(i/3)*3;
         int sc=(j/3)*3;
 
         for(int r=sr;r<sr+3;r++){
             for(int c=sc;c<sc+3;c++){
-                if(r==i && c==j) continue;
-                if(board[r][c]==board[i][j]) return false;
+                if(board[r][c]==ch) return false;
             }
         }
 
-        for(int x=0;x<9;x++){
-            if(x==i) continue;
-            if(board[x][j]==board[i][j]) return false;
-        }
-
-        for(int x=0;x<9;x++){
-            if(x==j) continue;
-            if(board[i][x]==board[i][j]) return false;
+        for(int k=0;k<9;k++){
+            if(board[i][k]==ch) return false;
+            if(board[k][j]==ch) return false;
         }
         return true;
+
     }
     bool solve(vector<vector<char>>& board,int i,int j){
         if(j==9){
-            j=0;
             i++;
+            j=0;
         }
         if(i==9) return true;
-        if(board[i][j]!='.') {
-            return solve(board,i,j+1);
-        }
-        for(char x='1';x<='9';x++){
-            board[i][j]=x;
-            if(isPossible(board,i,j)){
-                if(solve(board,i,j+1)) return true;
-            }
-            board[i][j]='.';
-        }
 
+        if(board[i][j]!='.') return solve(board,i,j+1);
+
+        for(char ch='1';ch<='9';ch++){
+            if(isPossible(board,i,j,ch)){
+                board[i][j]=ch;
+                if(solve(board,i,j+1)) return true;
+                board[i][j] = '.';
+            }
+        }
         return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
         solve(board,0,0);
+        
     }
 };
