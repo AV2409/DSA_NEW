7c4d34caa11c594f59a8d49808f061847686a8b6
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-26 15:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (75.84%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int r = n - 1;
-        int l = 0;
+        int n=nums.size();
+        int l=0;
+        int h=n-1;
 
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (nums[mid] == target)
-                return true;
-
-            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
-                l++;
-                r--;
-                continue;
-            }
-
-            // left half sorted
-            if (nums[l] <= nums[mid]) {
-                if (nums[l] <= target && target <= nums[mid]) {
-                    r = mid - 1;
-                } else {
+        while(l<=h){
+            int mid=(l+h)/2;
+            
+            if(nums[mid]==target) return true;
+            if(nums[l]==nums[mid]) l++;
+            // Left half is sorted
+            else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid])
+                    h = mid - 1;
+                else
                     l = mid + 1;
-                }
             }
-
-            // right half sorted
+            // Right half is sorted
             else {
-                if (nums[mid] <= target && target <= nums[r]) {
+                if (nums[mid] < target && target <= nums[h])
                     l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
+                else
+                    h = mid - 1;
             }
         }
         return false;
