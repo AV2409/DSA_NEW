3fe084df98c52dd394e0bdbf9028d47458601f4c
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-27 18:41:45 +0530

    Time: 10 ms (43.83%) | Memory: 19 MB (78.15%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,53 +1,37 @@
 class Solution {
 public:
-    int find(vector<int>& heights) {
+    int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         stack<int> st;
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) {
-                int idx = st.top();
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int h = heights[st.top()];
                 st.pop();
-
-                int pse = -1;
-                if (!st.empty())
-                    pse = st.top();
-                int nse = i;
-                int width = nse - pse - 1;
-                ans = max(ans, width * heights[idx]);
+                int left = st.empty() ? -1 : st.top();
+                int width = i - left - 1;
+                ans = max(ans, h * width);
             }
             st.push(i);
-        }
-
-        while (!st.empty()) {
-            int idx = st.top();
-            st.pop();
-
-            int pse = -1;
-            if (!st.empty())
-                pse = st.top();
-            int nse = n;
-            int width = nse - pse - 1;
-            ans = max(ans, width * heights[idx]);
         }
         return ans;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n=matrix[0].size();
-        int m=matrix.size();
-        
-        vector<int>arr(n,0);
-        int ans=0;
-        for(int i=0;i<m;i++){
-            
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]=='1') arr[j]++;
-                else arr[j]=0;
+        int n = matrix[0].size();
+        int m = matrix.size();
+
+        vector<int> arr(n, 0);
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1')
+                    arr[j]++;
+                else
+                    arr[j] = 0;
             }
-            int temp=find(arr);
-            ans=max(ans,temp);
+            int temp = largestRectangleArea(arr);
+            ans = max(ans, temp);
         }
         return ans;
-        
     }
 };
