3c94d6a2362d74eda7f7560655fb0ceeafb7889a
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-27 18:47:58 +0530

    Time: 3 ms (16.94%) | Memory: 27.4 MB (17.27%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size();
-        vector<int>prevM(n);
-        vector<int>nextM(n);
-        prevM[0]=-1;
-        nextM[n-1]=-1;
-        int maxi=height[0];
+        int l = 0;
+        int n = height.size();
+        int prevM = 0;
+        int nextM = 0;
+        int ans = 0;
+
+        vector<int>prevMaxi(n);
+        vector<int>nextMaxi(n);
+        int prev=height[0];
         for(int i=1;i<n;i++){
-            prevM[i]=max(maxi,height[i-1]);
-            maxi=max(maxi,prevM[i]);
-        }
-        maxi=height[n-1];
-
-        for(int i=n-2;i>0;i--){
-            nextM[i]=max(maxi,height[i+1]);
-            maxi=max(maxi,nextM[i]);
+            prevMaxi[i]=prev;
+            prev=max(height[i],prev);
         }
 
-        // for(int i=0;i<n;i++){
-        //     cout<<prevM[i]<<" ";
-        // }
-        // cout<<endl;
+        int next=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            nextMaxi[i]=next;
+            next=max(height[i],next);
+        }
 
-        // for(int i=0;i<n;i++){
-        //     cout<<nextM[i]<<" ";
-        // }
-        int ans=0;
         for(int i=0;i<n;i++){
-            int x=min(prevM[i],nextM[i]);
-            ans+=max(0,x-height[i]);
+            int mini=min(prevMaxi[i],nextMaxi[i]);
+            // if(mini==-1) continue;
+            ans+=max(0,mini-height[i]);
         }
-        return ans;
 
+
+        return ans;
     }
 };
