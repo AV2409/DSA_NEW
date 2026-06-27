724e14f6ad2dc38473b1e79a157521666fb08ee6
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-27 18:39:52 +0530

    Time: 8 ms (96.87%) | Memory: 85.8 MB (24.78%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -5,37 +5,16 @@ public:
         vector<int> ps(n);
         vector<int> ns(n);
         stack<int> st;
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[i] <= heights[st.top()])
-                st.pop();
-            if (st.empty())
-                ps[i] = -1;
-            else
-                ps[i] = st.top();
-
-            st.push(i);
-        }
-
-        while(!st.empty()) st.pop();
-
-        
-        for (int i = n-1; i >= 0; i--) {
-            while (!st.empty() && heights[i] <= heights[st.top()])
-                st.pop();
-            if (st.empty())
-                ns[i] = n;
-            else
-                ns[i] = st.top();
-
-            st.push(i);
-        }
-
         int ans=0;
-
-        for (int i = 0; i < n; i++) {
-            int ht=heights[i];
-            int width=ns[i]-ps[i]-1;
-            ans=max(ans,ht*width);
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int h = heights[st.top()];
+                st.pop();
+                int left = st.empty() ? -1 : st.top();
+                int width = i - left - 1;
+                ans = max(ans, h * width);
+            }
+            st.push(i);
         }
         return ans;
     }
