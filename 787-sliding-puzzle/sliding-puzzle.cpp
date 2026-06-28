c78d666bf4bd29acf29850b7e1dc144c30ba3714
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-28 18:43:12 +0530

    Time: 14 ms (29.77%) | Memory: 14.6 MB (22.18%) - LeetSync

---
+++ 787-sliding-puzzle/sliding-puzzle.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    string encode(vector<vector<int>>& board) {
+        string s;
+        for (auto& row : board)
+            for (int x : row)
+                s += char(x + '0');
+        return s;
+    }
+    bool check(vector<vector<int>>& board) {
+        int x = 1;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] != x)
+                    return false;
+                x = (x + 1) % 6;
+            }
+        }
+        return true;
+    }
+    int slidingPuzzle(vector<vector<int>>& board) {
+        queue<tuple<int, int, vector<vector<int>>>> q;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 0)
+                    q.push({i, j, board});
+            }
+        }
+
+        vector<int> dx = {1, -1, 0, 0};
+        vector<int> dy = {0, 0, 1, -1};
+        int ans = 0;
+        
+        unordered_map<string,bool>vis;
+        string state = encode(board);
+        vis[state]=true;
+        while (!q.empty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                auto [x, y, bb] = q.front();
+                q.pop();
+
+                if (check(bb))
+                    return ans;
+
+                for (int dir = 0; dir < 4; dir++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+
+                    if (nx >= 0 && ny >= 0 && nx < 2 && ny < 3) {
+
+                        swap(bb[x][y], bb[nx][ny]);
+                        string state = encode(bb);
+                        if (!vis[state]) {
+                            vis[state] = true;
+                            q.push({nx, ny, bb});
+                        }
+                        swap(bb[x][y], bb[nx][ny]);
+                    }
+                }
+            }
+            ans++;
+        }
+
+        return -1;
+    }
+};
