f417d67163752edc1d32070227f903f4b774704a
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-28 17:27:46 +0530

    Time: 7 ms (64.21%) | Memory: 16.7 MB (14.28%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -1,81 +1,46 @@
 class Solution {
 public:
-    int prec(char ch) {
-        if (ch == '/' || ch == '*')
-            return 2;
-        if (ch == '+' || ch == '-')
-            return 1;
-        return 3;
-    }
-    vector<string> convertToPostfix(string s) {
-        string ans;
-        stack<char> st;
-        vector<string> postfix;
-
+    int calculate(string s) {
+        stack<long long> st;
         int n = s.size();
-
+        int prevOp = '+';
         for (int i = 0; i < n; i++) {
-            if (isdigit(s[i])) {
-                string num;
+            if (s[i] == ' ')
+                continue;
+            if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') {
+                prevOp = s[i];
+            } else {
+                long long num = 0;
 
                 while (i < n && isdigit(s[i])) {
-                    num += s[i];
+                    num = num * 10 + (s[i] - '0');
                     i++;
                 }
-
-                postfix.push_back(num);
                 i--;
-            } else if (s[i] == ' ')
-                continue;
-            else if (s[i] == '(')
-                st.push(s[i]);
-            else if (s[i] == ')') {
 
-                while (!st.empty() && st.top() != '(') {
-                    postfix.push_back(string(1, st.top()));
-                    st.pop();
+                if (prevOp == '+') {
+                    st.push(num);
                 }
-                st.pop();
-            } else {
-                while (!st.empty() && st.top() != '(' &&
-                       prec(st.top()) >= prec(s[i])) {
-                    postfix.push_back(string(1, st.top()));
-                    st.pop();
+                if (prevOp == '-') {
+                    st.push(-num);
                 }
-                st.push(s[i]);
+                if (prevOp == '*') {
+                    int top=st.top();
+                    st.pop();
+                    st.push(top*num);
+                }
+                if (prevOp == '/') {
+                    int top=st.top();
+                    st.pop();
+                    st.push(top/num);
+                }
             }
         }
-        while (!st.empty()) {
-            postfix.push_back(string(1, st.top()));
+        long long ans=0;
+        while(!st.empty()){
+            ans+=st.top();
             st.pop();
         }
-        return postfix;
-    }
-    int calculate(string s) {
-        vector<string> exp = convertToPostfix(s);
-        stack<int> st;
-        int n = exp.size();
-        for (int i = 0; i < n; i++) {
-            if (exp[i] == "+" || exp[i] == "-"||exp[i] == "/"||exp[i] == "*") {
-                int n1 = st.top();
-                st.pop();
-                int n2 = st.top();
-                st.pop();
-                if (exp[i] == "+")
-                    st.push(n1 + n2);
-
-                else if (exp[i] == "*")
-                    st.push(n1 * n2);
-                else if (exp[i] == "-")
-                    st.push(n2 - n1);
-                else
-                    st.push(n2 / n1);
-            }
-
-            else {
-                st.push(stoi(exp[i]));
-            }
-        }
-        return st.top();
+        return ans;
     }
 };
