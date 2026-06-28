67faebb88d9d5fe465e634fd50ed68962cf94ba3
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-28 17:48:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (12.15%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<int, int>> q;
-        
+
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> vis(m, vector<int>(n, 0));
@@ -14,36 +13,41 @@ public:
                     fresh++;
                 if (grid[i][j] == 2) {
                     q.push({i, j});
-                    vis[i][j]=1;
+                    vis[i][j] = 1;
                 }
             }
         }
         if (fresh == 0)
             return 0;
 
-        vector<int>dx={0,0,-1,1};
-        vector<int>dy={-1,1,0,0};
+        vector<int> dx = {0, 0, -1, 1};
+        vector<int> dy = {-1, 1, 0, 0};
         int ans = 0;
-        while (!q.empty() && fresh > 0) {
-            int s = q.size();
-            ans++;
-            for (int i = 0; i < s; i++) {
+
+        while (!q.empty()) {
+            int ss = q.size();
+            for (int i = 0; i < ss; i++) {
                 auto [x, y] = q.front();
                 q.pop();
 
-                for(int d=0;d<4;d++){
-                    int nx=x+dx[d];
-                    int ny=y+dy[d];
-                    if(nx>=0 && nx<m && ny>=0 && ny<n && !vis[nx][ny] && grid[nx][ny]==1){
-                        q.push({nx,ny});
-                        vis[nx][ny]=1;
+                for (int dir = 0; dir < 4; dir++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+
+                    if (nx >= 0 && ny >= 0 && nx < m && ny < n &&
+                        !vis[nx][ny] && grid[nx][ny] == 1) {
+                        vis[nx][ny] = 1;
                         fresh--;
+                        q.push({nx, ny});
                     }
                 }
             }
+            if (!q.empty())
+                ans++;
         }
 
-        if(fresh==0) return ans;
+        if (fresh == 0)
+            return ans;
         return -1;
     }
 };
