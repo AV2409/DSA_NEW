65afacbe3361fd835f62f99c74f81b76251ee520
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-06-29 18:26:02 +0530

    Time: 245 ms (29.39%) | Memory: 71.2 MB (28.95%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int diff=INT_MAX;
+        vector<int>ans;
+        //{ele,row,col}
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+        multiset<tuple<int,int,int>>ms;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            pq.push({nums[i][0],i,0});
+            ms.insert({nums[i][0],i,0});
+        }
+        while(pq.size()==n){
+            auto [ele,row,col]=pq.top();
+            pq.pop();
+            auto [maxi,rm,cm]=*(prev(ms.end()));
+            if(maxi-ele<diff){
+                ans={ele,maxi};
+                diff=maxi-ele;
+            }
+            auto it=ms.find({ele,row,col});
+            if(it!=ms.end()) ms.erase(it);
+            
+            col++;
+            if(col<nums[row].size()){
+                pq.push({nums[row][col],row,col});
+                ms.insert({nums[row][col],row,col});
+            }
+        }
+        return ans;
+
+    }
+};
