b74d66622ce0f4698d359cb7bc99da3492efcd03
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-01 02:44:34 +0530

    Time: 4 ms (79.71%) | Memory: 55.1 MB (75.70%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -5,7 +5,7 @@ public:
         int n=arr.size();
         if(arr[0]!=1) arr[0]=1;
         for(int i=1;i<n;i++){
-            if(arr[i]-arr[i-1]>1){
+            if(arr[i]-arr[i-1]>1 && arr[i]>arr[i-1]+1){
                 arr[i]=arr[i-1]+1;
             }
         }
