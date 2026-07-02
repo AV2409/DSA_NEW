ecf14a4ad40378232d72aba63c9f481b5f949e40
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-02 19:22:06 +0530

    Time: 601 ms (20.43%) | Memory: 178.4 MB (47.45%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -16,7 +16,7 @@ public:
             }
         }
 
-        vector<pair<int, int>> dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        vector<pair<int, int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         while (!q.empty()) {
             auto [x, y] = q.front();
@@ -37,10 +37,10 @@ public:
 
         // Step 2: Modified Dijkstra (Maximum Minimum Path)
         priority_queue<vector<int>> pq;
-        vector<vector<int>> vis(n, vector<int>(n, 0));
+        vector<vector<int>> bestSafe(n, vector<int>(n, 0));
 
         pq.push({dist[0][0], 0, 0});
-        vis[0][0]=1;
+        bestSafe[0][0] = dist[0][0];
         while (!pq.empty()) {
             auto curr = pq.top();
             pq.pop();
@@ -48,7 +48,6 @@ public:
             int safe = curr[0];
             int x = curr[1];
             int y = curr[2];
-            
 
             if (x == n - 1 && y == n - 1)
                 return safe;
@@ -56,10 +55,11 @@ public:
             for (auto [dx, dy] : dir) {
                 int nx = x + dx;
                 int ny = y + dy;
-
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n &&!vis[nx][ny]) {
-                    vis[nx][ny] = 1;
-                    pq.push({min(safe, dist[nx][ny]), nx, ny});
+                
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n && min(safe, dist[nx][ny])>bestSafe[nx][ny]) {
+                    int newSafe = min(safe, dist[nx][ny]);
+                    bestSafe[nx][ny] = newSafe;
+                    pq.push({newSafe, nx, ny});
                 }
             }
         }
