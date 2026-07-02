4f52eb4dbbdabe6ee2fb0cb8acbb5be3c6169d3d
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-02 15:42:45 +0530

    Time: 23 ms (56.97%) | Memory: 23.4 MB (91.61%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,38 +10,22 @@
 
 class Solution {
 public:
-    void helper(TreeNode* root, int mini, int maxi, TreeNode*& ans) {
-        if (!root || ans != NULL)
-            return;
+    TreeNode* ans=NULL;
+    void helper(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(!root) return;
+        if(ans) return;
 
-        if (root->val >= mini && root->val <= maxi) {
-            ans = root;
-            return;
-        }
-        if (root->val < mini) {
-            helper(root->right, mini, maxi, ans);
-        }
-        if (root->val > maxi) {
-            helper(root->left, mini, maxi, ans);
+        int mini=min(p->val,q->val);
+        int maxi=max(q->val,p->val);
+
+        if(root->val>maxi) helper(root->left,p,q);
+        else if(root->val<mini) helper(root->right,p,q);
+        else{
+            if(!ans) ans=root;
         }
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        int x = p->val;
-        int y = q->val;
-
-        int mini = min(x, y);
-        int maxi = max(x, y);
-        while (root) {
-            if (root->val >= mini && root->val <= maxi) {
-                return root;
-            }
-            if (root->val < mini) {
-                root=root->right;
-            }
-            if (root->val > maxi) {
-                root=root->left;
-            }
-        }
-        return root;
+        helper(root,p,q);
+        return ans;
     }
 };
