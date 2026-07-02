de3641167d4ad7765f54135795e7063778593550
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-02 16:28:23 +0530

    Time: 0 ms (100.00%) | Memory: 104.2 MB (77.69%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -18,22 +18,24 @@ public:
         int mini;
         int maxi;
     };
-    int ans=0;
+    int ans = 0;
+    Node helper(TreeNode* root) {
+        if (!root)
+            return {true, 0, INT_MAX, INT_MIN};
 
-    Node helper(TreeNode* root){
-        if(!root) return {true,0,INT_MAX,INT_MIN};
+        Node l = helper(root->left);
+        Node r = helper(root->right);
 
-        Node l=helper(root->left);
-        Node r=helper(root->right);
-        int sum=0;
-        if(l.isBst && r.isBst && root->val>l.maxi && root->val<r.mini){
-            sum+=root->val+l.sum+r.sum;
-            ans=max(ans,sum);
-            return {true,sum,min(l.mini,root->val),max(root->val,r.maxi)};
+        if (l.isBst && r.isBst && root->val > l.maxi && root->val < r.mini) {
+            int temp = root->val + l.sum + r.sum;
+            ans = max(ans, temp);
+            int mn = min(root->val, l.mini);
+            int mx = max(root->val, r.maxi);
+            return {true, temp, mn, mx};
         }
-        return {false,0,INT_MIN,INT_MAX};
-    }
 
+        return {false, 0, INT_MIN, INT_MAX};
+    }
     int maxSumBST(TreeNode* root) {
         helper(root);
         return ans;
