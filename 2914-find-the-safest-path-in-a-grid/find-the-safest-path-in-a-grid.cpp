6dd763c4ffa901089e3f5640d656b3aabe5f96b3
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-02 19:16:03 +0530

    Time: 521 ms (43.82%) | Memory: 178.9 MB (44.09%) - LeetSync

---
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        vector<vector<int>> dist(n, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+
+        // Step 1: Multi-source BFS
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        vector<pair<int, int>> dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            for (auto [dx, dy] : dir) {
+                int nx = x + dx;
+                int ny = y + dy;
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n &&
+                    dist[nx][ny] == -1) {
+
+                    dist[nx][ny] = dist[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+
+        // Step 2: Modified Dijkstra (Maximum Minimum Path)
+        priority_queue<vector<int>> pq;
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+
+        pq.push({dist[0][0], 0, 0});
+        vis[0][0]=1;
+        while (!pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
+
+            int safe = curr[0];
+            int x = curr[1];
+            int y = curr[2];
+            
+
+            if (x == n - 1 && y == n - 1)
+                return safe;
+
+            for (auto [dx, dy] : dir) {
+                int nx = x + dx;
+                int ny = y + dy;
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n &&!vis[nx][ny]) {
+                    vis[nx][ny] = 1;
+                    pq.push({min(safe, dist[nx][ny]), nx, ny});
+                }
+            }
+        }
+
+        return 0;
+    }
+};
