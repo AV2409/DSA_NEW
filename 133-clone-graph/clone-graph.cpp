4c2c0b1cca41fb6447bf2656bab66929a1a439b0
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-03 16:03:17 +0530

    Time: 3 ms (68.35%) | Memory: 12.2 MB (47.92%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,68 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr)
+            return nullptr;
+        unordered_map<Node*, Node*> mp;
+        set<Node*> vis;
+        queue<Node*> q;
+        q.push(node);
+        vis.insert(node);
+
+        while (!q.empty()) {
+            Node* nn = q.front();
+            q.pop();
+            Node* newNode = new Node(nn->val);
+            mp[nn] = newNode;
+            for (Node* adj : nn->neighbors) {
+                if (!vis.count(adj)) {
+                    vis.insert(adj);
+                    q.push(adj);
+                }
+            }
+        }
+
+        vis.clear();
+        q.push(node);
+        vis.insert(node);
+
+        while (!q.empty()) {
+            Node* nn = q.front();
+            q.pop();
+
+            Node* clone = mp[nn];
+
+            for (Node* adj : nn->neighbors) {
+                clone->neighbors.push_back(mp[adj]);
+
+                if (!vis.count(adj)) {
+                    vis.insert(adj);
+                    q.push(adj);
+                }
+            }
+        }
+
+        return mp[node];
+    }
+};
