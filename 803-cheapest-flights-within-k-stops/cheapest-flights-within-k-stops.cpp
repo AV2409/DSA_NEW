cac7db5ed87cee3a6727a700e6951711ef1d9a01
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-03 18:45:41 +0530

    Time: 8 ms (20.27%) | Memory: 19.8 MB (11.14%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -2,37 +2,38 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>>adj(n);
+
         for(auto it:flights){
             int u=it[0];
             int v=it[1];
             int wt=it[2];
-
             adj[u].push_back({v,wt});
         }
+        vector<vector<int>>dist(n,vector<int>(k+2,1e9));
+        dist[src][0]=0;
 
-        vector<vector<int>>dist(k+2,vector<int>(n,1e9));;
-        dist[0][src]=0;
-
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
-        // {dist,node,stops}
-        pq.push({0,src,0});
+        
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        pq.push({0,0,src});
 
         while(!pq.empty()){
-            auto [d,node,st]=pq.top();
+            auto it=pq.top();
+            int d=it[0];
+            int stops=it[1];
+            int node=it[2];
             pq.pop();
-            if (d > dist[st][node]) continue;
-            for(auto [nei,wt]:adj[node]){
-                if( st<k+1 && wt+d<dist[st+1][nei]){
-                    dist[st+1][nei]=wt+d;
-                    pq.push({dist[st+1][nei], nei, st+1});
+            if(node==dst) return d;
+            if(stops==k+1) continue;
+            if(d>dist[node][stops]) continue;
+
+            for(auto& [adjNode,wt]:adj[node]){
+                if(wt+d<dist[adjNode][stops+1]){
+                    dist[adjNode][stops+1]=wt+d;
+                    pq.push({d+wt,stops+1,adjNode});
                 }
             }
         }
-        int ans=1e9;
-        for(int i=0;i<k+2;i++){
-            ans=min(ans,dist[i][dst]);
-        }
-        if(ans==1e9) return -1;
-        return ans;
+        return -1;
+
     }
 };
