21f9d78dca23e5889f2a5fc057a35f67a3e42cfb
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-04 17:30:11 +0530

    Time: 13 ms (32.67%) | Memory: 15.3 MB (25.58%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,33 +1,3 @@
-class DSU {
-public:
-    vector<int> parent;
-    vector<int> size;
-    DSU(int n) {
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
-    }
-    int find(int node) {
-        if (node == parent[node]) {
-            return node;
-        }
-        return parent[node] = find(parent[node]);
-    }
-    void unite(int u, int v) {
-        int ulp_u = find(u);
-        int ulp_v = find(v);
-        if (ulp_u == ulp_v) {
-            return;
-        }
-        if (size[ulp_u] < size[ulp_v])
-            swap(ulp_u, ulp_v);
-        parent[ulp_v] = ulp_u;
-        size[ulp_u] += size[ulp_v];
-    }
-};
-
 class Solution {
 public:
 //minimize max value of a path
