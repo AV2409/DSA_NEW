ce2efa87427b43e818f6fe1666f8c209b0e9d6f4
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-04 17:29:55 +0530

    Time: 22 ms (18.24%) | Memory: 15.3 MB (25.58%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,33 +1,69 @@
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> size;
+    DSU(int n) {
+        size.resize(n + 1, 1);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+    int find(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = find(parent[node]);
+    }
+    void unite(int u, int v) {
+        int ulp_u = find(u);
+        int ulp_v = find(v);
+        if (ulp_u == ulp_v) {
+            return;
+        }
+        if (size[ulp_u] < size[ulp_v])
+            swap(ulp_u, ulp_v);
+        parent[ulp_v] = ulp_u;
+        size[ulp_u] += size[ulp_v];
+    }
+};
+
 class Solution {
 public:
+//minimize max value of a path
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
-                       greater<tuple<int, int, int>>>
-            pq;
-        pq.push({grid[0][0], 0, 0});
-        vector<int> dx = {0, 0, -1, 1};
-        vector<int> dy = {1, -1, 0, 0};
-        vector<vector<int>> dist(n, vector<int>(n, 1e9));
-        dist[0][0] = grid[0][0];
-        while (!pq.empty()) {
-            auto [curr, x, y] = pq.top();
+        int n=grid.size();
+        vector<vector<int>>dist(n,vector<int>(n,1e9));
+        dist[0][0]=grid[0][0];
+        
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+
+        pq.push({dist[0][0],0,0});
+        vector<int>dx={0,0,1,-1};
+        vector<int>dy={1,-1,0,0};
+        while(!pq.empty()){
+            auto it=pq.top();
             pq.pop();
+            int maxi=it[0];
+            int x=it[1];
+            int y=it[2];
 
-            if (x == n - 1 && y == n - 1)
-                return curr;
+            if(x==n-1 && y==n-1) return maxi;
 
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = x + dx[dir];
-                int ny = y + dy[dir];
+            for(int dir=0;dir<4;dir++){
+                int nx=x+dx[dir];
+                int ny=y+dy[dir];
 
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n &&
-                    max(grid[nx][ny], curr) < dist[nx][ny]) {
-                    dist[nx][ny] = max(grid[nx][ny], curr);
-                    pq.push({dist[nx][ny], nx, ny});
+                if(nx>=0 && ny>=0 && nx<n && ny<n){
+                    int newMaxi=max(maxi,grid[nx][ny]);
+                    if(newMaxi<dist[nx][ny]){
+                        dist[nx][ny]=newMaxi;
+                        pq.push({newMaxi,nx,ny});
+                    }
                 }
             }
+            
         }
-        return dist[n - 1][n - 1];
+        return 0;
     }
 };
