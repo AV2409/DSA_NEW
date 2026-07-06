829d930fc32f97d034c4e725f102571e46efcad2
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-06 19:47:25 +0530

    Time: 359 ms (5.01%) | Memory: 107.9 MB (5.70%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    unordered_map<string,vector<int>>mp;
+    vector<vector<vector<int>>>dp;
+    int f(int i,vector<string>& strs, int m, int n){
+        if(i<0) {
+            return 0;
+        }
+        if(dp[i][m][n]!=-1) return dp[i][m][n];
+        int np=f(i-1,strs,m,n);
+        int p=0;
+        string word=strs[i];
+        int zeros=mp[word][0];
+        int ones=mp[word][1];
+
+        if(zeros<=m && ones<=n) p=1+f(i-1,strs,m-zeros,n-ones);
+        return dp[i][m][n]= max(p,np);
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        for(string s:strs){
+            int o=0;
+            int z=0;
+            for(char c:s){
+                if(c=='0') z++;
+                if(c=='1') o++;
+            }
+            mp[s]={z,o};
+        }
+        int x=strs.size();
+        dp.assign(x,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
+        return f(x-1,strs,m,n);
+    }
+};
