80d039b547435e187eb1bfdee5b0cd91d4bc6c30
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-07 17:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (20.30%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
-    int find(int idx,vector<int>& days,int package){
-        int currDay = days[idx];
-        int i=idx;
-        int n=days.size();
-        while(i<n && days[i]<currDay+package){
-            i++;
-        }
-        return i;
-    }
-    int f(int idx, vector<int>& days, vector<int>& costs,vector<int>& dp) {
-        if (idx >= days.size())
-            return 0;
+    vector<int>dp;
+    int f(int i,int timer,vector<int>& days, vector<int>& costs){
+        if(i==days.size()) return 0;
+        
+        if(timer>days[i]) return f(i+1,timer,days,costs);
+        if(dp[i]!=-1) return dp[i];
+        int one=costs[0]+f(i+1,days[i]+1,days,costs);
 
-        // 1day
-        if(dp[idx]!=-1) return dp[idx];
-        int op1 = costs[0]+f(idx+1,days,costs,dp);
+        int seven=costs[1]+f(i+1,days[i]+7,days,costs);
 
-        // 7day
-        int xx=find(idx,days,7);
-        int op2=costs[1]+f(xx,days,costs,dp);
+        int thirty=costs[2]+f(i+1,days[i]+30,days,costs);
 
-        // 30day
-        xx=find(idx,days,30);
-        int op3=costs[2]+f(xx,days,costs,dp);
-
-        return dp[idx]= min({op1,op2,op3});
+        return dp[i]= min({one,seven,thirty});
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n=days.size();
-        vector<int>dp(n+1,-1);
-        return f(0,days,costs,dp);
+        dp.assign(n,-1);
+        return f(0,0,days,costs);
     }
 };
