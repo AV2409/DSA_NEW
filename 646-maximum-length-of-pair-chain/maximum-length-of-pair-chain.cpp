4a7876834b8de3a3bc970cf0eb2778c9a269bb6f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-09 17:18:59 +0530

    Time: 40 ms (46.65%) | Memory: 27.2 MB (48.20%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -6,13 +6,9 @@ public:
         vector<int> dp(n, 1);
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            auto p1 = pairs[i];
-            int c = p1[0];
-            int d = p1[1];
+            int c = pairs[i][0];
             for (int j = 0; j < i; j++) {
-                auto p2 = pairs[j];
-                int a = p2[0];
-                int b = p2[1];
+                int b = pairs[j][1];
                 if (c>b) {
                     dp[i] = max(dp[i], 1 + dp[j]);
                 }
