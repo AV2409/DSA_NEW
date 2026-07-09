efc8877c0cdda73342939f3e263b7787f3f58b30
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-09 16:12:28 +0530

    Time: 568 ms (13.30%) | Memory: 296.6 MB (12.73%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    int find(vector<int>&arr,int key){
-        int l=0;
-        int r=arr.size();
-        int mid;
-        int ans=-1;
-        while(l<=r){
-            mid=(l+r)/2;
-            if(arr[mid]>=key){
-                ans=mid;
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return ans;
+    vector<vector<int>>dp;
+    int f(int i,int prev,vector<int>& nums){
+        if(i==nums.size()) return 0;
+
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        //dont pick
+        int op1=f(i+1,prev,nums);
+        int op2=0;
+        if(prev==-1||nums[prev]<nums[i]) op2=1+f(i+1,i,nums);
+
+        return dp[i][prev+1]= max(op1,op2);
+
     }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> arr;
-        arr.push_back(nums[0]);
-        int idx=0;
-        for(int i=1;i<n;i++){
-            if(arr[idx]<nums[i]){
-                arr.push_back(nums[i]);
-                idx++;
-            }
-            else{
-                int posi=find(arr,nums[i]);
-                arr[posi]=nums[i];
-            }
-        }
-        return arr.size();
+        int n=nums.size();
+        dp.assign(n,vector<int>(n+1,-1));
+        return f(0,-1,nums);
     }
 };
