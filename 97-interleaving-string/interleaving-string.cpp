c416a06f8365d3973b6888231e036e1c0a1d4cc0
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-10 02:21:46 +0530

    Time: 6 ms (26.41%) | Memory: 9.4 MB (48.89%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -4,27 +4,35 @@ public:
         int n = s1.size();
         int m = s2.size();
 
-        if (s3.size() != n + m) return false;
+        if (s3.size() != n + m)
+            return false;
 
         vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-
         dp[0][0] = true;
+        vector<bool> curr(m + 1, 0);
+        vector<bool> prev(n + 1, 0);
+        prev[0] = true;
 
         for (int i = 0; i <= n; i++) {
+            curr.assign(m + 1, 0);
             for (int j = 0; j <= m; j++) {
-                if (i == 0 && j == 0) continue;
+                if (i == 0 && j == 0) {
+                    curr[0] = true;
+                    continue;
+                }
                 int k = i + j - 1;
-                bool op1=false;
-                bool op2=false;
+                bool op1 = false;
+                bool op2 = false;
                 if (i > 0 && s1[i - 1] == s3[k])
-                    op1= dp[i - 1][j];
+                    op1 = prev[j];
                 if (j > 0 && s2[j - 1] == s3[k])
-                    op2= dp[i][j - 1];
-                
-                dp[i][j]=op1|op2;
+                    op2 = curr[j - 1];
+
+                curr[j] = op1 | op2;
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return curr[m];
     }
 };
