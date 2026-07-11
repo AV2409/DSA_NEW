b370ea2ba04242ea7196b4a1e7836bf2d317b653
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-11 17:21:24 +0530

    Time: 33 ms (90.65%) | Memory: 21 MB (54.21%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    int rows;
+    int cols;
+    vector<vector<vector<int>>>dp;
+    int f(int row,int j,int y,vector<vector<int>>& grid){
+        if(row==rows) return 0;
+        if(dp[row][j][y]!=-1) return dp[row][j][y];
+        int ans=0;
+        for(int dj=-1;dj<=1;dj++){
+            int nj=j+dj;
+            int newRow=row+1;
+            for(int dy=-1;dy<=1;dy++){
+                int ny=y+dy;
+                if(nj>=0 &&nj<cols && ny>=0 && ny<cols){
+                    ans=max(ans,f(newRow,nj,ny,grid));
+                }
+            }
+        }
+        ans+=grid[row][j];
+        if(j!=y) ans+=grid[row][y];
+        return dp[row][j][y]= ans;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        rows=grid.size();
+        cols=grid[0].size();
+        dp.assign(rows,vector<vector<int>>(cols,vector<int>(cols,-1)));
+        int i=0;
+        int j=0;
+        int y=cols-1;
+        return f(i,j,y,grid);
+    }
+};
