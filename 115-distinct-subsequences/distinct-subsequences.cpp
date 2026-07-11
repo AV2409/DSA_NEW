f3a3ca55e8dd729a40d8ec820cbb4fad3788851e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-11 18:03:22 +0530

    Time: 31 ms (52.58%) | Memory: 27 MB (64.24%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
+    vector<vector<int>>dp;
+    int f(int i,int j,string &s, string &t){
+        if(j<0) return 1;
+        if(i<0) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+        int op1=0;
+        int op2=0;
+        if(s[i]==t[j]) op1=f(i-1,j-1,s,t);
+        op2=f(i-1,j,s,t);
+        return dp[i][j]= op1+op2;
+    }
     int numDistinct(string s, string t) {
-        int n1 = s.size();
-        int n2 = t.size();
-        vector<vector<long long>> dp(n2 + 1, vector<long long>(n1 + 1));
-        const long long MOD=1e18+7;
-        for(int i=0;i<=n1;i++){
-            dp[0][i]=1;
-        }
-        for (int i = 1; i <= n2; i++) {
-            for (int j = 1; j <= n1; j++) {
-                if(s[j-1]==t[i-1]) dp[i][j]=(dp[i-1][j-1]+dp[i][j-1])%MOD;
-                else{
-                    dp[i][j]=dp[i][j-1];
-                }
-            }
-        }
-        return dp[n2][n1];
+        int m=s.size();
+        int n=t.size();
+        dp.assign(m,vector<int>(n,-1));
+        return f(m-1,n-1,s,t);
     }
 };
