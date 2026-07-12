a221290be65effba563cd14b5e57f3b827c01450
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-12 17:34:36 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (90.18%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -16,12 +16,6 @@ public:
                 }
             }
         }
-        int x=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==ele) x++;
-        }
-
-        if(x>n/2) return ele;
-        return -1;
+        return ele;
     }
 };
