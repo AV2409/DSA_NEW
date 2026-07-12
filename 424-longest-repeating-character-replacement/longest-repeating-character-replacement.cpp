2470e52583a288b8a1927f812d525c8e9a1cdd59
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-12 17:18:42 +0530

    Time: 14 ms (21.08%) | Memory: 10.8 MB (93.45%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -10,7 +10,7 @@ public:
         for (int h = 0; h < n; h++) {
             hash[s[h]-'A']++;
             maxfreq=*max_element(hash.begin(),hash.end());
-            while(h-l+1-maxfreq>k){
+            if(h-l+1-maxfreq>k){
                 hash[s[l]-'A']--;
                 l++;
                 maxfreq=*max_element(hash.begin(),hash.end());
