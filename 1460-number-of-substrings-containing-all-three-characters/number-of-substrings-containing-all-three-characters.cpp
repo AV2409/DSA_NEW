3d27dea3836894aead0ca55b97060d5b51357f51
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-12 17:10:34 +0530

    Time: 10 ms (44.22%) | Memory: 11 MB (51.93%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-
+    int atmost(string &s,int k){
         int n = s.size();
         int ans = 0;
         vector<int> hash(3);
         int i=0;
-        for (int j = i; j < n; j++) {
+        int cnt=0;
+        for (int j = 0; j < n; j++) {
+            if(hash[s[j]-'a']==0) cnt++;
             hash[s[j] - 'a']++;
-            while (hash[0] && hash[1] && hash[2]) {
-                ans += n - j;
+            while (cnt>k) {
                 hash[s[i]-'a']--;
+                if(hash[s[i]-'a']==0) cnt--;
                 i++;
             }
+            ans += j-i+1;
         }
 
         return ans;
+    }
+    int numberOfSubstrings(string s) {
+        return atmost(s,3)-atmost(s,2);
     }
 };
