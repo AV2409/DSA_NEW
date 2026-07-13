dcea9c320da3921893c661d7b189bf8b3d03210f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-13 21:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (24.87%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    void f(int k, int n, int i, vector<int>& list, vector<vector<int>>& ans) {
-        if (k == 0 && n == 0) {
-            ans.push_back(list);
+    void f(int i, int k, int target, vector<int>& cur,
+           vector<vector<int>>& ans) {
+        if (cur.size() == k && target == 0) {
+            ans.push_back(cur);
             return;
         }
-        if (k <= 0 || n <= 0 || i == 10)
+
+        if (i > 9)
             return;
 
-        for (int idx = i; idx <= 9; idx++) {
-            list.push_back(idx);
-            f(k - 1, n - idx, idx + 1, list, ans);
-            list.pop_back();
+        // Not Pick
+        f(i + 1, k, target, cur, ans);
+        // Pick
+        if (target - i >= 0) {
+            cur.push_back(i);
+            f(i + 1, k, target - i, cur, ans);
+            cur.pop_back();
         }
     }
+
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> list;
         vector<vector<int>> ans;
-        f(k, n, 1, list, ans);
+        vector<int> cur;
+        f(1, k, n, cur, ans);
         return ans;
     }
 };
