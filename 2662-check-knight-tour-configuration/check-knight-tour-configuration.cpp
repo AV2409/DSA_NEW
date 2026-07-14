0b63ea3db3710ad015c551a59e34c2f2009e64f3
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-14 14:26:26 +0530

    Time: 3 ms (9.84%) | Memory: 31.7 MB (66.32%) - LeetSync

---
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<pair<int,int>>dir={{-2,-1},{-2,1},{2,-1},{2,1},{-1,-2},{-1,2},{1,-2},{1,2}};
+    int n;
+    bool dfs(int i,int j,vector<vector<int>>& grid){
+        cout<<i<<"-"<<j<<endl;
+        if(grid[i][j]==((n*n)-1)) return true;
+
+        for(auto [dx,dy]:dir){
+            int nx=i+dx;
+            int ny=j+dy;
+
+            if(nx>=0 && ny>=0 && nx<n && ny<n && grid[nx][ny]==1+grid[i][j]){
+                if(dfs(nx,ny,grid)) return true;
+            }
+        } 
+        return false;
+    }
+    bool checkValidGrid(vector<vector<int>>& grid) {
+        n=grid.size();
+        if(grid[0][0]!=0) return false;
+        return dfs(0,0,grid);
+    }
+};
