639707204e9b8e202e49490e09ab38782fcd38cd
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-14 20:18:55 +0530

    Time: 6 ms (69.42%) | Memory: 55.7 MB (81.16%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -1,48 +1,48 @@
 class Twitter {
 public:
-    vector<set<int>>follows;
-    vector<vector<pair<int,int>>>tweets; //id,timer
     int timer;
-    Twitter() {
-        follows.resize(501);
-        tweets.resize(501);
-        timer=0;
-    }
-    
+    unordered_map<int, set<int>> following;
+    unordered_map<int, vector<pair<int, int>>> tweets;
+
+    Twitter() { timer = 0; }
+
     void postTweet(int userId, int tweetId) {
-        tweets[userId].push_back({tweetId,timer});
         timer++;
+        tweets[userId].push_back({timer, tweetId});
     }
-    
+
     vector<int> getNewsFeed(int userId) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-
-        set<int> users=follows[userId];
-        users.insert(userId);
-        vector<int>feed;
-        for(int id:users){
-            for(auto it:tweets[id]){
-                int tid=it.first;
-                int time=it.second;
-
-                pq.push({time,tid});
-                if(pq.size()>10) pq.pop();
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for (auto [time, tweet] : tweets[userId]) {
+            pq.push({time, tweet});
+            if (pq.size() > 10)
+                pq.pop();
+        }
+        for (int user : following[userId]) {
+            for (auto [time, tweet] : tweets[user]) {
+                pq.push({time, tweet});
+                if (pq.size() > 10)
+                    pq.pop();
             }
         }
-        while(!pq.empty()){
-            feed.push_back(pq.top().second);
+        int idx=pq.size();
+        vector<int> ans(idx);
+        idx--;
+        while (!pq.empty()) {
+            ans[idx]=(pq.top().second);
             pq.pop();
+            idx--;
         }
-        reverse(feed.begin(),feed.end());
-        return feed;
+        return ans;
     }
-    
+
     void follow(int followerId, int followeeId) {
-        follows[followerId].insert(followeeId);
+        following[followerId].insert(followeeId);
     }
-    
+
     void unfollow(int followerId, int followeeId) {
-        follows[followerId].erase(followeeId);
+        following[followerId].erase(followeeId);
     }
 };
 
