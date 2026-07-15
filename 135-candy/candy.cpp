a0d17b29a68ab522fc244ee4f0b32280ed527c68
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-15 14:19:52 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (91.59%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -3,23 +3,27 @@ public:
     int candy(vector<int>& ratings) {
         int i=1;
         int n=ratings.size();
-        vector<int>forward(n,1);
-        vector<int>backward(n,1);
-
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                forward[i]=forward[i-1]+1;
+        
+        int ans=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                i++;
+                ans++;
+                continue;
             }
-        }
-
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                backward[i]=backward[i+1]+1;
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                ans+=peak;
+                i++;
             }
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans+=max(forward[i],backward[i]);
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                ans+=down;
+                down++;
+                i++;
+            }
+            ans+=max(0,down-peak);
         }
         return ans;
 
