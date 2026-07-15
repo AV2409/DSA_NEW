3975a6adc92abf3378a7e5b0c0bc7435e4d26097
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-15 19:18:56 +0530

    Time: 91 ms (22.05%) | Memory: 51.4 MB (38.37%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.cpp
+++ 480-sliding-window-median/sliding-window-median.cpp
@@ -1,55 +1,55 @@
 class Solution {
 public:
-
-    multiset<int> low, high;
-    void balance() {
-        while (low.size() > high.size() + 1) {
-            high.insert(*prev(low.end()));
-            low.erase(prev(low.end()));
+    multiset<int>left;
+    multiset<int>right;
+    void balance(){
+        if(left.size()>right.size()+1){
+            int x=*(prev(left.end()));
+            right.insert(x);
+            left.erase(prev(left.end()));
         }
-
-        while (low.size() < high.size()) {
-            low.insert(*high.begin());
-            high.erase(high.begin());
+        if(right.size()>left.size()){
+            int x=*(right.begin());
+            left.insert(x);
+            right.erase(right.begin());
         }
     }
-
-    void insert(int num) {
-        if (low.empty() || num <= *prev(low.end()))
-            low.insert(num);
-        else
-            high.insert(num);
+    void add(int num){
+        if(left.empty()||num<=*(prev(left.end()))) left.insert(num);
+        else right.insert(num);
 
         balance();
     }
 
-    void remove(int num) {
-        auto it = low.find(num);
-        if (it != low.end())
-            low.erase(it);
-        else
-            high.erase(high.find(num));
+    void remove(int num){
+        auto leftit=left.find(num);
+        auto rightit=right.find(num);
 
+        if(leftit!=left.end()){
+            left.erase(leftit);
+        }
+        else right.erase(rightit);
         balance();
     }
 
-    double median(int k) {
-        if (k & 1)
-            return *prev(low.end());
-        return ((double)*prev(low.end()) + *high.begin()) / 2.0;
+    double median(){
+        double x=*(prev(left.end()));
+        if(left.size()==right.size()){
+            double y=*(right.begin());
+            return (x+y)/2.0;
+        }
+        else return x;
     }
-
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector<double> ans;
-        for (int i = 0; i < k; i++)
-            insert(nums[i]);
+        int n=nums.size();
+        vector<double>ans;
 
-        ans.push_back(median(k));
-
-        for (int i = k; i < nums.size(); i++) {
-            remove(nums[i - k]);
-            insert(nums[i]);
-            ans.push_back(median(k));
+        for(int i=0;i<n;i++){
+            add(nums[i]);
+            if(i>=k){
+                remove(nums[i-k]);
+            }
+            if(i>=k-1) ans.push_back(median());
         }
         return ans;
     }
