6f411b65879135cb5308d1c801fbe5b5ec6d69d7
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-16 19:54:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (36.34%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    vector<int>vis;
-    int cnt;
-    int K;
-    void f(string& s,string& list,string& ans){
-        if(list.size()==s.size()){
-            cnt++;
-            if(cnt==K){
-                ans=list;
-            }
-            return;
-        }
-        if(!ans.empty()) return;
-        for(int j=0;j<s.size();j++){
-            if(!vis[j]){
-                list.push_back(s[j]);
-                vis[j]=1;
-                f(s,list,ans);
-                list.pop_back();
-                vis[j]=0;
-            }
-        }
-    }
     string getPermutation(int n, int k) {
-        string s;
-        cnt=0;
-        K=k;
-        char ch='1';
-        for(int i=1;i<=n;i++){
-            s+=ch;
-            ch++;
-        }
-        vis.assign(n,0);
+        vector<int> nums;
+        int fact = 1;
+
+        for (int i = 1; i < n; i++)
+            fact *= i;
+
+        for (int i = 1; i <= n; i++)
+            nums.push_back(i);
+
+        k--;
         string ans;
-        string list;
-        f(s,list,ans);
+
+        while (true) {
+            ans += to_string(nums[k/fact]);
+            nums.erase(nums.begin() + k / fact);
+
+            if (nums.empty())
+                break;
+
+            k %= fact;
+            fact /= nums.size();
+        }
+
         return ans;
     }
 };
