1cd890b26369accec5973c307467e5e66c356f29
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-16 13:32:19 +0530

    Time: 1 ms (80.16%) | Memory: 27.6 MB (34.94%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,25 +11,28 @@
  */
 class Solution {
 public:
-    TreeNode* build(vector<int>& postorder,int &idx,int low,int high,unordered_map<int,int>&mp){
+    unordered_map<int,int>mp;
+    TreeNode* build(int &idx,int low,int high,vector<int>& postorder, vector<int>& inorder){
         if(low>high) return NULL;
-
+        if(idx<0) return NULL;
         TreeNode* root=new TreeNode(postorder[idx]);
-        int i=mp[postorder[idx]];
-        idx--;
 
-        root->right=build(postorder,idx,i+1,high,mp);
-        root->left=build(postorder,idx,low,i-1,mp);
+        int inIdx=mp[postorder[idx]];
+        idx--;
+        root->right=build(idx,inIdx+1,high,postorder,inorder);
+        root->left=build(idx,low,inIdx-1,postorder,inorder);
+        
         return root;
+
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n=inorder.size();
-        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
             mp[inorder[i]]=i;
         }
+
         int idx=n-1;
-        TreeNode* root=build(postorder,idx,0,n-1,mp);
-        return root;
+
+        return build(idx,0,n-1,postorder,inorder);
     }
 };
