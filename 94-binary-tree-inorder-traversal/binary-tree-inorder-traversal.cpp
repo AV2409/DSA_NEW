6a3215073200f6eace658f7be506691beec8b899
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-17 12:03:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (66.32%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -6,31 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        if(!root) return {};
-        stack<TreeNode*> st;
-        vector<int>ans;
-        TreeNode* temp=root;
-        while(temp){
-            st.push(temp);
-            temp=temp->left;
-        }
-        while(!st.empty()){
-            TreeNode* node=st.top();
-            st.pop();
-            ans.push_back(node->val);
-            TreeNode* tt=node->right;
-            while(tt){
-                st.push(tt);
-                tt=tt->left;
+        vector<int> ans;
+
+        TreeNode* curr = root;
+
+        while (curr) {
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+
+            else {
+                // find rightmost guy on left
+                TreeNode* temp = curr->left;
+                TreeNode* prev = curr->left;
+                while (temp && temp!=curr) {
+                    prev = temp;
+                    temp = temp->right;
+                }
+
+                // connect that guy to the root/curr
+                //creating thread
+                if (prev->right == NULL) {
+                    prev->right = curr;
+                    curr=curr->left;
+                }
+
+                //if already connected
+                else{
+                    prev->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
             }
         }
         return ans;
-
     }
 };
