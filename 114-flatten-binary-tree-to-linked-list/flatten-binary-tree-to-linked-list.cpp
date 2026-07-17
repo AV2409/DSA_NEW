7660d0773ed82f73cb6e9618f3e68ea59a2052c8
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-17 12:22:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (15.89%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -14,14 +14,48 @@ class Solution {
 public:
     TreeNode* prev = NULL;
     void flatten(TreeNode* root) {
-        if(!root) return;
+        vector<TreeNode*> ans;
+        TreeNode* curr = root;
+        while (curr) {
+            if (curr->left == NULL) {
+                ans.push_back(curr);
+                curr = curr->right;
+            }
 
-        flatten(root->right);
-        flatten(root->left);
+            else {
+                // find rightmost guy on left
+                TreeNode* temp = curr->left;
+                TreeNode* prev = curr->left;
+                while (temp && temp != curr) {
+                    prev = temp;
+                    temp = temp->right;
+                }
 
-        root->right=prev;
-        root->left=NULL;
-        prev=root;
+                // connect that guy to the root/curr
+                // creating thread
+                if (prev->right == NULL) {
+                    prev->right = curr;
+                    ans.push_back(curr);
+                    curr = curr->left;
+                }
+
+                // if already connected
+                else {
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+        int n = ans.size();
+        for (int i = 0; i < n - 1; i++) {
+            TreeNode* prev = ans[i];
+            TreeNode* next = ans[i + 1];
+            prev->right = next;
+            prev->left = NULL;
+        }
+        if (n > 0) {
+            ans[n - 1]->left = NULL;
+            ans[n - 1]->right = NULL;
+        }
     }
-
 };
