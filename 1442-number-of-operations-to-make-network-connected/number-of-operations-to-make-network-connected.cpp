1ba2cf3b7d8ca11355ad97a8d505d74bfaab3461
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-18 15:50:55 +0530

    Time: 19 ms (52.03%) | Memory: 46.4 MB (58.10%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,69 +1,50 @@
-class DisjointSet {
+class DSU{
 public:
-    vector<int> parent;
-    vector<int> size;
-
-    DisjointSet(int n) {
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
+    vector<int>par;
+    vector<int>size;
+    int n;
+    DSU(int _n){
+        n=_n;
+        par.resize(_n);
+        size.assign(_n,1);
+        for(int i=0;i<_n;i++) par[i]=i;
     }
-
-    int find(int node) {
-        if (node == parent[node]) {
-            return node;
+    
+    void unite(int u,int v){
+        int pu=findPar(u);
+        int pv=findPar(v);
+        if(pu==pv) return;
+        if(size[pu]<size[pv]){
+            swap(pu,pv);
         }
-
-        return parent[node] = find(parent[node]);
+        
+        par[pv]=pu;
+        size[pu]+=size[pv];
     }
-
-    void unite(int u, int v) {
-        int ulp_u = find(u);
-        int ulp_v = find(v);
-        if (ulp_u == ulp_v) {
-            return;
-        }
-
-        if (size[ulp_u] < size[ulp_v])
-            swap(ulp_u, ulp_v);
-
-        parent[ulp_v] = ulp_u;
-        size[ulp_u] += size[ulp_v];
+    
+    int findPar(int u){
+        if(u==par[u]) return u;
+        
+        return par[u]=findPar(par[u]);
     }
 };
 
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        DisjointSet ds(n);
-        int extraEdges = 0;
-        for (auto it : connections) {
-            int u = it[0];
-            int v = it[1];
-
-            if (ds.find(u) == ds.find(v)) {
-                extraEdges++;
-            } else {
-                ds.unite(u, v);
-            }
+        DSU dsu(n);
+        int extra=0;
+        for(auto con:connections){
+            int u=con[0];
+            int v=con[1];
+            if(dsu.findPar(u)==dsu.findPar(v)) extra++;
+            else dsu.unite(u,v);
         }
-
-        int components = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (ds.parent[i] == i) {
-                components++;
-            }
+        int comp=0;
+        for(int i=0;i<n;i++){
+            if(dsu.findPar(i)==i) comp++;
         }
-
-        int min_req = components - 1;
-
-        if (extraEdges >= min_req) {
-            return min_req;
-        } else {
-            return -1;
-        }
+        if(extra>=comp-1) return comp-1;
+        return -1;
     }
 };
