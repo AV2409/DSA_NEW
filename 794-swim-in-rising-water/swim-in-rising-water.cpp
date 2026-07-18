d6163e89836392b26ddb423074815ca45c94d7fd
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-18 16:17:07 +0530

    Time: 4 ms (77.40%) | Memory: 13.4 MB (57.21%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,63 +1,65 @@
 class DSU {
 public:
-    vector<int> parent;
+    vector<int> par;
     vector<int> size;
-    DSU(int n) {
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
+    int n;
+    DSU(int _n) {
+        n = _n;
+        par.resize(_n);
+        size.assign(_n, 1);
+        for (int i = 0; i < _n; i++)
+            par[i] = i;
     }
-    int find(int node) {
-        if (node == parent[node]) {
-            return node;
-        }
-        return parent[node] = find(parent[node]);
-    }
+
     void unite(int u, int v) {
-        int ulp_u = find(u);
-        int ulp_v = find(v);
-        if (ulp_u == ulp_v) {
+        int pu = findPar(u);
+        int pv = findPar(v);
+        if (pu == pv)
             return;
+        if (size[pu] < size[pv]) {
+            swap(pu, pv);
         }
-        if (size[ulp_u] < size[ulp_v])
-            swap(ulp_u, ulp_v);
-        parent[ulp_v] = ulp_u;
-        size[ulp_u] += size[ulp_v];
+
+        par[pv] = pu;
+        size[pu] += size[pv];
+    }
+
+    int findPar(int u) {
+        if (u == par[u])
+            return u;
+
+        return par[u] = findPar(par[u]);
     }
 };
 
 class Solution {
 public:
-    // minimize max value of a path
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<pair<int, int>> pos(n * n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                pos[grid[i][j]] = {i, j};
+        int n=grid.size();
+        vector<pair<int,int>>hash(n*n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                hash[grid[i][j]]={i,j};
             }
         }
-        vector<vector<int>> vis(n, vector<int>(n, 0));
-        int dx[4] = {1, -1, 0, 0};
-        int dy[4] = {0, 0, 1, -1};
 
-        DSU dsu(n * n);
-        for (int t = 0; t < n * n; t++) {
-            auto [x, y] = pos[t];
-            vis[x][y] = 1;
-            int id = x * n + y;
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n && vis[nx][ny]) {
-                    dsu.unite(id, nx * n + ny);
+        DSU dsu(n*n);
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        vector<int>dx={0,0,1,-1};
+        vector<int>dy={1,-1,0,0};
+        for(int t=0;t<n*n;t++){
+            auto [x,y]=hash[t];
+            int og=n*x+y;
+            vis[x][y]=1;
+
+            for(int dir=0;dir<4;dir++){
+                int nx=x+dx[dir];
+                int ny=y+dy[dir];
+                if(nx>=0 && ny>=0 && nx<n && ny<n && vis[nx][ny]){
+                    dsu.unite(og,nx*n+ny);
                 }
             }
-
-            if (dsu.find(0) == dsu.find(n * n - 1))
-                return t;
+            if(dsu.findPar(0)==dsu.findPar(n*n-1)) return t;
         }
         return -1;
     }
