d5db16269cf241aa8e3ce27405f6f684237ef708
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-18 16:28:00 +0530

    Time: 197 ms (57.94%) | Memory: 121.3 MB (57.24%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,30 +1,34 @@
 class DSU {
 public:
-    vector<int> parent;
+    vector<int> par;
     vector<int> size;
-    DSU(int n) {
-        size.resize(n, 1);
-        parent.resize(n);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
+    int n;
+    DSU(int _n) {
+        n = _n;
+        par.resize(_n);
+        size.assign(_n, 1);
+        for (int i = 0; i < _n; i++)
+            par[i] = i;
     }
-    int find(int node) {
-        if (node == parent[node]) {
-            return node;
-        }
-        return parent[node] = find(parent[node]);
-    }
+
     void unite(int u, int v) {
-        int ulp_u = find(u);
-        int ulp_v = find(v);
-        if (ulp_u == ulp_v) {
+        int pu = findPar(u);
+        int pv = findPar(v);
+        if (pu == pv)
             return;
+        if (size[pu] < size[pv]) {
+            swap(pu, pv);
         }
-        if (size[ulp_u] < size[ulp_v])
-            swap(ulp_u, ulp_v);
-        parent[ulp_v] = ulp_u;
-        size[ulp_u] += size[ulp_v];
+
+        par[pv] = pu;
+        size[pu] += size[pv];
+    }
+
+    int findPar(int u) {
+        if (u == par[u])
+            return u;
+
+        return par[u] = findPar(par[u]);
     }
 };
 
@@ -33,48 +37,44 @@ public:
     int largestIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         DSU dsu(n * n);
-        vector<int> dx = {0, 0, -1, 1};
+        queue<pair<int, int>> q;
+        vector<int> dx = {0, 0, 1, -1};
         vector<int> dy = {1, -1, 0, 0};
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    int node=i*n+j;
+                int id = i * n + j;
+                if (grid[i][j] == 0)
+                    q.push({i, j});
+                else {
                     for (int dir = 0; dir < 4; dir++) {
-                        int ni = i + dx[dir];
-                        int nj = j + dy[dir];
-                        if (ni >= 0 && nj >= 0 && ni < n && nj < n && grid[ni][nj] == 1) {
-                            int adjNode = ni * n + nj;
-                            dsu.unite(node, adjNode);
+                        int nx = i + dx[dir];
+                        int ny = j + dy[dir];
+                        if (nx >= 0 && ny >= 0 && nx < n && ny < n &&
+                            grid[nx][ny]) {
+                            dsu.unite(id, nx * n + ny);
                         }
                     }
                 }
             }
         }
-        int ans=0;
-        for(int i=0;i<n*n;i++){
-            ans=max(ans,dsu.size[i]);
-        }
+        int ans = *max_element(dsu.size.begin(),dsu.size.end());
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    int node=i*n+j;
-                    unordered_set<int>st;
-                    for (int dir = 0; dir < 4; dir++) {
-                        int ni = i + dx[dir];
-                        int nj = j + dy[dir];
-                        if (ni >= 0 && nj >= 0 && ni < n && nj < n && grid[ni][nj] == 1) {
-                            int adjNode = ni * n + nj;
-                            st.insert(dsu.find(adjNode));
-                        }
-                    }
-                    int temp=1;
-                    for(int x:st){
-                        temp+=dsu.size[x];
-                    }
-                    ans=max(ans,temp);
+            set<int> pars;
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n && grid[nx][ny]) {
+                    pars.insert(dsu.findPar(nx*n+ny));
                 }
             }
+            int cnt=0;
+            for(int p:pars){
+                cnt+=dsu.size[p];
+            }
+            ans=max(ans,cnt+1);
         }
         return ans;
     }
