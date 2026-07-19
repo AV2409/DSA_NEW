5e668c62ed79a196bd967cae1c2463c3c6a6f9b5
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-19 13:37:28 +0530

    Time: 139 ms (10.29%) | Memory: 54.8 MB (9.62%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
+    int n1;
+    int n2;
     vector<vector<int>>dp;
-    bool f(int i, int j, string& s, string& p) {
-        if (i < 0 && j < 0)
-            return true;
-        // s finished
-        if (i < 0) {
-            while (j >= 0 && p[j] == '*')
-                j--;
-            return j < 0;
-        }
-        // p finished
-        if (j < 0) {
+    bool f(int i,int j,string &s, string &p){
+        if(i>=n1) {
+            while(j<n2 && p[j]=='*') j++;
+            if(j==n2) return true;
             return false;
         }
+        if(j>=n2) return false;
         if(dp[i][j]!=-1) return dp[i][j];
-        if (p[j] == '*') {
-            // bool op1 = f(i - 1, j - 1, s, p);
-            bool op2 = f(i - 1, j, s, p);
-            bool op3 = f(i, j - 1, s, p);
-            return dp[i][j]=  op2 || op3;
-        } else if (p[j] == '?') {
-            return dp[i][j]= f(i - 1, j - 1, s, p);
-        }
+        if(p[j]=='*'){
+            //0 char 
+            bool op1=f(i,j+1,s,p);
+            //1 or more
+            bool op2=f(i+1,j,s,p);
 
-        if (s[i] == p[j])
-            return dp[i][j]= f(i - 1, j - 1, s, p);
-        return dp[i][j]= false;
+            return dp[i][j]= op1||op2;
+        }
+        else if(p[j]=='?'){
+            return dp[i][j]= f(i+1,j+1,s,p);
+        }
+        else{
+            if(s[i]==p[j]) return f(i+1,j+1,s,p);
+            return dp[i][j]= false;
+        }
     }
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        dp.assign(n,vector<int>(m,-1));
-        return f(n - 1, m - 1, s, p);
+        n1 = s.size();
+        n2 = p.size();
+        dp.assign(n1,vector<int>(n2,-1));
+        return f(0, 0, s, p);
     }
 };
