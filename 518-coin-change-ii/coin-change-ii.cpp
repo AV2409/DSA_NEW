e588d0bb2edb9be8f23525b5366bf1794260cab9
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-19 15:31:11 +0530

    Time: 31 ms (49.54%) | Memory: 36.7 MB (39.11%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int f(int i, int amt, vector<int>& coins) {
-        if (i == 0) {
-            if (amt % coins[0] == 0)
-                return 1;
-            else
-                return 0;
-        }
-        if (dp[i][amt] != -1)
-            return dp[i][amt];
-        int np = f(i - 1, amt, coins);
-        int p = 0;
-        if (coins[i] <= amt) {
-            p = f(i, amt - coins[i], coins);
+    vector<vector<int>>dp;
+    int f(int i,vector<int>& coins,int amount){
+        if(i<0) {
+            if(amount==0) return 1;
+            return 0;
         }
 
-        return dp[i][amt] = p+np;
+        if(dp[i][amount]!=-1) return dp[i][amount];
+
+        int np=f(i-1,coins,amount);
+        int p=0;
+        if(amount>=coins[i]) p=f(i,coins,amount-coins[i]);
+
+        return dp[i][amount]= p+np;
     }
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        sort(coins.begin(),coins.end());
-        dp.assign(n, vector<int>(amount + 1, -1));
-        int x = f(n - 1, amount, coins);
-        return x;
+        // if(amount == 0) return 0;
+        int n=coins.size();
+        dp.assign(n,vector<int>(amount+1,-1));
+        int ans=f(n-1,coins,amount);
+        return ans;
     }
 };
