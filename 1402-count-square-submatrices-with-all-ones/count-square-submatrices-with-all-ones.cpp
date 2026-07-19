5ab8e78a544995836475b1da06a4a844efc73a84
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-19 15:47:44 +0530

    Time: 7 ms (56.06%) | Memory: 30.5 MB (22.30%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-        vector<int> prev(m + 1, 0);
-        vector<int> curr(m + 1, 0);
-        int sq = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (matrix[i - 1][j - 1] == 1) {
-                    curr[j] = 1 + min({prev[j], prev[j - 1], curr[j - 1]});
-                    sq += curr[j];
+        int ans=0;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(matrix[i-1][j-1]==1){
+                    dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+                    ans+=dp[i][j];
                 }
+                
             }
-
-            prev = curr;
-            curr = vector<int>(m+1,0);
         }
+        return ans;
+        
 
-        return sq;
     }
 };
