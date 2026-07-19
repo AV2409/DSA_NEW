4cb75c88b65fa072564e207ceb189e475b26ba33
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-19 13:14:01 +0530

    Time: 27 ms (36.61%) | Memory: 26.8 MB (71.77%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        int m=str1.size();
+        int n=str2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(str1[i-1]==str2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
+        string ans="";
 
-        string ans;
-        int i = n;
-        int j = m;
-        while (i > 0 && j > 0) {
-            if (str1[i - 1] == str2[j - 1]) {
-                ans += str1[i - 1];
+        int i=m;
+        int j=n;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                ans+=str1[i-1];
                 i--;
                 j--;
             }
+
+            else if(dp[i][j-1]>dp[i-1][j]){
+                ans+=str2[j-1];
+                j--;
+            }
             else{
-                if(dp[i-1][j]>dp[i][j-1]){
-                    ans+=str1[i-1];
-                    i--;
-                }
-                else{
-                    ans+=str2[j-1];
-                    j--;
-                }
+                ans+=str1[i-1];
+                i--;
             }
         }
 
