aa03743529838adae465ae509e30e8e383fec4db
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-19 13:27:08 +0530

    Time: 7 ms (60.99%) | Memory: 14.2 MB (19.40%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int f(int i,int j,string &word1, string &word2){
-
+    int f(int i,int j,string &w1, string &w2){
         if(i<0 && j<0) return 0;
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+        if(i<0 || j<0){
+            if(i<0) return j+1;
+            return i+1;
+        }
         if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i]==word2[j]) return dp[i][j]= f(i-1,j-1,word1,word2);
 
-        return dp[i][j]= min({1+f(i,j-1,word1,word2),1+f(i-1,j,word1,word2),1+f(i-1,j-1,word1,word2)});
+        int ans=INT_MAX;
+        if(w1[i]==w2[j]){
+            ans=min(ans,f(i-1,j-1,w1,w2));
+        }
+        else{
+            //delete
+            int op1=1+f(i-1,j,w1,w2);
+            //insert
+            int op2=1+f(i,j-1,w1,w2);
+            //replace
+            int op3=1+f(i-1,j-1,w1,w2);
+
+            int mini=min({op1,op2,op3});
+            ans=min(ans,mini);
+        }
+
+        return dp[i][j]=  ans;
     }
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        dp.assign(n,vector<int>(m,-1));
-        return f(n-1,m-1,word1,word2);
+        int n1=word1.size();
+        int n2=word2.size();
+        dp.assign(n1,vector<int>(n2,-1));
+        return f(n1-1,n2-1,word1,word2);
     }
 };
