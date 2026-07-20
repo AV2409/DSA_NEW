dc006fbcbf6ad00b51e4d3c873169673300d5206
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-20 23:08:55 +0530

    Time: 1 ms (83.71%) | Memory: 11.3 MB (83.46%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> hash(256, 0);
-        int cnt = 0;
-        for (char ch : t) {
-            if (hash[ch] == 0)
-                cnt++;
-            hash[ch]++;
+        int m=s.size();
+        int n=t.size();
+        int cnt1=0;
+        vector<int>hash(256,0);
+        for(char c:t) {
+            if(hash[c]==0) cnt1++;
+            hash[c]++;
         }
 
-        int l = 0;
-        int r = 0;
-        int n = s.size();
-        int cnt2 = 0;
-        int minLen = INT_MAX;
-        int st = -1;
-        while (r < n) {
+        int l=0;
+        int cnt2=0;
+        int st=-1;
+        int minlen=INT_MAX;
+        for(int r=0;r<m;r++){
             hash[s[r]]--;
-            if (hash[s[r]] == 0)
-                cnt2++;
-            while (cnt2 == cnt) {
-                int len = r - l + 1;
-                if (len < minLen) {
-                    minLen = len;
-                    st = l;
+            if(hash[s[r]]==0) cnt2++;
+            while(cnt1==cnt2){
+                int len=r-l+1;
+                if(len<minlen){
+                    minlen=len;
+                    st=l;
                 }
-                if (hash[s[l]] == 0)
-                    cnt2--;
+                if(hash[s[l]]==0) cnt2--;
                 hash[s[l]]++;
                 l++;
             }
-            r++;
         }
-        if (minLen == INT_MAX)
-            return "";
-        return s.substr(st, minLen);
+        if(st==-1) return "";
+        return s.substr(st,minlen);
     }
 };
