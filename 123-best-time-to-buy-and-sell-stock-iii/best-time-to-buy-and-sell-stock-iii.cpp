cbcf2c5eb04d871d3e72e68c4d5049faf1c410e0
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-20 12:07:48 +0530

    Time: 447 ms (28.24%) | Memory: 223.7 MB (30.97%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-//buy=1 -->can buy
-//buy=0--> not buy
-    int f(int i, int buy, int trans, int n, vector<int>& prices,vector<vector<vector<int>>> &dp) {
-        if(i==n) return 0;
-        if(trans==2) return 0;
+    int n;
+    vector<vector<vector<int>>>dp;
 
-        if(dp[i][trans][buy]!=-1) return dp[i][trans][buy];
-        if(buy){
-            int op1=f(i+1,1,trans,n,prices,dp);
-            int op2=-prices[i]+f(i+1,0,trans,n,prices,dp);
-            return dp[i][trans][buy]=max(op1,op2);
+    int f(int i,bool bought,int trans,vector<int>& prices){
+        if(i==n) return 0;
+        if(trans>=2) return 0;
+        if(dp[i][bought][trans]!=-1) return dp[i][bought][trans];
+        if(bought){
+            //sell
+            int op1=prices[i]+f(i+1,false,trans+1,prices);
+
+            //skip
+            int op2=f(i+1,bought,trans,prices);
+
+            return dp[i][bought][trans]= max(op1,op2);
         }
-        else{
-            int op1=f(i+1,0,trans,n,prices,dp);
-            int op2=+prices[i]+f(i+1,1,trans+1,n,prices,dp);
-            return dp[i][trans][buy]=max(op1,op2);
-        }
-        return 0;
+        //buy
+        int op1=-prices[i]+f(i+1,true,trans,prices);
+        int op2=f(i+1,bought,trans,prices);
+
+        return dp[i][bought][trans]= max(op1,op2);
     }
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n + 1,
-                                       vector<vector<int>>(3, vector<int>(2,-1)));
-        
-        return f(0, 1, 0, n, prices, dp);
+        n=prices.size();
+        dp.assign(n,vector<vector<int>>(2,vector<int>(2,-1)));
+        return f(0,false,0,prices);
     }
 };
