a2375eb6dbcad35613462e32bb32e2ab3982774f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-20 14:05:27 +0530

    Time: 158 ms (22.72%) | Memory: 291.7 MB (9.83%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
-    int start = 0;
-    int maxLen = 1;
+    vector<vector<int>> ispal;
+    int n;
+    void check(int mid, string& s) {
+        // odd length;
+        int i = mid;
+        int j = mid;
+        while (i >= 0 && j < n && s[i] == s[j]) {
+            ispal[i][j] = 1;
+            i--;
+            j++;
+        }
 
-    void expand(int left, int right, string& s) {
-
-        while (left >= 0 && right < s.size() && s[left] == s[right]) {
-            int len = right - left + 1;
-            if (len > maxLen) {
-                maxLen = len;
-                start = left;
-            }
-            left--;
-            right++;
+        // even length;
+        i = mid - 1;
+        j = mid;
+        while (i >= 0 && j < n && s[i] == s[j]) {
+            ispal[i][j] = 1;
+            i--;
+            j++;
         }
     }
     string longestPalindrome(string s) {
-        int ans = 0;
-        int n = s.size();
-        for (int i = 0; i < s.size(); i++) {
-            expand(i, i, s);
-            expand(i, i + 1, s);
+        n = s.size();
+        int start = 0;
+        int maxLen = 1;
+
+        ispal.assign(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            check(i,s);
         }
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(ispal[i][j]) {
+                    int len=j-i+1;
+                    if(len>maxLen){
+                        maxLen=len;
+                        start=i;
+                    }
+                }
+            }
+        }
+
         return s.substr(start, maxLen);
     }
 };
