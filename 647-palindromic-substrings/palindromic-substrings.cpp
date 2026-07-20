88ba4036b3a48c28d8838b9425d9e3939b9c57da
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-20 13:51:08 +0530

    Time: 23 ms (39.17%) | Memory: 34.5 MB (21.29%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    bool f(int i, int j,string &s) {
-        if (i >= j)
-            return true;
+    vector<vector<int>>ispal;
+    int n;
+    void check(int mid,string &s){
+        //odd length;
+        int i=mid;
+        int j=mid;
+        while(i>=0 && j<n && s[i]==s[j]){
+            ispal[i][j]=1;
+            i--;
+            j++;
+        }
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (s[i] != s[j])
-            return dp[i][j] = false;
-
-        return dp[i][j] = f(i + 1, j - 1,s);
+        //even length;
+        i=mid-1;
+        j=mid;
+        while(i>=0 && j<n && s[i]==s[j]){
+            ispal[i][j]=1;
+            i--;
+            j++;
+        }
     }
+
     int countSubstrings(string s) {
-        int n=s.size();
-        dp.assign(n,vector<int>(n,-1));
-        int cnt=0;
+        n=s.size();
+        ispal.assign(n,vector<int>(n,0));
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(f(i,j,s)) cnt++;
+            check(i,s);
+        }
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(ispal[i][j]) ans++;
             }
         }
-        return cnt;
+        return ans;
     }
 };
