e9e29f2bef5ed7c74888927d0a932f8dd8f3ee1e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-20 12:13:08 +0530

    Time: 122 ms (28.74%) | Memory: 107.6 MB (34.55%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-// buy=1 -->can buy
-    // buy=0--> not buy
-    int f(int i, int buy, int n, vector<int>& prices,
-          vector<vector<int>>& dp,int fee) {
-        if (i >= n)
+    int n;
+    int tfee;
+    vector<vector<int>> dp;
+    int f(int i, bool bought, vector<int>& prices) {
+        if (i == n)
             return 0;
+        if (dp[i][bought] != -1)
+            return dp[i][bought];
+        if (bought) {
+            // sell
+            int op1 = prices[i]-tfee + f(i + 1, false, prices);
 
-        if (dp[i][buy] != -1)
-            return dp[i][buy];
-        if (buy) {
-            int op1 = f(i + 1, 1, n, prices, dp,fee);
-            int op2 = -prices[i] + f(i + 1, 0, n, prices, dp,fee);
-            return dp[i][buy] = max(op1, op2);
-        } else {
-            int op1 = f(i + 1, 0, n, prices, dp,fee);
-            int op2 = prices[i] + f(i + 1, 1, n, prices, dp,fee)-fee;
-            return dp[i][buy] = max(op1, op2);
+            // skip
+            int op2 = f(i + 1, bought, prices);
+
+            return dp[i][bought] = max(op1, op2);
         }
-        return 0;
+        // buy
+        int op1 = -prices[i] + f(i + 1, true, prices);
+        int op2 = f(i + 1, bought, prices);
+
+        return dp[i][bought] = max(op1, op2);
     }
     int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-
-        return f(0, 1, n, prices, dp,fee);
+        n = prices.size();
+        tfee=fee;
+        dp.assign(n, vector<int>(2, -1));
+        return f(0, false, prices);
     }
 };
