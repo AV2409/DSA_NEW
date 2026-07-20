3d64a71d4ff777e7cdd138e948cb6ebf5c198520
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-20 22:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (93.68%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans="";
-        int n=strs.size();
-        int mini=201;
-        for(int i=0;i<n;i++){
-            int x=strs[i].size();
-            mini=min(mini,x);
-        }
+    string find(string& w1, string& w2) {
+        string ans;
+        int n1 = w1.size();
+        int n2 = w2.size();
 
-        for(int i=0;i<mini;i++){
-            char ch=strs[0][i];
-            for(int j=1;j<n;j++){
-                string word=strs[j];
-                if(word[i]!=ch) return ans;
+        int i = 0;
+        int j = 0;
+        while (i < n1 && j < n2 && w1[i] == w2[j]) {
+            ans += w1[i];
+            i++;
+            j++;
+        }
+        return ans;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        string ans = "";
+        int mini = 1e9;
+        for (int i = 0; i < n; i++) {
+            mini = min(mini, (int)strs[i].size());
+        }
+        for (int i = 0; i < mini; i++) {
+            char ch = strs[0][i];
+            for (int j = 0; j < n; j++) {
+                if(strs[j][i]!=ch) return ans;
             }
             ans+=ch;
         }
