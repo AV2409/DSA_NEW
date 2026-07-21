6413a52219625cac314847bb0bed0d26036c0ab2
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-21 17:30:13 +0530

    Time: 32 ms (50.97%) | Memory: 14.1 MB (7.98%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int minCost(int n, vector<int>& cuts) {
-        cuts.push_back(0);
-        cuts.push_back(n);
-        sort(cuts.begin(), cuts.end());
-        int m = cuts.size();
-        vector<vector<int>> dp(m, vector<int>(m, 0));
-
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = i + 2; j <= m - 1; j++) {
-                int temp = 1e9;
-                for (int k = i + 1; k < j; k++) {
-                    int cost = dp[i][k] + dp[k][j] +(cuts[j] - cuts[i]);
-                    temp = min(cost, temp);
-                }
-                dp[i][j] = temp;
-            }
+    vector<vector<int>>dp;
+    int f(int st,int end,vector<int>& arr){
+        if(st>=end-1) return 0;
+        if(dp[st][end]!=-1) return dp[st][end];
+        int ans=INT_MAX;
+        int len=arr[end]-arr[st];
+        for(int i=st+1;i<end;i++){
+            int temp=len+f(st,i,arr)+f(i,end,arr);
+            ans=min(ans,temp);
         }
-        return dp[0][m - 1];
+        return dp[st][end]= ans;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        vector<int>arr;
+        arr.push_back(0);
+        sort(cuts.begin(),cuts.end());
+        for(int x:cuts) arr.push_back(x);
+        arr.push_back(n);
+        int ss=arr.size();
+        dp.assign(ss,vector<int>(ss,-1));
+        return f(0,ss-1,arr);
+
     }
 };
