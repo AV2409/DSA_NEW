4b8c781937d905ee62c5d67ae1abd91c7ac5ea71
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-21 17:10:29 +0530

    Time: 206 ms (25.15%) | Memory: 14 MB (6.93%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int f(int i,int j,vector<int>& nums){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans=0;
-        for(int k=i;k<=j;k++){
-            int temp=f(i,k-1,nums)+f(k+1,j,nums)+nums[i-1]*nums[k]*nums[j+1];
+    int f(int st,int end,vector<int>&arr){
+        if(st>end) return 0;
+        if(dp[st][end]!=-1) return dp[st][end];
+        int ans=INT_MIN;
+        for(int i=st;i<=end;i++){
+            int temp=arr[st-1]*arr[i]*arr[end+1]+f(st,i-1,arr)+f(i+1,end,arr);
             ans=max(ans,temp);
         }
-        return dp[i][j] = ans;
+        return dp[st][end]= ans;
     }
     int maxCoins(vector<int>& nums) {
-        vector<int>x={1};
-        for(int i:nums){
-            x.push_back(i);
-        }
-        x.push_back(1);
-        int i=1;
-        int n = nums.size();
-        dp.assign(n+1,vector<int>(n+1,-1));
-        return f(i,n,x);
+        vector<int>arr;
+        arr.push_back(1);
+        for(int x:nums) arr.push_back(x);
+        arr.push_back(1);
+
+        int st=1;
+        int end=nums.size();
+
+        dp.assign(end+1,vector<int>(end+1,-1));
+
+        return f(st,end,arr);
     }
 };
