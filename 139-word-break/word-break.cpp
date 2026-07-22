5f0dbabbd213e6cc9755de6e6b6683cf398ac949
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-22 17:20:17 +0530

    Time: 4 ms (61.31%) | Memory: 16.8 MB (29.34%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,25 +1,50 @@
+class TrieNode{
+public:
+    TrieNode* children[26];
+    bool terminal;
+    TrieNode(){
+        for(int i=0;i<26;i++){
+            children[i]=NULL;
+        }
+        terminal=false;
+    }
+};
+
 class Solution {
 public:
-    unordered_set<string>dict;
-    int n;
-    vector<int>dp;
-    bool solve(string &s,int idx){
-        if(idx==n) return true;
-        if(dp[idx]!=-1) return dp[idx];
-        string x="";
-        bool ans=false;
-        for(int i=idx;i<n;i++){
-            x+=s[i];
-            if(dict.count(x)){
-                ans=ans||solve(s,i+1);
+    TrieNode* root=new TrieNode();
+    void insert(string word) {
+        TrieNode* temp=root;
+        for(char ch:word){
+            int idx=ch-'a';
+            if(temp->children[idx]==NULL){
+                temp->children[idx]=new TrieNode();
             }
+            temp=temp->children[idx];
         }
-        return dp[idx]= ans;
+        temp->terminal=true;
     }
+    unordered_map<int,unordered_map<TrieNode*,bool>>mp;
+    bool find(int idx,TrieNode* head,string &s){
+        if(idx==s.size()) return head == root || head->terminal;
+        if (mp[idx].count(head))
+            return mp[idx][head];
+        int i=s[idx]-'a';
+        if(head->children[i]==NULL) return false;
+
+        TrieNode* temp=head->children[i];
+        //start from root;
+        bool op1=false;
+        if(temp->terminal) op1=find(idx+1,root,s);
+        bool op2=find(idx+1,temp,s);
+        return mp[idx][head]= op1||op2;
+    }
+
     bool wordBreak(string s, vector<string>& wordDict) {
-        for(string x:wordDict) dict.insert(x);
-        n=s.size();
-        dp.assign(n,-1);
-        return solve(s,0);
+        for(string &w:wordDict){
+            insert(w);
+        }
+
+        return find(0,root,s);
     }
 };
