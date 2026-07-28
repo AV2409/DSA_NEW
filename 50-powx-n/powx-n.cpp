e1418a87769530391be381206ea202a9d5eb4586
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-28 11:33:15 +0530

    Time: 3 ms (2.15%) | Memory: 8.5 MB (93.45%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    double helper(double x, long long n) {
-        if (n == 0)
-            return 1;
-        double tt = helper(x, n / 2);
-        if (n % 2) {
-            return x * tt * tt;
-        }
-        return tt * tt;
+    double pow(double x, long long n){
+        if(n==0) return 1;
+        if(n==1) return x;
+
+        double temp=pow(x,n/2);
+        double ans=temp*temp;
+
+        if(n%2) ans=ans*x;
+        return ans;
     }
     double myPow(double x, int n) {
-        if(x==1||n==0) return 1;
+        if(n==0) return 1;
+        if(x==1) return 1;
         if(x==-1){
             if(n%2) return -1;
             else return 1;
         }
-        long long pow=n;
-        if(x==0) return 0;
-        if (n < 0) {
-            x = 1 / x;
-            pow=-pow;
+        long long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
         }
-        return helper(x, pow);
+        return pow(x,N);
     }
 };
