516ec7104235aecbf08d64366da3c32458606ff7
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-29 11:56:27 +0530

    Time: 2 ms (73.14%) | Memory: 10.7 MB (28.05%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -3,23 +3,24 @@ public:
     vector<int>cols;
     int N;
     vector<vector<string>>ans;
-    
-    void storeAns(){
-        vector<string>temp(N,string(N,'.'));
-        int x=cols.size();
-        for(int r=0;r<x;r++){
-            int c=cols[r];
-            temp[r][c]='Q';
-        }
-        ans.push_back(temp);
-    }
+
     bool isPossible(int row,int col){
-        int x=cols.size();
-        for(int r=0;r<x;r++){
+        int ss=cols.size();
+        for(int r=0;r<ss;r++){
             int c=cols[r];
-            if(c==col||abs(row-r)==abs(col-c)) return false;
+            if(c==col) return false;
+
+            if(abs(row-r)==abs(col-c)) return false;
         }
         return true;
+    }
+    void storeAns(){
+        vector<string>str(N,string(N,'.'));
+        for(int r=0;r<N;r++){
+            int c=cols[r];
+            str[r][c]='Q';
+        }
+        ans.push_back(str);
     }
     void solve(int row){
         if(row==N){
@@ -28,7 +29,7 @@ public:
         }
 
         for(int col=0;col<N;col++){
-            if(isPossible(row,col)) {
+            if(isPossible(row,col)){
                 cols.push_back(col);
                 solve(row+1);
                 cols.pop_back();
