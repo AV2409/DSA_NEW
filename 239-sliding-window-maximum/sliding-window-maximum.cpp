f591e241cf94a58bb15bc15b0629d3160895eee6
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-29 16:33:48 +0530

    Time: 27 ms (41.81%) | Memory: 139.3 MB (46.09%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -6,11 +6,10 @@ public:
         vector<int>ans;
         for(int i=0;i<n;i++){
             while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
-            while(!dq.empty() && nums[dq.back()]<=nums[i]) dq.pop_back();
+            while(!dq.empty() && nums[i]>=nums[dq.back()]) dq.pop_back();
 
             dq.push_back(i);
-            if(i>=k-1)
-                ans.push_back(nums[dq.front()]);
+            if(i+1>=k) ans.push_back(nums[dq.front()]);
         }
         return ans;
     }
