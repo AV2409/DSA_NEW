1197c39de88785e4b3e78a12c9532b2146e24f9c
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-07-30 15:08:36 +0530

    Time: 3 ms (73.20%) | Memory: 106.5 MB (39.26%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -6,36 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+struct Node{
+    bool isBst;
+    long long sum;
+    long long mini;
+    long long maxi; 
+};
 class Solution {
 public:
-    struct Node {
-        bool isBst;
-        int sum = 0;
-        long long mini;
-        long long maxi;
-    };
-
-    int ans = 0;
-    Node helper(TreeNode* root) {
-        if (!root)
-            return {true, 0, INT_MAX, INT_MIN};
-
-        Node l = helper(root->left);
-        Node r = helper(root->right);
-
-        if (l.isBst && r.isBst && root->val > l.maxi && root->val < r.mini) {
-            ans = max(ans, root->val + l.sum + r.sum);
-            long long mini = min((long long)root->val, l.mini);
-            long long maxi = max((long long)root->val, r.maxi);
-
-            return {true, root->val + l.sum + r.sum, mini, maxi};
-            return {true, root->val + l.sum + r.sum, mini, maxi};
+    long long ans=0;
+    Node helper(TreeNode* root){
+        if(!root){
+            return {true,0,INT_MAX,INT_MIN};
         }
-        return {false, 0, INT_MIN, INT_MAX};
+
+        Node l=helper(root->left);
+        Node r=helper(root->right);
+
+        if(l.isBst && r.isBst && root->val>l.maxi && root->val<r.mini){
+            ans=max(ans,root->val+l.sum+r.sum);
+            long long mini=min(1LL*root->val,l.mini);
+            long long maxi=max(1LL*root->val,r.maxi);
+            return {true,root->val+l.sum+r.sum,mini,maxi};
+        }
+        return {false,0,INT_MIN,INT_MAX};
     }
     int maxSumBST(TreeNode* root) {
         helper(root);
