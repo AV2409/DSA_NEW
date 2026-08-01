f3099b258ecad7d167a0acc44dae5bfa02a95a4b
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-01 14:13:48 +0530

    Time: 2 ms (88.16%) | Memory: 15.4 MB (54.52%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int f(int i, int sum,vector<int>& nums){
-        if(i<0) return sum==0;
-        if(dp[i][sum]!=-1) return dp[i][sum];
-        int np=f(i-1,sum,nums);
-        int p=0;
-        if(nums[i]<=sum) p=f(i-1,sum-nums[i],nums);
-        return dp[i][sum]= p+np;
-    }
+    vector<vector<int>> dp;
+    int f(int i, vector<int>& arr, int target) {
+        // if(target==0) return 1;
+        if (i < 0)
+            return target == 0;
+        if (dp[i][target] != -1)
+            return dp[i][target];
+        int np = f(i - 1, arr, target);
+        int p = 0;
+        if (target - arr[i] >= 0)
+            p = f(i - 1, arr, target - arr[i]);
 
-    int perfectSum(vector<int>& nums,int sum){
-        int n=nums.size();
-        dp.assign(n,vector<int>(sum+1,-1));
-        return f(n-1,sum,nums);
+        return dp[i][target] = p + np;
+    }
+    int perfectSum(vector<int>& arr, int target) {
+        // code here
+        int n = arr.size();
+        dp.assign(n, vector<int>(target + 1, -1));
+        return f(n - 1, arr, target);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
@@ -21,10 +26,13 @@ public:
         for (int i = 0; i < n; i++) {
             sum += nums[i];
         }
-        sum-=target;
-        if ((sum) < 0 || (sum) % 2 != 0) {
-            return 0;
-        }
-        return perfectSum(nums, sum/2);
+
+        if(target>sum) return 0;
+        int reqSum=sum-target;
+        if((reqSum)%2) return 0;
+        reqSum/=2;
+        
+        return perfectSum(nums,reqSum);
+        
     }
 };
