79943bcfb13f70ea5581b0848e9d6ad8e13fbcaa
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-01 09:48:45 +0530

    Time: 7 ms (55.60%) | Memory: 12 MB (69.31%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int n;
     set<string>st;
+    int n;
     vector<int>dp;
-    bool f(int idx,string &s){
-        if(idx==n) return true;
-        if(dp[idx]!=-1) return dp[idx];
+    bool f(int i,string &s){
+        if(i>=n) return true;
+        if(dp[i]!=-1) return dp[i];
+        string temp="";
         bool ans=false;
-        string x="";
-        for(int i=idx;i<n;i++){
-            x+=s[i];
-            if(st.count(x)) ans=ans||f(i+1,s);
-            if(ans) return dp[idx]= true;
+        for(int part=i;part<n;part++){
+            temp+=s[part];
+            if(st.count(temp)){
+                ans=ans||f(part+1,s);
+            }
         }
-        return dp[idx]= ans;
+        return dp[i]= ans;
     }
-
     bool wordBreak(string s, vector<string>& wordDict) {
         n=s.size();
+        st.insert(wordDict.begin(),wordDict.end());
         dp.assign(n,-1);
-        for(string &w:wordDict){
-            st.insert(w);
-        }
-
         return f(0,s);
     }
 };
