a5916f6c6819c289f61c3afe9269677ef0e7eb15
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-02 06:06:09 +0530

    Time: 68 ms (71.20%) | Memory: 173.2 MB (68.22%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,80 +1,93 @@
 class Node {
 public:
-    Node* next;
-    Node* prev;
     int key;
     int val;
-
+    Node* next;
+    Node* prev;
     Node(int k, int v) {
-        this->key = k;
-        this->val = v;
-        this->next = NULL;
-        this->prev = NULL;
+        key = k;
+        val = v;
+        next=NULL;
+        prev=NULL;
     }
 };
 
-void insertAtHead(Node*& head, Node*& node) {
-    node->next = head->next;
-    head->next->prev = node;
-    head->next = node;
-    node->prev = head;
-}
-
-void delete_node(Node*& node) {
-    Node* nn = node->next;
-    Node* pp = node->prev;
-    pp->next = nn;
-    nn->prev = pp;
-}
-
 class LRUCache {
+
 public:
-    Node* head;
-    Node* tail;
+    Node* head = NULL;
+    Node* tail = NULL;
+    int cap;
     unordered_map<int, Node*> mp;
-    int cap = 0;
-    LRUCache(int capacity) {
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head->next = tail;
-        tail->prev = head;
-        cap = capacity;
+    LRUCache(int capacity) { cap = capacity; }
+
+    void deleteNode(Node* node) {
+        Node* prev = node->prev;
+        Node* next = node->next;
+        if (node == head) {
+            head = node->next;
+        }
+        if (node == tail) {
+            tail = node->prev;
+        }
+
+        if (prev)
+            prev->next = next;
+        if (next)
+            next->prev = prev;
+
+        
+        node->next = NULL;
+        node->prev = NULL;
     }
-
-    int get(int key) {
-        if (mp.find(key) == mp.end())
-            return -1;
-        int vall = mp[key]->val;
-        Node* node = mp[key];
-        delete_node(node);
-        insertAtHead(head, node);
-        return vall;
-    }
-
-    void put(int key, int value) {
-        if (mp.find(key) != mp.end()) {
-            Node* node = mp[key];
-            node->val = value;
-
-            // remove
-            delete_node(node);
-            insertAtHead(head, node);
+    void insertFront(Node* node) {
+        if (!head) {
+            head = node;
+            tail = node;
             return;
         }
 
-        else if (mp.size() < cap) {
-            Node* node = new Node(key, value);
-            insertAtHead(head, node);
-            mp[key] = node;
-            return;
+        node->next = head;
+        head->prev = node;
+        head = node;
+    }
 
-        } else if (mp.size() >= cap) {
-            Node* toDel = tail->prev;
-            delete_node(toDel);
-            mp.erase(toDel->key);
-            delete toDel;
+    void deleteBack() {
+        Node* toDel = tail;
+        int k = toDel->key;
+        mp.erase(k);
+        if (head == tail) {
+            head = tail = NULL;
+        } else {
+            tail = tail->prev;
+            tail->next = NULL;
+        }
+        toDel->prev = NULL;
+        delete toDel;
+    }
+
+    int get(int key) {
+        if (!mp.count(key))
+            return -1;
+        Node* node = mp[key];
+        deleteNode(node);
+        insertFront(node);
+        return node->val;
+    }
+
+    void put(int key, int value) {
+
+        if (mp.count(key)) {
+            Node* node = mp[key];
+            node->val = value;
+            deleteNode(node);
+            insertFront(node);
+        } else {
+            if (mp.size() == cap) {
+                deleteBack();
+            }
             Node* node = new Node(key, value);
-            insertAtHead(head, node);
+            insertFront(node);
             mp[key] = node;
         }
     }
