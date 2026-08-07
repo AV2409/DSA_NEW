a0e17acb8a1c2aa4723aa60ea178148d4e20d413
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-07 14:41:35 +0530

    Time: 1086 ms (5.03%) | Memory: 495.5 MB (5.14%) - LeetSync

--- 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.cpp
+++ 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.cpp
@@ -1,128 +1,94 @@
-class TrieNode {
-public:
-    vector<TrieNode*> children;
-    bool isTerminal;
+class TrieNode{
+    public:
+    vector<TrieNode*>child;
 
-    TrieNode() {
-        this->isTerminal = false;
-        children.assign(2, NULL);
-    }
-};
-
-class Trie {
-public:
-    TrieNode* root;
-    Trie() { root = new TrieNode(); }
-
-    void insert(string word) {
-        int n = word.size();
-        TrieNode* temp = root;
-        for (int i = 0; i < n; i++) {
-            int idx = word[i] - '0';
-            if (temp->children[idx] == NULL) {
-                temp->children[idx] = new TrieNode();
-            }
-            temp = temp->children[idx];
-        }
-        temp->isTerminal = true;
-    }
-
-    int findMax(string word) {
-        int n = word.size();
-        TrieNode* temp = root;
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans <<= 1;
-            int idx = word[i] - '0';
-            if (idx == 0) {
-                if (temp->children[1]) {
-                    temp = temp->children[1];
-                    ans |= 1;
-                } else if (temp->children[0]) {
-                    temp = temp->children[0];
-                } else
-                    break;
-            }
-            if (idx == 1) {
-                if (temp->children[0]) {
-                    temp = temp->children[0];
-                    ans |= 1;
-                } else if (temp->children[1]) {
-                    temp = temp->children[1];
-                } else
-                    break;
-            }
-        }
-        return ans;
+    TrieNode(){
+        child.assign(2,NULL);
     }
 };
 
 class Solution {
 public:
-    static bool comp(vector<int>& a, vector<int>& b) { return a[1] < b[1]; }
+    TrieNode* root=new TrieNode();
+    void insert(string word) {
+        TrieNode* temp=root;
+        for(char c:word){
+            int idx=c-'0';
+            if(temp->child[idx]==NULL){
+                temp->child[idx]=new TrieNode();
+            }
+            temp=temp->child[idx];
+        }
+    }
 
-    string numToBit(int n) {
-        string ans;
-        ans.assign(32, '0');
-        for (int i = 0; i < 32; i++) {
-            if (n & 1)
-                ans[31 - i] = '1';
-            else
-                ans[31 - i] = '0';
-            n = n / 2;
-            if (n == 0)
-                break;
+    int find(string word) {
+        TrieNode* temp=root;
+        int res=0;
+        for(int i=0;i<32;i++){
+            int idx=word[i]-'0';
+            int inv=1-idx;
+            if(temp->child[inv]){
+                res<<=1;
+                res|=1;
+                temp=temp->child[inv];
+            }
+            else if(temp->child[idx]){
+                res<<=1;
+                temp=temp->child[idx];
+            }
+            else return -1;
+        }
+        return res;
+    }
+    string toBit(int num){
+        string ans(32,'0');
+        int idx=31;
+        while(num>0){
+            int dig=num&1;
+            if(dig) ans[idx]='1';
+            num>>=1;
+            idx--;
         }
         return ans;
     }
 
-    int bitToNum(string x) {
-        int ans = 0;
-        for (char c : x) {
-            ans <<= 1;
-            ans |= (c - '0');
-        }
-        return ans;
+    static bool comp(vector<int>&a,vector<int>&b){
+        return a[1]<b[1];
     }
-
     vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
-        Trie* trie = new Trie();
-        sort(nums.begin(), nums.end());
-        int qs = queries.size();
-        vector<int> ans(qs, 0);
-        int numsIdx = 0;
-        int i = 0;
-        for (auto& q : queries) {
+        int i=0;
+        for(auto &q:queries){
             q.push_back(i);
             i++;
         }
-        vector<string> bitNums;
-        for (int x : nums) {
-            string s = numToBit(x);
-            bitNums.push_back(s);
+        int ss=queries.size();
+        vector<int>ans(ss,-1);
+
+        vector<string>strs;
+        sort(nums.begin(),nums.end());
+        for(int x:nums){
+            strs.push_back(toBit(x));
         }
-        sort(queries.begin(), queries.end(), comp);
-        int n = nums.size();
-        for (auto& q : queries) {
-            int num = q[0];
-            int maxi = q[1];
-            int idx = q[2];
 
-            while (numsIdx < n && nums[numsIdx] <= maxi) {
-                trie->insert(bitNums[numsIdx]);
-                numsIdx++;
+        sort(queries.begin(),queries.end(),comp);
+        int idx=0;
+        int n=nums.size();
+
+        for(auto q:queries){
+            int num=q[0];
+            string strNum=toBit(num);
+            int maxi=q[1];
+            int k=q[2];
+
+            while(idx<n && nums[idx]<=maxi){
+                insert(strs[idx]);
+                idx++;
             }
 
-            if (trie->root->children[0] == NULL &&
-                trie->root->children[1] == NULL) {
-                ans[idx] = -1;
-                continue;
-            }
-
-            string strNum = numToBit(num);
-            int res = trie->findMax(strNum);
-            ans[idx] = res;
+            int mm=find(strNum);
+            ans[k]=mm;
         }
         return ans;
+
     }
 };
