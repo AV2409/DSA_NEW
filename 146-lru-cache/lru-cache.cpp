681376fd050154fbf826831154069adbe2f9720e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-07 18:03:15 +0530

    Time: 71 ms (66.54%) | Memory: 173.1 MB (68.76%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -4,67 +4,57 @@ public:
     int val;
     Node* next;
     Node* prev;
-    Node(int k, int v) {
-        key = k;
-        val = v;
-        next=NULL;
-        prev=NULL;
+    Node(int k,int v) {
+        key=k;
+        val=v;
+        next = nullptr;
+        prev = nullptr;
     }
 };
 
 class LRUCache {
-
 public:
-    Node* head = NULL;
-    Node* tail = NULL;
+    Node* head = nullptr;
+    Node* tail = nullptr;
     int cap;
     unordered_map<int, Node*> mp;
-    LRUCache(int capacity) { cap = capacity; }
+    void insertFront(Node* node) {
+        node->next=head;
+        if(head) head->prev=node;
+        head=node;
+        if(tail==nullptr) tail=node;
+    }
 
     void deleteNode(Node* node) {
         Node* prev = node->prev;
         Node* next = node->next;
-        if (node == head) {
-            head = node->next;
-        }
-        if (node == tail) {
-            tail = node->prev;
-        }
 
         if (prev)
             prev->next = next;
         if (next)
             next->prev = prev;
-
         
-        node->next = NULL;
-        node->prev = NULL;
+        if(node==head) head=head->next;
+        if(node==tail) tail=tail->prev;
+
+        node->next=NULL;
+        node->prev=NULL;
     }
-    void insertFront(Node* node) {
-        if (!head) {
-            head = node;
-            tail = node;
+    void deleteBack(){
+        if(head==tail) {
+            delete tail;
+            head=nullptr;
+            tail=nullptr;
             return;
         }
 
-        node->next = head;
-        head->prev = node;
-        head = node;
-    }
-
-    void deleteBack() {
-        Node* toDel = tail;
-        int k = toDel->key;
-        mp.erase(k);
-        if (head == tail) {
-            head = tail = NULL;
-        } else {
-            tail = tail->prev;
-            tail->next = NULL;
-        }
-        toDel->prev = NULL;
+        Node* toDel=tail;
+        tail=tail->prev;
+        toDel->prev=nullptr;
+        tail->next=nullptr;
         delete toDel;
     }
+    LRUCache(int capacity) { cap = capacity; }
 
     int get(int key) {
         if (!mp.count(key))
@@ -72,24 +62,28 @@ public:
         Node* node = mp[key];
         deleteNode(node);
         insertFront(node);
+
         return node->val;
     }
 
     void put(int key, int value) {
-
-        if (mp.count(key)) {
-            Node* node = mp[key];
-            node->val = value;
+        if(mp.count(key)){
+            Node* node=mp[key];
+            node->val=value;
             deleteNode(node);
             insertFront(node);
-        } else {
-            if (mp.size() == cap) {
-                deleteBack();
-            }
-            Node* node = new Node(key, value);
-            insertFront(node);
-            mp[key] = node;
+            return;
         }
+
+        Node* node=new Node(key,value);
+
+        if(mp.size()==cap){
+            int last=tail->key;
+            mp.erase(last);
+            deleteBack();
+        }
+        mp[key]=node;
+        insertFront(node);
     }
 };
 
