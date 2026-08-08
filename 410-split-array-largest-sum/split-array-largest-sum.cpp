bdaa0ba1aab03ad0104058bc969cd2e110906fb4
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-08 20:01:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (97.87%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& nums, int k,int mid){
-        int sum=0;
-        int cnt=1;
+    bool isPossible(int mid,vector<int>& nums, int k){
+        int num=1;
         int n=nums.size();
+        int sum=0;
         for(int i=0;i<n;i++){
-            if(sum+nums[i]<=mid){
-                sum+=nums[i];
+            if(sum+nums[i]>mid){
+                num++;
+                sum=nums[i];
             }
             else{
-                sum=nums[i];
-                cnt++;
+                sum+=nums[i];
             }
-            if(cnt>k) return false;
+            if(num>k) return false;
         }
-        return cnt<=k;
+        return num<=k;
     }
     int splitArray(vector<int>& nums, int k) {
         int l=*max_element(nums.begin(),nums.end());
@@ -22,8 +22,8 @@ public:
         int ans=-1;
         while(l<=h){
             int mid=(l+h)/2;
-            // cout<<mid;
-            if(isPossible(nums,k,mid)){
+
+            if(isPossible(mid,nums,k)){
                 ans=mid;
                 h=mid-1;
             }
