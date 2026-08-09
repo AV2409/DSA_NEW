1c0ab27bd67e31deee5170a2a086af4904f0b3b6
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-09 12:38:16 +0530

    Time: 23 ms (5.26%) | Memory: 37.2 MB (5.73%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -6,42 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    unordered_map<TreeNode*, int> canPickDP;
-    unordered_map<TreeNode*, int> cannotPickDP;
+    unordered_map<TreeNode*,unordered_map<int,int>>dp;
+    int helper(TreeNode* root,int canPick){
+        if(!root) return 0;
+        if(dp[root].count(canPick)) return dp[root][canPick];
 
-    int solve(TreeNode* root, bool canPick) {
-        if (!root)
-            return 0;
-        if (canPick) {
-            if (canPickDP.count(root))
-                return canPickDP[root];
-        } else {
-            if (cannotPickDP.count(root))
-                return cannotPickDP[root];
-        }
-        int pick = 0;
-        int notpick = 0;
-        // can pick
-        if (canPick) {
-            int left = solve(root->left, false);
-            int right = solve(root->right, false);
-            pick = root->val + left + right;
-        }
-        // cannot pick
-        int left = solve(root->left, true);
-        int right = solve(root->right, true);
-        notpick = left + right;
+        int pick=0;
+        if(canPick) pick=root->val+helper(root->left,0)+helper(root->right,0);
+        int notPick=helper(root->left,1)+helper(root->right,1);
 
-        if (canPick) {
-            return canPickDP[root] = max(pick, notpick);
-        }
-        return cannotPickDP[root] = max(pick, notpick);
+        return dp[root][canPick]= max(pick,notPick);
+
     }
-    int rob(TreeNode* root) { return solve(root, true); }
+    int rob(TreeNode* root) {
+        return helper(root,1);
+    }
 };
