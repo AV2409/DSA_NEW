bd7c0196312a191091483204749617807afca7ef
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-09 15:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (27.44%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<int>vis;
     int n;
-    void f(vector<int>& nums,vector<int>&list,vector<vector<int>>&ans){
+    vector<int>list;
+    vector<vector<int>>ans;
+    vector<int>vis;
+    void helper(vector<int>& nums){
         if(list.size()==n){
             ans.push_back(list);
             return;
         }
-        for(int j=0;j<nums.size();j++){
-            if(!vis[j]){
-                list.push_back(nums[j]);
-                vis[j]=1;
-                f(nums,list,ans);
-                list.pop_back();
-                vis[j]=0;
-            }
+
+        for(int i=0;i<n;i++){
+            if(vis[i]) continue;
+            vis[i]=1;
+            list.push_back(nums[i]);
+            helper(nums);
+            list.pop_back();
+            vis[i]=0;
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         n=nums.size();
         vis.assign(n,0);
-        vector<vector<int>>ans;
-        vector<int>list;
-        f(nums,list,ans);
+        helper(nums);
         return ans;
     }
 };
