96f78cd74e592d585159f2478fc93ec84363857e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-09 15:21:30 +0530

    Time: 6 ms (26.80%) | Memory: 18.3 MB (18.68%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    vector<int>list;
-    int n;
-    void f(int i,vector<int>& candidates, int target){
-        if(i==n){
-            if(target==0){
+    vector<int> list;
+    vector<vector<int>> ans;
+    void helper(int i, vector<int>& nums, int target) {
+        if (i < 0) {
+            if (target == 0)
                 ans.push_back(list);
-            }
             return;
         }
-        if(target==0){
+        if (target == 0){
             ans.push_back(list);
             return;
         }
-
-        //pick
-        if(target-candidates[i]>=0){
-            list.push_back(candidates[i]);
-            f(i,candidates,target-candidates[i]);
+            
+        // notpick
+        helper(i - 1, nums, target);
+        // pick
+        if (target >= nums[i]) {
+            list.push_back(nums[i]);
+            helper(i, nums,target-nums[i]);
             list.pop_back();
         }
-
-        f(i+1,candidates,target);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        n=candidates.size();
-        f(0,candidates,target);
+        int n=candidates.size();
+        helper(n-1,candidates,target);
         return ans;
     }
 };
