b6616e6e7e612d2ccafd6140d420d6e75243119f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-09 14:26:37 +0530

    Time: 35 ms (38.05%) | Memory: 66.9 MB (5.03%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> hash(26);
-        for (char c : tasks)
-            hash[c-'A']++;
-        
-        int cnt=0;
-        int maxi=1;
-        int tot=0;
-        for (int it : hash) {
-            maxi=max(maxi,it);
-            if(it) cnt++;
-            tot+=it;
+
+        int ans=0;
+        vector<int> hash(26, 0);
+        for (char c : tasks) {
+            hash[c - 'A']++;
+        }
+        priority_queue<int> pq;
+        for (int i = 0; i < 26; i++) {
+            if(hash[i]>0)
+                pq.push(hash[i]);
         }
 
-        int temp=0;
+        while (!pq.empty()) {
+            auto cnt1 = pq.top();
+            pq.pop();
+            ans++;
+            cnt1--;
+            queue<int>q;
+            if (cnt1 > 0) {
+                for(int i=0;i<n;i++){
+                    ans++;
+                    if(pq.empty()) continue;
+                    auto cnt2 = pq.top();
+                    pq.pop();
+                    cnt2--;
+                    if(cnt2>0) q.push(cnt2);
+                }
+                while(!q.empty()){
+                    pq.push(q.front());
+                    q.pop();
+                }
+                pq.push(cnt1);
+            }
 
-        for (int it : hash) {
-            if(it==maxi) temp++;
         }
-
-        int slotSize=n+1;
-        int slots=maxi;
-        int ss=tasks.size();
-        int ans=(slots-1)*(slotSize);
-        ans=max(ss,ans+temp);
         return ans;
     }
 };
