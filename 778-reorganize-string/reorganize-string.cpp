a8b11ddae7bcca5704b76debce4e3d25604426c1
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-09 14:00:13 +0530

    Time: 2 ms (26.81%) | Memory: 8.3 MB (80.34%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -4,44 +4,40 @@ public:
         int n = s.size();
         if (n == 1)
             return s;
+
         string ans = "";
-        unordered_map<char, int> mp;
+        vector<int> hash(26, 0);
         for (char c : s) {
-            mp[c]++;
+            hash[c - 'a']++;
         }
-
-        if (n == 2 && mp.size() == 2)
-            return s;
-        if (n == 2 && mp.size() == 1)
-            return "";
-
         priority_queue<pair<int, char>> pq;
-
-        for (auto it : mp) {
-            int cnt = it.second;
-            char ch = it.first;
-            pq.push({cnt, ch});
+        for (int i = 0; i < 26; i++) {
+            if(hash[i]>0)
+                pq.push({hash[i], i + 'a'});
         }
 
         while (!pq.empty()) {
-            auto [cnt1,ch1]=pq.top();
+            auto it1 = pq.top();
             pq.pop();
-            if(!ans.empty() && ans.back()==ch1) return "";
-            ans+=ch1;
+            int cnt1 = it1.first;
+            char ch1 = it1.second;
+
+            ans += ch1;
             cnt1--;
-            if(cnt1==0) continue;
-
-            if(!pq.empty()){
-                auto [cnt2,ch2]=pq.top();
+            if (cnt1 > 0) {
+                if (pq.empty())
+                    return "";
+                auto it2 = pq.top();
                 pq.pop();
-                ans+=ch2;
-                cnt2--;
-                if(cnt2) pq.push({cnt2,ch2});
-            }
-            else return "";
-            if(cnt1) pq.push({cnt1,ch1});
-        }
+                int cnt2 = it2.first;
+                char ch2 = it2.second;
 
+                ans += ch2;
+                cnt2--;
+                if(cnt2>0) pq.push({cnt2,ch2});
+                pq.push({cnt1,ch1});
+            }
+        }
         return ans;
     }
 };
