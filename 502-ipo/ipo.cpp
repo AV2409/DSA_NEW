40d3edc6f2440fc4ed75730a88131826ef01638d
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-09 14:56:10 +0530

    Time: 218 ms (5.35%) | Memory: 176.1 MB (5.35%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int findMaximizedCapital(int k, int w, vector<int>& profits,
-                             vector<int>& capital) {
-        priority_queue<int> pq;
-        vector<pair<int,int>>tasks;
+    static bool comp(vector<int>& a, vector<int>& b) { return a[0] < b[0]; }
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<vector<int>>tasks;
         int n = profits.size();
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             tasks.push_back({capital[i],profits[i]});
         }
+        
+        sort(tasks.begin(), tasks.end(), comp);
+        int idx = 0;
+        priority_queue<int>pq;
 
-        sort(tasks.begin(),tasks.end());
-        int idx=0;
-        for(int i=0;i<k;i++){
-            while(idx<n && tasks[idx].first<=w){
-                pq.push(tasks[idx].second);
+        while (idx < n && tasks[idx][0] <= w) {
+            pq.push(tasks[idx][1]);
+            idx++;
+        }
+        int ans=w;
+        while (!pq.empty() && k>0) {
+            auto pro = pq.top();
+            pq.pop();
+            k--;
+            ans+=pro;
+            w+=pro;
+            while (idx < n && tasks[idx][0] <= w) {
+                pq.push(tasks[idx][1]);
                 idx++;
             }
-
-            if(pq.empty()) break;
-
-            int maxPro=pq.top();
-            pq.pop();
-            w+=maxPro;
         }
-
-        return w;
+        return ans;
     }
 };
