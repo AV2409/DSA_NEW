77a635887f344fc27b2832b75b6799e6d807333b
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-10 15:38:31 +0530

    Time: 6 ms (25.00%) | Memory: 27.5 MB (79.06%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-vector<int>dx={0,0,-1,1};
-vector<int>dy={-1,1,0,0};
-int rows;
-int cols;
-    void dfs_helper(vector<vector<int>>& grid,int x,int y,int &ans){
-        ans++;
+    vector<int>dx={0,0,1,-1};
+    vector<int>dy={1,-1,0,0};
+    int m;
+    int n;
+    void dfs(int x,int y,vector<vector<int>>& grid,int &size){
         grid[x][y]=0;
+        size++;
         for(int dir=0;dir<4;dir++){
             int nx=x+dx[dir];
             int ny=y+dy[dir];
 
-            if(nx>=0 && ny>=0 && nx<rows && ny<cols && grid[nx][ny]==1){
-                dfs_helper(grid,nx,ny,ans);
+            if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny]==1){
+                dfs(nx,ny,grid,size);
             }
         }
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        
-        int n=grid.size();
-        int m=grid[0].size();
-        rows=n;
-        cols=m;
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    int ans=0;
-                    dfs_helper(grid,i,j,ans);
-                    maxi=max(ans,maxi);
+        m=grid.size();
+        n=grid[0].size();
+        int ans=0;
+        for(int x=0;x<m;x++){
+            for(int y=0;y<n;y++){
+                if(grid[x][y]==1){
+                    int size=0;
+                    dfs(x,y,grid,size);
+                    ans=max(ans,size);
                 }
             }
         }
-        return maxi;
+        return ans;
     }
 };
