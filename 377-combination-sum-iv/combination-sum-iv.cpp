26c671713bc5ef325bf5d2f5d57c309babf9f184
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-11 11:55:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (52.61%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    vector<int> dp;
-    int f(int target, vector<int>& nums) {
-        if(target==0) return 1;
+    vector<int>dp;
+    int f(vector<int>& nums, int target) {
+        if (target == 0)
+            return 1;
+
         if(dp[target]!=-1) return dp[target];
         int ans=0;
-        for(int x:nums){
-            if(x<=target) ans+=f(target-x,nums);
+        for (int i = 0; i < nums.size(); i++) {
+            int p = 0;
+            if (target >= nums[i])
+                p = f(nums, target - nums[i]);
+            ans+= p;
         }
         return dp[target]= ans;
     }
     int combinationSum4(vector<int>& nums, int target) {
         int n = nums.size();
-        dp.assign(target + 1, -1);
-        int x = f(target, nums);
-        return x;
+        dp.assign(target+1,-1);
+        return f(nums, target);
     }
 };
