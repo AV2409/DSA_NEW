69a5f7a6375d8e12a0a57b2f4a5d090b0115ffdb
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-11 10:26:50 +0530

    Time: 29 ms (67.70%) | Memory: 41.4 MB (29.83%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,69 +1,70 @@
-class DisjointSet {
+class DSU{
 public:
-    vector<int> parent;
-    vector<int> size;
-    DisjointSet(int n) {
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
+    vector<int>par;
+    vector<int>size;
+    int n;
+    DSU(int _n){
+        n=_n;
+        size.assign(n,1);
+        for(int i=0;i<n;i++){
+            par.push_back(i);
         }
     }
-    int find(int node) {
-        if (node == parent[node]) {
-            return node;
-        }
-        return parent[node] = find(parent[node]);
+
+    int findPar(int u){
+        if(par[u]==u) return u;
+        return par[u]=findPar(par[u]);
     }
-    void unite(int u, int v) {
-        int ulp_u = find(u);
-        int ulp_v = find(v);
-        if (ulp_u == ulp_v) {
-            return;
-        }
-        if (size[ulp_u] < size[ulp_v])
-            swap(ulp_u, ulp_v);
-        parent[ulp_v] = ulp_u;
-        size[ulp_u] += size[ulp_v];
+
+    void unite(int u,int v){
+        int pu=findPar(u);
+        int pv=findPar(v);
+        if(pu==pv) return;
+        if(size[pu]<size[pv]) swap(pu,pv);
+
+        size[pu]+=size[pv];
+        par[pv]=pu;
     }
 };
 
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        unordered_map<string, int> emails;
-        int n = accounts.size();
-        vector<string> names(n);
-        DisjointSet dsu(n);
-        for (int i = 0; i < n; i++) {
-            int ss = accounts[i].size();
-            names[i] = accounts[i][0];
-            for (int j = 1; j < ss; j++) {
-                string mail = accounts[i][j];
-                if (emails.count(mail)) {
-                    dsu.unite(i, emails[mail]);
-                } else
-                    emails[mail] = i;
+        int n=accounts.size();
+        vector<string>names(n);
+        unordered_map<string,int>mp;
+        DSU dsu(n);
+
+        for(int i=0;i<n;i++){
+            int ss=accounts[i].size();
+            names[i]=accounts[i][0];
+
+            for(int j=1;j<ss;j++){
+                string mail=accounts[i][j];
+                if(mp.count(mail)){
+                    dsu.unite(i,mp[mail]);
+                }
+                else mp[mail]=i;
             }
         }
-        unordered_map<int, set<string>> merged;
-        for (auto& it : emails) {
-            string mail = it.first;
-            int idx = it.second;
-            int parent = dsu.find(idx);
-            merged[parent].insert(mail);
+
+        vector<set<string>>temp(n);
+        for(auto it:mp){
+            int idx=it.second;
+            string mail=it.first;
+
+            idx=dsu.findPar(idx);
+            temp[idx].insert(mail);
         }
         vector<vector<string>>ans;
-        for(auto it:merged){
-            int idx=it.first;
-            vector<string>temp;
-            temp.push_back(names[idx]);
-            for(auto mm:it.second){
-                temp.push_back(mm);
+        for(int i=0;i<n;i++){
+            vector<string>tt;
+            if(temp[i].size()>0) {
+                tt.push_back(names[i]);
+                for(auto mail:temp[i]) tt.push_back(mail);
+                ans.push_back(tt);
             }
-            ans.push_back(temp);
         }
         return ans;
-        
     }
 };
