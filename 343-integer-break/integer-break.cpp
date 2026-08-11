95c0ad9ef71a6e6d79c70b7397618a91df557575
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-11 12:15:51 +0530

    Time: 1 ms (14.38%) | Memory: 9.9 MB (5.68%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    vector<int>dp;
-    int f(int n,int k){
-        if(n==0){
-            if(k==1) return 0;
-            return 1;
+    vector<int> dp;
+
+    int f(int n) {
+        if(n == 1) return 1;
+        if(dp[n] != -1)
+            return dp[n];
+
+        int ans = 0;
+
+        for(int i = 1; i < n; i++) {
+            ans = max({ans, i * f(n-i),i*(n-i)});
         }
-        if(n==1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        int ans=1;
-        for(int i=1;i<=n;i++){
-            int temp=i*f(n-i,k+1);
-            ans=max(ans,temp);
-        }
-        return dp[n]= ans;
+        return dp[n] = ans;
     }
+
     int integerBreak(int n) {
-        dp.assign(n+1,-1);
-        return f(n,0);
-        
+        dp.assign(n+1, -1);
+        return f(n);
     }
 };
