0d373ec56e5dcac7b754297dba039bcb5961d24c
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-11 12:28:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (64.56%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
+    int n;
     vector<int>dp;
-    int f(int i, string s) {
-        if (i < 0)
-            return 1;
-        
+    int f(int i,string &s){
+        if(i==n) return 1;
+        if(s[i]=='0') return 0;
         if(dp[i]!=-1) return dp[i];
-        int op1 = 0;
-        int op2 = 0;
-
-        int nn=s[i]-'0';
-        if(nn>=1 && nn<=9){
-            op1 = f(i - 1, s);
+        char ch=s[i];
+        int ans=0;
+        if(ch=='1'){
+            ans+=f(i+1,s);
+            if(i+2<=n) ans+=f(i+2,s);
         }
-
-        if (i - 1 >= 0) {
-            int num = (s[i - 1] - '0') * 10 + (s[i] - '0');
-            if (num >= 10 && num <= 26)
-                op2 = f(i - 2, s);
+        else if(ch=='2'){
+            ans+=f(i+1,s);
+            if(i+2<=n && s[i+1]>='0' && s[i+1]<='6') ans+=f(i+2,s);
         }
-        
-        return dp[i]= op1 + op2;
+        else{
+            ans+=f(i+1,s);
+        }
+        return dp[i]= ans;
     }
     int numDecodings(string s) {
-        int n = s.size();
+        n=s.size();
         dp.assign(n,-1);
-        return f(n - 1, s);
+        return f(0,s);
     }
 };
