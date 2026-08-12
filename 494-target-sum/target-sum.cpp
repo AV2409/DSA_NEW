22552fa1f63d26c99916e47d1e15590e0b0a622b
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-12 11:14:29 +0530

    Time: 4 ms (76.53%) | Memory: 15.4 MB (55.12%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int f(int i, vector<int>& arr, int target) {
-        // if(target==0) return 1;
-        if (i < 0)
-            return target == 0;
-        if (dp[i][target] != -1)
-            return dp[i][target];
-        int np = f(i - 1, arr, target);
-        int p = 0;
-        if (target - arr[i] >= 0)
-            p = f(i - 1, arr, target - arr[i]);
-
-        return dp[i][target] = p + np;
-    }
-    int perfectSum(vector<int>& arr, int target) {
-        // code here
-        int n = arr.size();
-        dp.assign(n, vector<int>(target + 1, -1));
-        return f(n - 1, arr, target);
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
+    vector<vector<int>>dp;
+    int f(int i,vector<int>& nums, int target){
+        if(i<0){
+            if(target==0) return 1;
+            return 0;
         }
 
-        if(target>sum) return 0;
-        int reqSum=sum-target;
-        if((reqSum)%2) return 0;
-        reqSum/=2;
+        if(dp[i][target]!=-1) return dp[i][target];
+
+        int np=f(i-1,nums,target);
+        int p=0;
+        if(target>=nums[i]) p=f(i-1,nums,target-nums[i]);
+        return dp[i][target]= p+np;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum=accumulate(nums.begin(),nums.end(),0);
+
+        if(abs(target)>sum) return 0;
+        int req=sum+target;
+        if(req & 1) return 0;
+        req/=2;
+        int n=nums.size();
+        dp.assign(n,vector<int>(req+1,-1));
         
-        return perfectSum(nums,reqSum);
+        return f(n-1,nums,req);
         
     }
 };
