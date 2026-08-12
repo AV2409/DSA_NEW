4007adc538507aa2f51baac701d41895a8a8e0b8
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-12 19:57:56 +0530

    Time: 34 ms (73.98%) | Memory: 41.7 MB (5.11%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-        if (n % groupSize)
-            return false;
-        // card,cnt
-        unordered_map<int, int> mp;
-        for (int x : hand)
+        unordered_map<int,int>mp;
+        for(int x:hand){
             mp[x]++;
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            pq;
-
-        for (auto it : mp) {
-            pq.push({it.first, it.second});
         }
-        while (!pq.empty()) {
+        priority_queue<pair<int,int>>pq;
+        for(auto it:mp){
+            pq.push({it.first,it.second});
+        }
+
+        while(!pq.empty()){
             auto [card,cnt]=pq.top();
             pq.pop();
             queue<pair<int,int>>q;
             for(int i=1;i<=groupSize-1;i++){
                 if(pq.empty()) return false;
-                auto [neiCard,neiCnt]=pq.top();
+                auto [c1,cnt2]=pq.top();
                 pq.pop();
-
-                if(neiCard!=card+i) return false;
-                if(neiCnt<cnt) return false;
-                neiCnt-=cnt;
-                if(neiCnt>0) q.push({neiCard,neiCnt});
+                if(c1!=card-i) return false;
+                if(cnt2<cnt) return false;
+                cnt2-=cnt;
+                if(cnt2) q.push({c1,cnt2});
             }
             while(!q.empty()){
                 pq.push(q.front());
                 q.pop();
             }
         }
-
         return true;
     }
 };
