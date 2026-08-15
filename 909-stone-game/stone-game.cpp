ca20ccd4d9b65760fcb7a57c2e703bbb349d7973
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-15 10:30:24 +0530

    Time: 310 ms (5.20%) | Memory: 119.3 MB (7.34%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    int alice = 0;
-    int bob = 0;
+    
     vector<vector<vector<int>>>dp;
-    bool f(int i, int j, int turn, vector<int>& piles) {
+    bool f(int i, int j, int turn, vector<int>& piles,int alice,int bob) {
         if (i > j) {
             return alice > bob;
         }
         if(dp[i][j][turn]!=-1) return dp[i][j][turn];
         if (turn == 0) {
-            alice += piles[i];
-            bool op1 = f(i + 1, j, 1 - turn, piles);
-            alice -= piles[i];
-            alice += piles[j];
-            bool op2 = f(i, j - 1, 1 - turn, piles);
-            alice -= piles[j];
+            bool op1 = f(i + 1, j, 1 - turn, piles,alice+piles[i],bob);
+            bool op2 = f(i, j - 1, 1 - turn, piles,alice+piles[j],bob);
             return dp[i][j][turn]= op1 || op2;
         }
 
-        bob += piles[i];
-        bool op1 = f(i + 1, j, 1 - turn, piles);
-        bob -= piles[i];
-        bob += piles[j];
-        bool op2 = f(i, j - 1, 1 - turn, piles);
-        bob -= piles[j];
+        bool op1 = f(i + 1, j, 1 - turn, piles,alice,bob+piles[i]);
+        bool op2 = f(i, j - 1, 1 - turn, piles,alice,bob+piles[j]);
         return dp[i][j][turn]= op1 || op2;
     }
 
     bool stoneGame(vector<int>& piles) {
         int n=piles.size();
         dp.assign(n,vector<vector<int>>(n,vector<int>(2,-1)));
-        return f(0,n-1,0,piles);
+        return f(0,n-1,0,piles,0,0);
     }
 };
