4d159cee4affff019bd8cab6bcac92ba14f39b4c
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-15 10:55:18 +0530

    Time: 11 ms (47.88%) | Memory: 15 MB (15.03%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>>dp;
+    int f(int i, int M, vector<int>& piles) {
+        if (i >= n) {
+            return 0;
+        }
+        if(dp[i][M]!=-1) return dp[i][M];
+        int ans=INT_MIN;
+        int sum=0;
+        for(int idx=i;idx<min(n,i+(2*M));idx++){
+            sum+=piles[idx];
+            int x=sum-f(idx+1,max(M,idx-i+1),piles);
+            ans=max(ans,x);
+        }
+        return dp[i][M]= ans;
+    }
+
+    int stoneGameII(vector<int>& piles) {
+        n=piles.size();
+        dp.assign(n,vector<int>(2*n+1,-1));
+        int sum=accumulate(piles.begin(),piles.end(),0);
+        int diff=f(0,1,piles);
+        cout<<diff<<endl;
+        cout<<sum;
+        return (diff+sum)/2;
+    }
+};
