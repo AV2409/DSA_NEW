5bd54085ffa7a184de13574958fa4c12d1ed7428
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-23 11:26:02 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (61.28%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        if (m > n)
-            return findMedianSortedArrays(nums2, nums1);
-        int left = (m + n + 1) / 2;
-        int low = 0;
-        int high = m;
+        int m=nums1.size();
+        int n=nums2.size();
+        if(m<n) return findMedianSortedArrays(nums2,nums1);
 
-        while (low <= high) {
-            int m1 = (low + high) / 2;
-            int m2 = left - m1;
+        int req=(m+n)/2;
+        if((m+n)%2) req++;
+        int tot=m+n;
 
-            int l1 = INT_MIN;
-            int r1 = INT_MAX;
-            int l2 = INT_MIN;
-            int r2 = INT_MAX;
+        int l=req-n;
+        int h=req;
 
-            if (m1 - 1 >= 0)
-                l1 = nums1[m1 - 1];
-            if (m1 < m)
-                r1 = nums1[m1];
+        while(l<=h){
+            int mid=(l+h)/2;
+            int part1=mid;
+            int part2=req-mid;
 
-            if (m2 - 1 >= 0)
-                l2 = nums2[m2 - 1];
-            if (m2 < n)
-                r2 = nums2[m2];
+            int l1=INT_MIN;
+            int l2=INT_MIN;
+            int r1=INT_MAX;
+            int r2=INT_MAX;
 
-            if (l1 > r2)
-                high = m1 - 1;
-            else if (l2 > r1)
-                low = m1 + 1;
+            if(part1-1>=0) l1=nums1[part1-1];
+            if(part2-1>=0) l2=nums2[part2-1];
+            if(part1<m) r1=nums1[part1];
+            if(part2<n) r2=nums2[part2];
 
-            else {
-                if ((m + n) % 2)
-                    return max(l1, l2);
-
-                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            if(l1<=r2 && l2<=r1){
+                if(tot%2) return max(l1,l2);
+                double ans=(max(l1,l2)+min(r1,r2))/2.0;
+                return ans;
+            }
+            else if(l1>r2){
+                h=mid-1;
+            }
+            else if(l2>r1){
+                l=mid+1;
             }
         }
+        return -1;
 
-        return 0;
     }
 };
