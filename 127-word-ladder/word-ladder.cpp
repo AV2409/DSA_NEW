459d4f64231a1616afed473691fb69287ad24f3f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-29 22:20:59 +0530

    Time: 48 ms (81.79%) | Memory: 19.8 MB (87.10%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -3,36 +3,40 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string>st;
         st.insert(wordList.begin(),wordList.end());
-        st.erase(beginWord);
+
+        if(st.count(beginWord)) st.erase(beginWord);
+        if(!st.count(endWord)) return 0;
+
         queue<string>q;
         q.push(beginWord);
-        int n=beginWord.size();
-        int dist=1;
+        int ans=1;
         while(!q.empty()){
             int ss=q.size();
-
             for(int i=0;i<ss;i++){
                 string word=q.front();
                 q.pop();
-                if(word==endWord) return dist;
-                
-                for(int i=0;i<n;i++){
-                    char ch=word[i];
+
+                if(word==endWord) return ans; 
+
+                int n=word.size();
+                for(int j=0;j<n;j++){
+                    char ch=word[j];
                     for(char c='a';c<='z';c++){
-                        if(c==ch) continue;
-                        word[i]=c;
-                        if(st.count(word)) {
+                        if(ch==c) continue;
+
+                        word[j]=c;
+                        if(st.count(word)){
                             q.push(word);
                             st.erase(word);
                         }
                     }
-                    word[i]=ch;
+                    word[j]=ch;
                 }
 
+
             }
-            dist++;
+            ans++;
         }
         return 0;
-
     }
 };
