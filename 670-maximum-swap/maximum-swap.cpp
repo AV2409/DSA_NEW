2a93b2d58a249a48dd6801787d09ed21f90f340b
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-29 18:32:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (32.54%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    bool static comp(pair<char,int>&a,pair<char,int>&b){
-        return a.first>b.first;
-    }
     int maximumSwap(int num) {
-        string n=to_string(num);
-        
-        int ss=n.size();
-        vector<pair<char,int>>v(ss);
-        v[ss-1]={n[ss-1],ss-1};
-        for(int i=ss-2;i>=0;i--){
-            auto [maxi,idx]=v[i+1];
-            if(maxi>=n[i]){
-                v[i]={maxi,idx};
-            }
-            else{
-                v[i]={n[i],i};
+        string n = to_string(num);
+
+        // Last occurrence of each digit
+        vector<int> last(10, -1);
+
+        for (int i = 0; i < n.size(); i++) {
+            last[n[i] - '0'] = i;
+        }
+
+        // Find the first position where we can improve
+        for (int i = 0; i < n.size(); i++) {
+            int curr = n[i] - '0';
+
+            // Try the largest digit first
+            for (int d = 9; d > curr; d--) {
+                if (last[d] > i) {
+                    swap(n[i], n[last[d]]);
+                    return stoi(n);
+                }
             }
         }
 
-        for(int i=0;i<ss;i++){
-            if(n[i]<v[i].first){
-                int idx=v[i].second;
-                swap(n[i],n[idx]);
-                break;
-            }
-        }
-        int ans=stoi(n);
-        return ans;
-
+        return num;
     }
 };
