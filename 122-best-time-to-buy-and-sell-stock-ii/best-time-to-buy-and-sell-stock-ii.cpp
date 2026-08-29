6d21b9dcd75994fa1e312326479340f7ab719ea0
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-08-29 22:01:15 +0530

    Time: 15 ms (14.32%) | Memory: 27.6 MB (9.99%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     int n;
     vector<vector<int>>dp;
-    int f(int i,bool bought,vector<int>& prices){
-        if(i==n) return 0;
-        if(dp[i][bought]!=-1) return dp[i][bought];
-        if(bought){
-            //sell
-            int op1=prices[i]+f(i+1,false,prices);
+    int f(int i, int canBuy, vector<int>& prices) {
+        if (i == n)
+            return 0;
 
-            //skip
-            int op2=f(i+1,bought,prices);
+        if(dp[i][canBuy]!=-1) return dp[i][canBuy];
+        if (canBuy) {
+            // buy
+            int op1 = -prices[i] + f(i + 1, 0, prices);
+            // nobuy
+            int op2 = f(i + 1, 1, prices);
 
-            return dp[i][bought]= max(op1,op2);
+            return dp[i][canBuy]= max(op1, op2);
         }
-        //buy
-        int op1=-prices[i]+f(i+1,true,prices);
-        int op2=f(i+1,bought,prices);
 
-        return dp[i][bought]= max(op1,op2);
+        // sell
+        int op1 = prices[i] + f(i + 1, 1, prices);
 
+        // notsell
+        int op2 = f(i + 1, 0, prices);
+        return dp[i][canBuy]= max(op1, op2);
     }
     int maxProfit(vector<int>& prices) {
-        n=prices.size();
+        n = prices.size();
         dp.assign(n,vector<int>(2,-1));
-        return f(0,false,prices);
+        return f(0, 1, prices);
     }
 };
