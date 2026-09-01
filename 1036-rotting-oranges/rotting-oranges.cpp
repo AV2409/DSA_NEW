3c26a5b44da7de494e3f00cd92efad7bac92f9b6
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-01 10:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (86.20%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int fresh=0;
-        queue<pair<int,int>>q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2) q.push({i,j});
-                if(grid[i][j]==1) fresh++;
+        int f = 0;
+        queue<pair<int, int>> q;
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1)
+                    f++;
+                else if (grid[i][j] == 2) {
+                    q.push({i, j});
+                }
             }
         }
+        int time = 0;
 
-        int time=0;
-        vector<int>dx={1,-1,0,0};
-        vector<int>dy={0,0,1,-1};
-        while(!q.empty() && fresh>0){
-            int ss=q.size();
-            for(int i=0;i<ss;i++){
-                auto [x,y]=q.front();
+        vector<int> dx = {1, -1, 0, 0};
+        vector<int> dy = {0, 0, 1, -1};
+
+        while (f > 0 && !q.empty()) {
+            int ss = q.size();
+
+            for (int i = 0; i < ss; i++) {
+                auto [x, y] = q.front();
                 q.pop();
 
-                for(int dir=0;dir<4;dir++){
-                    int nx=x+dx[dir];
-                    int ny=y+dy[dir];
+                for (int dir = 0; dir < 4; dir++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
 
-                    if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny]==1){
-                        q.push({nx,ny});
-                        grid[nx][ny]=2;
-                        fresh--;
+                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        q.push({nx, ny});
+                        f--;
                     }
                 }
             }
             time++;
         }
-        if(fresh>0) return -1;
+        if(f) return -1;
         return time;
     }
 };
