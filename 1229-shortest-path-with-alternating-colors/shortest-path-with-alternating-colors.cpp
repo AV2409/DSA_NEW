78662d3f1bc3dd7623a9629f0febfdc09d640505
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-01 13:09:36 +0530

    Time: 5 ms (42.59%) | Memory: 19.7 MB (20.72%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
@@ -1,56 +1,71 @@
 class Solution {
 public:
-    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
-        vector<int>dist(2*n,1e9);
-        vector<vector<int>>adj(2*n);
-        for(auto it:redEdges){
-            int u=it[0];
-            int v=it[1];
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges,
+                                         vector<vector<int>>& blueEdges) {
 
-            adj[u].push_back(n+v);
+        vector<vector<int>> adjRed(n);
+        vector<vector<int>> adjBlue(n);
+        for (auto it : redEdges) {
+            int u = it[0];
+            int v = it[1];
+
+            adjRed[u].push_back(v);
         }
 
-        for(auto it:blueEdges){
-            int u=it[0];
-            int v=it[1];
+        for (auto it : blueEdges) {
+            int u = it[0];
+            int v = it[1];
 
-            adj[n+u].push_back(v);
+            adjBlue[u].push_back(v);
         }
 
-        dist[0]=0;
-        dist[n]=0;
+        vector<vector<int>> vis(n, vector<int>(2, 0));
+        vector<int> dist(n, 1e9);
 
-        queue<int>q;
-        q.push(0);
-        q.push(n);
+        dist[0] = 0;
 
-        int d=1;
-        vector<int>vis(2*n,0);
-        vis[0]=1;
-        vis[n]=1;
-        while(!q.empty()){
-            int ss=q.size();
-            while(ss--){
-                int node=q.front();
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        q.push({0, 1});
+
+        int d = 1;
+        vis[0][0] = 1;
+        vis[0][1] = 1;
+        while (!q.empty()) {
+            int ss = q.size();
+            while (ss--) {
+                auto [node, col] = q.front();
                 q.pop();
 
-                for(int nei:adj[node]){
-                    if(!vis[nei]){
-                        dist[nei]=d;
-                        q.push(nei);
-                        vis[nei]=1;
+                int red = 1 - col;
+                int blue = 1 - red;
+                // we need next blue
+                if (red) {
+                    for (int nei : adjBlue[node]) {
+                        if (!vis[nei][1]) {
+                            dist[nei] = min(d,dist[nei]);
+                            q.push({nei,1});
+                            vis[nei][1] = 1;
+                        }
+                    }
+                }
+                // we need red
+                else if (blue) {
+                    for (int nei : adjRed[node]) {
+                        if (!vis[nei][0]) {
+                            dist[nei] = min(d,dist[nei]);
+                            q.push({nei,0});
+                            vis[nei][0] = 1;
+                        }
                     }
                 }
             }
             d++;
         }
 
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            int x=min(dist[i],dist[i+n]);
-            if(x==1e9) ans.push_back(-1);
-            else ans.push_back(x);
+        for (int i = 0; i < n; i++) {
+            if(dist[i]==1e9) dist[i]=-1;
         }
-        return ans;
+        return dist;
     }
 };
