918489e1bfdcef548b4480f69392cdf116446565
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-01 11:21:45 +0530

    Time: 7 ms (56.56%) | Memory: 22.5 MB (62.63%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,52 +1,55 @@
 class Solution {
 public:
+    int m;
+    int n;
     vector<int> dx = {0, 0, 1, -1};
     vector<int> dy = {1, -1, 0, 0};
-    int rows;
-    int cols;
-
     void dfs(int x, int y, vector<vector<int>>& heights,
-             vector<vector<int>>& vis) {
-        vis[x][y] = 1;
-
+             vector<vector<bool>>& vis) {
+        vis[x][y] = true;
         for (int dir = 0; dir < 4; dir++) {
             int nx = x + dx[dir];
             int ny = y + dy[dir];
 
-            if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && vis[nx][ny] == 0 && heights[nx][ny] >= heights[x][y]) {
-                dfs(nx,ny,heights,vis);
+            if (nx >= 0 && ny >= 0 && nx < m && ny < n &&
+                heights[nx][ny] >= heights[x][y] && !vis[nx][ny]) {
+                dfs(nx, ny, heights, vis);
             }
         }
     }
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        rows = heights.size();
-        cols = heights[0].size();
-        vector<vector<int>> pacific(rows, vector<int>(cols, 0));
-        vector<vector<int>> atlantic(rows, vector<int>(cols, 0));
+        m = heights.size();
+        n = heights[0].size();
+        vector<vector<bool>> p(m, vector<bool>(n, false));
+        vector<vector<bool>> a(m, vector<bool>(n, false));
 
-        for (int i = 0; i < rows; i++)
-            dfs(i, 0, heights, pacific);
+        for (int i = 0; i < n; i++) {
 
-        for (int j = 0; j < cols; j++)
-            dfs(0, j, heights, pacific);
+            dfs(0, i, heights, p);
+        }
+        for (int i = 0; i < m; i++) {
 
-        // Atlantic (bottom row + right column)
-        for (int i = 0; i < rows; i++)
-            dfs(i, cols - 1, heights, atlantic);
+            dfs(i, 0, heights, p);
+        }
 
-        for (int j = 0; j < cols; j++)
-            dfs(rows - 1, j, heights, atlantic);
+        for (int i = 0; i < n; i++) {
+
+            dfs(m - 1, i, heights, a);
+        }
+        for (int i = 0; i < m; i++) {
+
+            dfs(i, n - 1, heights, a);
+        }
 
         vector<vector<int>> ans;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (pacific[i][j] && atlantic[i][j]) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (p[i][j] && a[i][j]) {
                     ans.push_back({i, j});
                 }
             }
         }
-
         return ans;
     }
 };
