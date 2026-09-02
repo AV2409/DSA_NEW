4b04288cb299b32ec4060f05fac2244249a72c1f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-02 13:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (63.76%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int find(vector<int>& citations,int i){
+    bool isPossible(int mid,vector<int>& citations){
         int ans=0;
         for(int x:citations){
-            if(x>=i) ans++;
+            if(x>=mid) ans++;
         }
-        return ans;
+        return ans>=mid;
     }
     int hIndex(vector<int>& citations) {
         int n=citations.size();
         int ans=0;
-        for(int i=i;i<=n;i++){
-            int cnt=find(citations,i);
-            if(cnt>=i) ans=i;
+        int l=0;
+        int h=n;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(isPossible(mid,citations)){
+                ans=mid;
+                l=mid+1;
+            }
+            else h=mid-1;
         }
         return ans;
     }
