fca11ba013027f224ecbddd97fc7fa39e7e2be17
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-02 14:36:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (79.50%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    set<string>st;
+    
     vector<string>ans;
-    string str;
+    vector<string>lst;
+    unordered_set<string>st;
     int n;
-    void helper(int i,string &s){
-        if(i==n){
-            str.pop_back();
-            ans.push_back(str);
+    void updateAns(){
+        string str="";
+        for(string x:lst){
+            str+=x;
             str+=" ";
+        }
+        str.pop_back();
+        ans.push_back(str);
+    }
+    void f(int i,string &s){
+        if(i==n){
+            updateAns();
             return;
         }
 
+        string temp="";
         for(int part=i;part<n;part++){
-            string temp=s.substr(i,part-i+1);
+            temp+=s[part];
             if(st.count(temp)){
-                str+=temp;
-                str+=" ";
-                helper(part+1,s);
-                str.pop_back();
-                int nn=temp.size();
-                while(nn--){
-                    str.pop_back();
-                }
+                lst.push_back(temp);
+                f(part+1,s);
+                lst.pop_back();
             }
         }
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         st.insert(wordDict.begin(),wordDict.end());
         n=s.size();
-        helper(0,s);
+        f(0,s);
         return ans;
     }
 };
