d026bf0e6049c4d46448eec0eba9a9018155cb78
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-02 11:31:33 +0530

    Time: 19 ms (42.64%) | Memory: 47.1 MB (73.90%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> prefix;
+    int total = 0;
+
+    Solution(vector<int>& w) {
+        for (int x : w) {
+            total += x;
+            prefix.push_back(total);
+        }
+    }
+
+    int pickIndex() {
+        int target = rand() % total + 1;
+
+        return lower_bound(prefix.begin(), prefix.end(), target) - prefix.begin();
+    }
+};
