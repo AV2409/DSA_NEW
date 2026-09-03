bea3ee51e5f01472b3c47d24d995b9cae371717b
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-03 10:26:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (20.88%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    int n;
-    int m;
+    int n1;
+    int n2;
+    int n3;
     vector<vector<int>>dp;
     bool f(int i,int j,string &s1, string &s2, string &s3){
-        if(i==n && j==m) return true;
+        int k=i+j;
+        if(i==n1 && j==n2) return true;
+        if(i==n1){
+            return s2.substr(j,n2-j)==s3.substr(k,n3-k);
+        }
+        if(j==n2){
+            return s1.substr(i,n1-i)==s3.substr(k,n3-k);
+        }
+
         if(dp[i][j]!=-1) return dp[i][j];
-        bool ans=false;
-        if(i<n && s1[i]==s3[i+j]) ans=ans||f(i+1,j,s1,s2,s3); 
-        if(j<m && s2[j]==s3[i+j]) ans=ans||f(i,j+1,s1,s2,s3); 
-        return dp[i][j]= ans;
+
+        bool op1=false;
+        bool op2=false;
+
+
+        if(s1[i]==s3[k]) op1=f(i+1,j,s1,s2,s3);
+        if(s2[j]==s3[k]) op2=f(i,j+1,s1,s2,s3);
+
+        return dp[i][j] = op1||op2;
+        
+
     }
     bool isInterleave(string s1, string s2, string s3) {
-        n=s1.size();
-        m=s2.size();
-        dp.assign(n+1,vector<int>(m+1,-1));
-        if(s3.size()!=n+m) return false;
-
+        n1=s1.size();
+        n2=s2.size();
+        n3=s3.size();
+        if(n1+n2!=n3) return false;
+        dp.assign(n1,vector<int>(n2,-1));
         return f(0,0,s1,s2,s3);
     }
 };
