4f9840d492e846bd7dfb2a549f72d507ed7bf6df
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-03 12:14:03 +0530

    Time: 11 ms (46.43%) | Memory: 37.3 MB (39.67%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-int MOD = 1e9 + 7;
+    int MOD=1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<long long, int>>> adj(n);
-        for (auto it : roads) {
-            int u = it[0];
-            int v = it[1];
-            int wt = it[2];
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto it:roads){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
 
-            adj[u].push_back({v, wt});
-            adj[v].push_back({u, wt});
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
         }
 
-        vector<long long> dist(n, 1e18);
-        vector<long long> ways(n, 0);
-        dist[0] = 0;
-        ways[0] = 1;
-
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
-                       greater<pair<long long, int>>>
-            pq;
-        pq.push({0, 0});
-        while (!pq.empty()) {
-            auto [d, node] = pq.top();
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
+        pq.push({0,0});
+        vector<long long> dist(n, LLONG_MAX);
+        vector<int>cnt(n,0);
+        cnt[0]=1;
+        dist[0]=0;
+        while(!pq.empty()){
+            auto [d,node]=pq.top();
             pq.pop();
-
-            if (d > dist[node])
-                continue;
-
-            for (auto [nei, wt] : adj[node]) {
-                if(d+wt==dist[nei]) ways[nei] = (ways[nei] + ways[node]) % MOD;
-                else if (d + wt < dist[nei]) {
-                    dist[nei] = d + wt;
-                    ways[nei]=ways[node];
-                    pq.push({dist[nei], nei});
+            if(d>dist[node]) continue;
+            for(auto &[nei,wt]:adj[node]){
+                if(wt+d<dist[nei]){
+                    dist[nei]=wt+d;
+                    cnt[nei]=cnt[node];
+                    pq.push({wt+d,nei});
+                }
+                else if(wt+d==dist[nei]){
+                    cnt[nei]=(cnt[nei]+cnt[node])%MOD;
                 }
             }
         }
-        
-        return ways[n-1]%MOD;
+
+        return cnt[n-1]%MOD;
     }
 };
