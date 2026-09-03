7d5f020890ca1fba3ad871d72fbe3dc048d76971
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-03 15:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (65.07%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int m=s.size();
-        int n=t.size();
-        int cnt1=0;
         vector<int>hash(256,0);
-        for(char c:t) {
-            if(hash[c]==0) cnt1++;
-            hash[c]++;
+        int cnt1=0;
+        for(char c:t){
+            if(hash[c]++==0) cnt1++;
         }
 
+        int n=s.size();
+        int st=-1;
+        int len=1e9;
         int l=0;
         int cnt2=0;
-        int st=-1;
-        int minlen=INT_MAX;
-        for(int r=0;r<m;r++){
-            hash[s[r]]--;
-            if(hash[s[r]]==0) cnt2++;
+        for(int r=0;r<n;r++){
+            char ch=s[r];
+            if(--hash[ch]==0) cnt2++;
             while(cnt1==cnt2){
-                int len=r-l+1;
-                if(len<minlen){
-                    minlen=len;
+                if(r-l+1<len){
+                    len=r-l+1;
                     st=l;
                 }
-                if(hash[s[l]]==0) cnt2--;
-                hash[s[l]]++;
+                if(++hash[s[l]]>0) cnt2--;
                 l++;
             }
         }
         if(st==-1) return "";
-        return s.substr(st,minlen);
+        return s.substr(st,len);
     }
 };
