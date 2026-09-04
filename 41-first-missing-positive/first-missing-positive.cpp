4c5f5b608cd5d1ae55c4403dc0ef2b9ac4a60849
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-04 16:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 54.8 MB (98.81%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -4,21 +4,18 @@ public:
         int n=nums.size();
         int i=0;
         while(i<n){
-            if(nums[i]==i+1) {
+            int ele=nums[i];
+            if(ele<=0 || ele>=n) {
                 i++;
                 continue;
             }
-            if(nums[i]<0){
-                i++;
-                continue;
-            }
-            int correctidx=nums[i]-1;
-            if(correctidx >= 0 && correctidx<n && nums[i] != nums[correctidx]) {
-                swap(nums[i],nums[correctidx]);
+            int idx=ele-1;
+            if(i!=idx && nums[i]!=nums[idx]){
+                swap(nums[i],nums[idx]);
             }
             else i++;
-
         }
+
         for(int i=0;i<n;i++){
             if(nums[i]!=i+1) return i+1;
         }
