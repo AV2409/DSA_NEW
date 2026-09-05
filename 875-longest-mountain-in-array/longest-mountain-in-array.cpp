80b7d6f02df88a4725a534772fc53c88d7ca338f
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-05 22:42:47 +0530

    Time: 458 ms (5.06%) | Memory: 23.7 MB (7.41%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.cpp
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -5,19 +5,29 @@ public:
         vector<int>left(n,1);
         vector<int>right(n,1);
         int ans=0;
-        for(int i=0;i<n;i++){
-            int x=i-1;
-            int y=i+1;
-            while(x>=0 && arr[x]<arr[x+1]) x--;
-            while(y<n && arr[y]<arr[y-1]) y++;
 
-            left[i]=i-x;
-            right[i]=y-i;
+        for(int i=1;i<n;i++){
+            if(arr[i]>arr[i-1]) left[i]=1+left[i-1];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>arr[i+1]) right[i]=1+right[i+1];
+        }
+
+        for(int i=0;i<n;i++){
+            // int x=i-1;
+            // int y=i+1;
+            // while(x>=0 && arr[x]<arr[x+1]) x--;
+            // while(y<n && arr[y]<arr[y-1]) y++;
+
+            // left[i]=i-x;
+            // right[i]=y-i;
 
             if(left[i]>1 && right[i]>1){
                 ans=max(ans,left[i]+right[i]-1);
             }
         }
+
         for(int i=0;i<n;i++){
             cout<<left[i]<<" ";
         }
@@ -25,6 +35,7 @@ public:
         for(int i=0;i<n;i++){
             cout<<right[i]<<" ";
         }
+        
         return ans;
         
     }
