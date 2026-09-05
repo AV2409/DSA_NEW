8aad045621605e1d5c7f64f4606d678cfc41bc4e
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-05 22:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (21.54%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.cpp
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -15,27 +15,10 @@ public:
         }
 
         for(int i=0;i<n;i++){
-            // int x=i-1;
-            // int y=i+1;
-            // while(x>=0 && arr[x]<arr[x+1]) x--;
-            // while(y<n && arr[y]<arr[y-1]) y++;
-
-            // left[i]=i-x;
-            // right[i]=y-i;
-
             if(left[i]>1 && right[i]>1){
                 ans=max(ans,left[i]+right[i]-1);
             }
-        }
-
-        for(int i=0;i<n;i++){
-            cout<<left[i]<<" ";
-        }
-        cout<<endl;
-        for(int i=0;i<n;i++){
-            cout<<right[i]<<" ";
-        }
-        
+        }        
         return ans;
         
     }
