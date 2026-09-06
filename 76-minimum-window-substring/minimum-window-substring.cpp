486c24dd1049b8234797501d261841abe809e4c2
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-06 21:33:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (26.78%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -7,13 +7,10 @@ public:
         return true;
     }
     string minWindow(string s, string t) {
-        vector<int>have(256,0);
-        vector<int>needed(256,0);
         vector<int>hash(256,0);
         int cnt1=0;
         for(char c:t){
             if(hash[c]++==0) cnt1++;
-            needed[c]++;
         }
 
         int n=s.size();
@@ -24,14 +21,12 @@ public:
         for(int r=0;r<n;r++){
             char ch=s[r];
             if(--hash[ch]==0) cnt2++;
-            have[ch]++;
             while(cnt1==cnt2){
                 if(r-l+1<len){
                     len=r-l+1;
                     st=l;
                 }
                 if(++hash[s[l]]==1) cnt2--;
-                have[s[l]]--;
                 l++;
             }
         }
