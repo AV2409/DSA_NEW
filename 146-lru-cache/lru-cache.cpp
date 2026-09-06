78b25059a6028cb178e07ac93aa62071cff70d98
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-06 15:37:28 +0530

    Time: 16 ms (87.03%) | Memory: 47.9 MB (92.62%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,32 +1,29 @@
 class Node {
 public:
-    int key, val;
-    Node *prev, *next;
+    int key;
+    int val;
+    Node* next;
+    Node* prev;
 
     Node(int k, int v) {
         key = k;
         val = v;
-        prev = next = nullptr;
+        next = NULL;
+        prev = NULL;
     }
 };
 
 class LRUCache {
-private:
-    int cap;
-    unordered_map<int, Node*> mp;
-    Node *head, *tail; // Dummy head & dummy tail
-
-    void insertFront(Node* node) {
+public:
+    void insertNode(Node* node) {
         Node* prev = head;
         Node* next = head->next;
 
         node->next = next;
-        node->prev = prev;
-
+        node->prev = head;
         prev->next = node;
         next->prev = node;
     }
-
     void deleteNode(Node* node) {
         Node* prev = node->prev;
         Node* next = node->next;
@@ -35,10 +32,14 @@ private:
         next->prev = prev;
     }
 
-public:
+    Node* head;
+    Node* tail;
+    int cap;
+    int n;
+    unordered_map<int, Node*> mp;
     LRUCache(int capacity) {
+        n = 0;
         cap = capacity;
-
         head = new Node(-1,-1);
         tail = new Node(-1,-1);
 
@@ -50,8 +51,9 @@ public:
         if (!mp.count(key))
             return -1;
         Node* node = mp[key];
+
         deleteNode(node);
-        insertFront(node);
+        insertNode(node);
 
         return node->val;
     }
@@ -61,20 +63,22 @@ public:
             Node* node = mp[key];
             node->val=value;
             deleteNode(node);
-            insertFront(node);
+            insertNode(node);
             return;
         }
 
-        Node* node=new Node({key,value});
-        if(mp.size()==cap){
+        if(cap==n){
             Node* lru=tail->prev;
-            mp.erase(lru->key);
             deleteNode(lru);
+            mp.erase(lru->key);
             delete lru;
+            n--;
         }
 
-        insertFront(node);
+        Node* node=new Node(key,value);
         mp[key]=node;
+        insertNode(node);
+        n++;
     }
 };
 
