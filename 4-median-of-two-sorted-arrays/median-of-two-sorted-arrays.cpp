a146532dc3557d5af89586669467e4bac733cd83
Akshansh Vij <142912539+AV2409@users.noreply.github.com>
2026-09-06 14:58:44 +0530

    Time: 11 ms (5.78%) | Memory: 95.6 MB (33.16%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,43 +3,23 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m=nums1.size();
         int n=nums2.size();
-        if(m<n) return findMedianSortedArrays(nums2,nums1);
 
-        int req=(m+n)/2;
-        if((m+n)%2) req++;
-        int tot=m+n;
-
-        int l=req-n;
-        int h=req;
-
-        while(l<=h){
-            int mid=(l+h)/2;
-            int part1=mid;
-            int part2=req-mid;
-
-            int l1=INT_MIN;
-            int l2=INT_MIN;
-            int r1=INT_MAX;
-            int r2=INT_MAX;
-
-            if(part1-1>=0) l1=nums1[part1-1];
-            if(part2-1>=0) l2=nums2[part2-1];
-            if(part1<m) r1=nums1[part1];
-            if(part2<n) r2=nums2[part2];
-
-            if(l1<=r2 && l2<=r1){
-                if(tot%2) return max(l1,l2);
-                double ans=(max(l1,l2)+min(r1,r2))/2.0;
-                return ans;
-            }
-            else if(l1>r2){
-                h=mid-1;
-            }
-            else if(l2>r1){
-                l=mid+1;
-            }
+        int k=(m+n+2)/2;
+        priority_queue<int>pq;
+        for(int x:nums1){
+            pq.push(x);
+            if(pq.size()>k) pq.pop();
         }
-        return -1;
 
+        for(int x:nums2){
+            pq.push(x);
+            if(pq.size()>k) pq.pop();
+        }
+
+        if((m+n)%2) return pq.top();
+        int a1=pq.top();
+        pq.pop();
+        int a2=pq.top();
+        return (a1+a2)/2.0;
     }
 };
